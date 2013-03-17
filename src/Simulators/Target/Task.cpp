a515610e0da3a48ff9ad1e1f9bb507692d491388
Joao Fortuna <fortuna.araujo@gmail.com>
2013-03-17 12:49:58 +0000

    Target Simulator, intended for integration tests with INOV.

---
+++ src/Simulators/Target/Task.cpp
@@ -0,0 +1,70 @@
+//***************************************************************************
+// Copyright 2007-2013 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://www.lsts.pt/dune/licence.                                        *
+//***************************************************************************
+// Author: Joao Fortuna                                                     *
+//***************************************************************************
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+namespace Simulators
+{
+  namespace Target
+  {
+    using DUNE_NAMESPACES;
+
+    struct Task: public DUNE::Tasks::Task
+    {
+      Task(const std::string& name, Tasks::Context& ctx):
+        Tasks::Task(name, ctx)
+      {
+        bind<GpsFix>(this);
+      }
+
+      void
+      consume(const GpsFix* gps_fix)
+      {
+        IMC::Target trg;
+
+        trg.label = "lauv-seacon-1";
+        trg.lat = gps_fix->lat;
+        trg.lon = gps_fix->lon;
+        trg.z = gps_fix->height;
+        trg.z_units = IMC::Z_HEIGHT;
+        trg.cog = gps_fix->cog;
+        trg.sog = gps_fix->sog;
+
+        dispatch(trg);
+      }
+
+      void
+      onMain(void)
+      {
+        waitForMessages(1.0);
+      }
+    };
+  }
+}
+
+DUNE_TASK
