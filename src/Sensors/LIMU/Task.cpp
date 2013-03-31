f8d7d825b7aa7dbc8c37becda216cd8478364102
Ricardo Martins <rasm@lsts.pt>
2013-03-31 04:40:46 +0100

    Sensors.LIMU: added first functional version. Still needs a cleanup.

--- src/Sensors/LIMU/Task.cpp
+++ src/Sensors/LIMU/Task.cpp
@@ -28,6 +28,9 @@
 // DUNE headers.
 #include <DUNE/DUNE.hpp>
 
+// Local headers.
+#include "Packets.hpp"
+
 namespace Sensors
 {
   namespace LIMU
@@ -36,23 +39,6 @@ namespace Sensors
 
     //! Baud rate is constant.
     static const unsigned c_baud_rate = 115200;
-
-    //! Sample.
-    struct Sample
-    {
-      //! Acceleration.
-      float accel[3];
-      //! Delta Angles.
-      float deltang[3];
-      //! Delta Velocity.
-      float deltvel[3];
-      //! Euler Angles.
-      float magne[3];
-      //! Temperature.
-      float temp;
-      //! Flags.
-      uint16_t flags;
-    };
 
     //! Calibration parameters.
     struct Calibration
@@ -68,14 +54,28 @@ namespace Sensors
     {
       //! Serial port device.
       std::string uart_dev;
-      //! Sampling frequency.
-      unsigned sample_frq;
+      //! Output frequency.
+      unsigned output_frq;
     };
 
     struct Task: public Tasks::Task
     {
       //! Task arguments.
       Arguments m_args;
+      //! Angular velocity.
+      IMC::AngularVelocity m_ang_vel;
+      //! Acceleration.
+      IMC::Acceleration m_accel;
+      //! Delta angles.
+      IMC::EulerAnglesDelta m_delt_ang;
+      //! Delta velocity.
+      IMC::VelocityDelta m_delt_vel;
+      //! Euler Angles.
+      IMC::EulerAngles m_euler;
+      //! Magnetic Field.
+      IMC::MagneticField m_magn;
+      //! Temperature.
+      IMC::Temperature m_temp;
       //! Serial port device.
       UCTK::InterfaceUART* m_uart;
       //! Watchdog.
@@ -90,10 +90,10 @@ namespace Sensors
         .defaultValue("")
         .description("Serial port device used to communicate with the sensor");
 
-        param("Sampling Frequency", m_args.sample_frq)
+        param("Output Frequency", m_args.output_frq)
         .units(Units::Hertz)
         .defaultValue("100")
-        .description("Sampling frequency");
+        .description("Output frequency");
       }
 
       ~Task(void)
@@ -123,18 +123,147 @@ namespace Sensors
       void
       onResourceInitialization(void)
       {
-        m_wdog.setTop(5.0);
+        debug("initializing");
+
+        OutputConfig out_cfg;
+        out_cfg.frequency = m_args.output_frq;
+
+        if (m_uart->command(out_cfg))
+          debug("frequency configured");
+        else
+          debug("failed");
+
+        m_wdog.setTop(2.0);
+      }
+
+      void
+      decodeOutputData(const UCTK::Frame& frame)
+      {
+        double tstamp = Clock::getSinceEpoch();
+        float tmp = 0;
+        const uint8_t* ptr = frame.getData();
+
+        // Angular Velocity.
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_ang_vel.x = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_ang_vel.y = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_ang_vel.z = tmp;
+        m_ang_vel.setTimeStamp(tstamp);
+        dispatch(m_ang_vel, DF_KEEP_TIME);
+
+        // Acceleration.
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_accel.x = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_accel.y = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_accel.z = tmp;
+        m_accel.setTimeStamp(tstamp);
+        dispatch(m_accel, DF_KEEP_TIME);
+
+        // Delta Angles.
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_delt_ang.x = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_delt_ang.y = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_delt_ang.z = tmp;
+        m_delt_ang.setTimeStamp(tstamp);
+        dispatch(m_delt_ang, DF_KEEP_TIME);
+
+        // Delta Velocity.
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_delt_vel.x = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_delt_vel.y = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_delt_vel.z = tmp;
+        m_delt_vel.setTimeStamp(tstamp);
+        dispatch(m_delt_vel, DF_KEEP_TIME);
+
+        // Magnetic Field.
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_magn.x = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_magn.y = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_magn.z = tmp;
+        m_magn.setTimeStamp(tstamp);
+        dispatch(m_magn, DF_KEEP_TIME);
+
+        // Euler Angles.
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_euler.phi = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_euler.theta = tmp;
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_euler.psi_magnetic = tmp;
+        m_euler.psi = tmp;
+        m_euler.setTimeStamp(tstamp);
+        dispatch(m_euler, DF_KEEP_TIME);
+
+        // Temperature.
+        ptr += ByteCopy::fromLE(tmp, ptr);
+        m_temp.value = tmp;
+        m_temp.setTimeStamp(tstamp);
+        dispatch(m_temp, DF_KEEP_TIME);
+
+        // Error flags.
+        uint8_t error = *ptr;
+
+        spew("%u | % 0.8f | % 0.8f | % 0.8f | % 0.8f | % 0.8f | % 0.8f | MAG: % 0.8f | % 0.8f | % 0.8f",
+             error,
+             m_accel.x,
+             m_accel.y,
+             m_accel.z,
+             Angles::degrees(m_euler.phi),
+             Angles::degrees(m_euler.theta),
+             Angles::degrees(m_euler.psi),
+             m_magn.x,
+             m_magn.y,
+             m_magn.z
+             );
+
+        m_wdog.reset();
+      }
+
+      void
+      decode(const UCTK::Frame& frame)
+      {
+        if (frame.getId() == 1)
+          decodeOutputData(frame);
       }
 
       void
       onMain(void)
       {
+        uint8_t bfr[128];
+        UCTK::Parser parser;
+        UCTK::Frame frame;
+
         while (!stopping())
         {
-          waitForMessages(0.1);
+          consumeMessages();
 
           if (m_wdog.overflow())
+          {
             setEntityState(IMC::EntityState::ESTA_ERROR, Status::CODE_COM_ERROR);
+            throw RestartNeeded("input timeout", 1);
+          }
+
+          if (!m_uart->poll(1.0))
+            continue;
+
+          unsigned rv = m_uart->read(bfr, sizeof(bfr));
+          for (unsigned i = 0; i < rv; ++i)
+          {
+            if (parser.parse(bfr[i], frame))
+            {
+              decode(frame);
+            }
+          }
         }
       }
     };
