088d4c4c319f2971613a4b387e7fb953728006d2
Ricardo Martins <rasm@lsts.pt>
2013-05-26 20:49:43 +0100

    UserInterfaces.MantaPanel: dispatching LoggingControl is no longer required.

--- src/UserInterfaces/MantaPanel/Task.cpp
+++ src/UserInterfaces/MantaPanel/Task.cpp
@@ -271,9 +271,6 @@ namespace UserInterfaces
           if (!m_power_down)
           {
             m_power_down = true;
-            IMC::LoggingControl lctl;
-            lctl.op = IMC::LoggingControl::COP_REQUEST_STOP;
-            dispatch(lctl);
           }
 
           if (!m_power_down_now)
@@ -299,10 +296,6 @@ namespace UserInterfaces
           dispatch(m_lcd);
           if (std::system(m_args.cmd_pwr_down_abort.c_str()) == -1)
             err("failed to execute power down command");
-
-          IMC::LoggingControl lctl;
-          lctl.op = IMC::LoggingControl::COP_REQUEST_START;
-          dispatch(lctl);
 
           selectSystem(false);
         }
