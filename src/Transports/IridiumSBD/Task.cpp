b06ab25d7350dcfcb59e2a240996b1b05422e10d
Ricardo Martins <rasm@lsts.pt>
2013-06-15 04:25:31 +0100

    Transports.IridiumSBD: cleanup, added support for periodically check mailbox.

--- src/Transports/IridiumSBD/Task.cpp
+++ src/Transports/IridiumSBD/Task.cpp
@@ -45,6 +45,10 @@ namespace Transports
       std::string uart_dev;
       //! Serial port baud rate.
       unsigned uart_baud;
+      //! Mailbox check periodicity.
+      double mbox_check_per;
+      //! SBD session timeout.
+      double sbd_timeout;
     };
 
     struct Task: public DUNE::Tasks::Task
@@ -53,10 +57,14 @@ namespace Transports
       SerialPort* m_uart;
       //! Driver handler.
       Driver* m_driver;
-      //! Queue of transmit requests.
-      std::queue<TxRequest*> m_queue;
+      //! List of transmission requests.
+      std::list<TxRequest*> m_tx_requests;
       //! Number of MT messages queued at the GSS.
       unsigned m_queued_mt;
+      //! Mailbox check timer.
+      Counter<double> m_mbox_check_timer;
+      //! SBD session timer.
+      Counter<double> m_sbd_timer;
       //! Task arguments.
       Arguments m_args;
 
@@ -76,18 +84,39 @@ namespace Transports
         .defaultValue("19200")
         .description("Serial port baud rate");
 
+        param("Mailbox Check - Periodicity", m_args.mbox_check_per)
+        .units(Units::Second)
+        .defaultValue("300")
+        .description("Amount of time without alert rings or "
+                     "MT SBDs before doing a mailbox check");
+
+        param("SBD Session - Timeout", m_args.sbd_timeout)
+        .units(Units::Second)
+        .defaultValue("60")
+        .description("Amount of time to wait for an SBD session to finish");
+
         bind<IMC::IridiumMsgTx>(this);
       }
 
+      //! Destructor.
       ~Task(void)
       {
-        clearQueue();
+        while (!m_tx_requests.empty())
+        {
+          TxRequest* req = m_tx_requests.front();
+          m_tx_requests.pop_front();
+          sendTxRequestStatus(req,
+                              IMC::IridiumTxStatus::TXSTATUS_ERROR,
+                              "task unavailable");
+          delete req;
+        }
       }
 
       //! Update internal state with new parameter values.
       void
       onUpdateParameters(void)
       {
+        m_mbox_check_timer.setTop(m_args.mbox_check_per);
       }
 
       //! Reserve entity identifiers.
@@ -129,46 +158,93 @@ namespace Transports
       void
       consume(const IMC::IridiumMsgTx* msg)
       {
+        debug("queueing message");
         unsigned sys_id = resolveSystemName(msg->destination);
         TxRequest* req = new TxRequest(sys_id, msg->req_id, msg->data);
-        m_queue.push(req);
+        m_tx_requests.push_back(req);
+      }
+
+      void
+      sendTxRequestStatus(const TxRequest* request,
+                          IMC::IridiumTxStatus::StatusCodeEnum code,
+                          const std::string& text = "")
+      {
+        IMC::IridiumTxStatus status;
+        status.setDestination(request->getSource());
+        status.req_id = request->getId();
+        status.status = code;
+        status.text = text;
+        dispatch(status);
+      }
+
+      void
+      dequeueTxRequest(unsigned msn)
+      {
+        if (m_tx_requests.empty())
+          return;
+
+        TxRequest* req = m_tx_requests.front();
+        if (!req->hasValidMSN() || (req->getMSN() != msn))
+          return;
+
+        debug("dequeing message");
+        m_driver->clearBufferMO();
+        m_tx_requests.pop_front();
+        sendTxRequestStatus(req, IMC::IridiumTxStatus::TXSTATUS_OK);
+        delete req;
+      }
+
+      void
+      invalidateTxRequest(unsigned msn)
+      {
+        if (m_tx_requests.empty())
+          return;
+
+        TxRequest* req = m_tx_requests.front();
+        if (!req->hasValidMSN() || (req->getMSN() != msn))
+          return;
+
+        debug("invalidating MSN");
+        req->invalidateMSN();
+      }
+
+      void
+      handleSBD(void)
+      {
+        uint8_t bfr[340];
+        unsigned rv = m_driver->readBufferMT(bfr, sizeof(bfr));
+        if (rv > 2)
+        {
+          IMC::IridiumMsgRx sbd;
+          sbd.setSource((bfr[0] << 8) | bfr[1]);
+          sbd.setDestination(getSystemId());
+          sbd.data.assign(bfr + 2, bfr + rv);
+          sbd.toText(std::cerr);
+          dispatch(sbd, DF_KEEP_SRC_EID);
+        }
+        else
+        {
+          err("invalid SBD message of size %u", rv);
+        }
       }
 
       void
       handleSessionResult(void)
       {
         const SessionResult& res = m_driver->getSessionResult();
+
         if (res.isSuccessMO())
         {
-          //! FIXME: Check MSNs.
-          if (!m_queue.empty())
-          {
-            debug("dequeing message");
-            TxRequest* req = m_queue.front();
-            delete req;
-            m_queue.pop();
-            m_driver->clearBufferMO();
-          }
+          m_mbox_check_timer.reset();
+          dequeueTxRequest(res.getSequenceMO());
+        }
+        else
+        {
+          invalidateTxRequest(res.getSequenceMO());
         }
 
         if (res.getStatusMT() == 1)
-        {
-          uint8_t bfr[340];
-          unsigned rv = m_driver->readBufferMT(bfr, sizeof(bfr));
-          if (rv > 2)
-          {
-            IMC::IridiumMsgRx sbd;
-            sbd.setSource((bfr[0] << 8) | bfr[1]);
-            sbd.setDestination(getSystemId());
-            sbd.data.assign(bfr + 2, bfr + rv);
-            sbd.toText(std::cerr);
-            dispatch(sbd, DF_KEEP_SRC_EID);
-          }
-          else
-          {
-            err("invalid SBD message of size %u", rv);
-          }
-        }
+          handleSBD();
       }
 
       void
@@ -183,28 +259,19 @@ namespace Transports
         if (m_driver->getRSSI() == 0)
           return;
 
-        if (m_queue.empty())
+        if (m_tx_requests.empty())
         {
           if (m_driver->hasRingAlert())
             m_driver->checkMailBoxAlert();
-          else if (m_driver->getQueuedMT() > 0)
+          else if ((m_driver->getQueuedMT()) > 0 || m_mbox_check_timer.overflow())
             m_driver->checkMailBox();
         }
         else
         {
-          TxRequest* req = m_queue.front();
+          TxRequest* req = m_tx_requests.front();
+          unsigned msn = m_driver->getMOMSN();
+          req->setMSN(msn);
           m_driver->sendSBD(req->getData());
-        }
-      }
-
-      void
-      clearQueue(void)
-      {
-        while (!m_queue.empty())
-        {
-          TxRequest* req = m_queue.front();
-          delete req;
-          m_queue.pop();
         }
       }
 
