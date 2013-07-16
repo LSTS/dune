230353c6889aa84ae264a12fcd49e5d10c30116a
Ricardo Martins <rasm@lsts.pt>
2013-07-16 03:48:50 +0100

    CMake: create empty version file.

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -73,6 +73,7 @@ if(DUNE_VERSION_TPL AND DUNE_VERSION_OUT)
 else()
   set(DUNE_CORE_SOURCES ${DUNE_CORE_SOURCES}
     ${DUNE_GENERATED}/src/DUNE/Version.cpp)
+  file(WRITE ${DUNE_GENERATED}/src/DUNE/Version.cpp "")
 
   add_custom_target(dune-version
     COMMAND ${CMAKE_COMMAND} -DPROJECT_SOURCE_DIR="${PROJECT_SOURCE_DIR}"
