6f2a6bd0e8ff1598d57a9571539bce9623359d81
Pedro Calado <pdcalado@gmail.com>
2013-07-26 18:34:46 +0100

    Control.PathController: Added bottom tracker's admissible depth to the task's arguments.

--- src/DUNE/Control/PathController.cpp
+++ src/DUNE/Control/PathController.cpp
@@ -184,6 +184,11 @@ namespace DUNE
       .units(Units::Hertz)
       .description("Bottom tracker's execution frequency");
 
+      param("Bottom Track -- Admissible Altitude", m_btd.args.adm_alt)
+      .defaultValue("3.0")
+      .units(Units::Meter)
+      .description("Admissible altitude when doing depth control");
+
       bind<IMC::Brake>(this);
       bind<IMC::ControlLoops>(this);
       bind<IMC::DesiredPath>(this);
