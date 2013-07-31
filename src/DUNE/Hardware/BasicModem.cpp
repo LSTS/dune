a4fd53b2eb653426a2f3e250cc1d9241b76ca620
Ricardo Martins <rasm@lsts.pt>
2013-07-31 04:48:14 +0100

    DUNE BasicModem: removed unused constant.

--- src/DUNE/Hardware/BasicModem.cpp
+++ src/DUNE/Hardware/BasicModem.cpp
@@ -34,10 +34,8 @@ namespace DUNE
   {
     using DUNE_NAMESPACES;
 
-    //! Default AT command timeout.
+    //! Default command timeout.
     static const double c_timeout = 5.0;
-    //! Maximum number of revision lines.
-    static const unsigned c_max_rev_lines = 10;
 
     //! Constructor.
     //! @param[in] task parent task.
