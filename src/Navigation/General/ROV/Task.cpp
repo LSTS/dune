49b7b034cd035dbc8768e1ca270170d971a3ac14
José Braga <eejbraga@gmail.com>
2013-08-07 10:30:46 +0100

    Navigation: corrected function parameters.

--- src/Navigation/General/ROV/Task.cpp
+++ src/Navigation/General/ROV/Task.cpp
@@ -186,7 +186,7 @@ namespace Navigation
         }
 
         void
-        getSpeedOutputStates(double* u, double* v)
+        getSpeedOutputStates(unsigned* u, unsigned* v)
         {
           *u = OUT_U;
           *v = OUT_V;
