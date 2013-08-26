ef46ce7afb6a6fe65a8c9b62ce95aded5570b11b
Pedro Calado <pdcalado@gmail.com>
2013-08-26 10:43:27 +0100

    Control.Path.ILOS: Using inline function for readability. Cleaning up according to developer's guide.

--- src/Control/Path/ILOS/Task.cpp
+++ src/Control/Path/ILOS/Task.cpp
@@ -51,32 +51,32 @@ namespace Control
     {
       struct Arguments
       {
-	double corridor;
-	double entry_angle;
+        double corridor;
+        double entry_angle;
         double lookahead;
         double int_gain;
-	double int_init;
+        double int_init;
+        bool out_vec;
+        bool out_los;
       };
 
       struct Task: public DUNE::Control::PathController
       {
-	//! ILOS integrator
-	double integrator;
-	//! Time of last path controller step
-	Delta m_last_step;
-	//! Loiter controller gain.
+        //! ILOS m_integrator
+        double m_integrator;
+        //! Time of last path controller step
+        Delta m_last_step;
+        //! Loiter controller gain.
         double m_gain;
         //! Outgoing desired heading message.
         IMC::DesiredHeading m_heading;
         //! Task arguments.
         Arguments m_args;
-	bool out_vec;
-	bool out_LOS;
 
         Task(const std::string& name, Tasks::Context& ctx):
           DUNE::Control::PathController(name, ctx)
         {
-	  param("Corridor -- Width", m_args.corridor)
+          param("Corridor -- Width", m_args.corridor)
           .minimumValue("1.0")
           .maximumValue("50.0")
           .defaultValue("5.0")
@@ -90,11 +90,11 @@ namespace Control
           .units(Units::Degree)
           .description("Attack angle when lateral track error equals corridor width");
 
-	  param("Corridor -- Out Vector Field", out_vec)
+          param("Corridor -- Out Vector Field", m_args.out_vec)
           .defaultValue("false")
           .description("Out of corridor guidance law: vector field");
 
-	  param("Corridor -- Out LOS", out_LOS)
+          param("Corridor -- Out LOS", m_args.out_los)
           .defaultValue("false")
           .description("Out of corridor guidance law: LOS");
 
@@ -105,31 +105,31 @@ namespace Control
           .units(Units::Meter)
           .description("Integral Line-of-Sight look ahead distance");
 
-          param("ILOS Integrator Gain", m_args.int_gain)
+          param("ILOS M_Integrator Gain", m_args.int_gain)
           .minimumValue("0")
           .maximumValue("4")
           .defaultValue("0")
           .description("Integral Line-of-Sight integral gain");
 
-          param("ILOS Integrator Initial Value", m_args.int_init)
+          param("ILOS M_Integrator Initial Value", m_args.int_init)
           .minimumValue("0")
           .maximumValue("10")
           .defaultValue("0")
-          .description("Integrator inital value");
+          .description("M_Integrator inital value");
         }
 
         void
         onUpdateParameters(void)
         {
-	  // Initialize ILOS integrator
-	  integrator = m_args.int_init;
+          // Initialize ILOS m_integrator
+          m_integrator = m_args.int_init;
           PathController::onUpdateParameters();
 
-	  if (paramChanged(m_args.entry_angle))
+          if (paramChanged(m_args.entry_angle))
             m_args.entry_angle = Angles::radians(m_args.entry_angle);
 
-          m_gain = std::tan(m_args.entry_angle) / m_args.corridor;	  
-	}
+          m_gain = std::tan(m_args.entry_angle) / m_args.corridor;
+        }
 
         void
         onEntityReservation(void)
@@ -144,17 +144,23 @@ namespace Control
           enableControlLoops(IMC::CL_YAW);
         }
 
-      	void
-      	onReportEntityState(void)
-      	{
+        void
+        onReportEntityState(void)
+        {
           // Everything is ok so set task entity state at normal with 'Active' message.
           setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
-      	}
+        }
 
-	void
+        void
         reset(void)
         {
-          integrator = 0.0;
+          m_integrator = 0.0;
+        }
+
+        inline double
+        computeK(double l1, double l2, double ts_y, double factor)
+        {
+          return l1 / (std::pow(ts_y + m_args.int_gain * (m_integrator + factor), 2) + l2);
         }
 
         //! Execute a path control step
@@ -167,52 +173,52 @@ namespace Control
           // and along-track position = ts.track_pos.x
           double ref;
           double k1;
-	  double k2;
-	  double k3;
-	  double k4;
-	  double loc_1 = m_args.lookahead*ts.track_pos.y;
-	  double loc_2 = std::pow(m_args.lookahead,2);
-	  double timestep = m_last_step.getDelta();
-	  double kcorr = ts.track_pos.y / m_args.corridor;
+          double k2;
+          double k3;
+          double k4;
+          double loc_1 = m_args.lookahead * ts.track_pos.y;
+          double loc_2 = std::pow(m_args.lookahead, 2);
+          double timestep = m_last_step.getDelta();
+          double kcorr = ts.track_pos.y / m_args.corridor;
           double akcorr = std::fabs(kcorr);
-	  
-	  if (akcorr > 1)
-       	  {
-	    // Outside corridor, integrator OFF
-	    integrator = 0.0;
-	  }
-	  else
-	  {
-	    // Inside corridor, integrator ON
-	    // RK4 integratin
-	    k1 = loc_1/(std::pow(ts.track_pos.y+m_args.int_gain*integrator,2)+loc_2);
-	    k2 = loc_1/(std::pow(ts.track_pos.y+m_args.int_gain*(integrator+k1/2),2)+loc_2);
-	    k3 = loc_1/(std::pow(ts.track_pos.y+m_args.int_gain*(integrator+k2/2),2)+loc_2);
-	    k4 = loc_1/(std::pow(ts.track_pos.y+m_args.int_gain*(integrator+k3),2)+loc_2);
 
-	    integrator += timestep*(k1 + 2*k2 + 2*k3 + k4)/6;
-	  }
-	  
-	  // ILOS guidance
+          if (akcorr > 1)
+          {
+            // Outside corridor, m_integrator OFF
+            m_integrator = 0.0;
+          }
+          else
+          {
+            // Inside corridor, m_integrator ON
+            // RK4 integratin
+            k1 = computeK(loc_1, loc_2, ts.track_pos.y, 0.0);
+            k2 = computeK(loc_1, loc_2, ts.track_pos.y, k1/2);
+            k3 = computeK(loc_1, loc_2, ts.track_pos.y, k2/2);
+            k4 = computeK(loc_1, loc_2, ts.track_pos.y, k3);
+
+            m_integrator += timestep * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+          }
+
+          // ILOS guidance
           if (ts.track_pos.x > ts.track_length)
           {
-	    // Past the track goal: this should never happen but ...
+            // Past the track goal: this should never happen but ...
             ref = getBearing(state, ts.end);
           }
-          else if (akcorr > 1 && out_vec && !out_LOS)
+          else if (akcorr > 1 && m_args.out_vec && !m_args.out_los)
           {
-            // Outside corridor, integrator OFF, vector field guidance
-	    ref = ts.track_bearing - std::atan(m_gain * ts.track_pos.y);
+            // Outside corridor, m_integrator OFF, vector field guidance
+            ref = ts.track_bearing - std::atan(m_gain * ts.track_pos.y);
           }
-          else if (akcorr > 1 && out_vec && out_LOS)
+          else if (akcorr > 1 && m_args.out_vec && m_args.out_los)
           {
-            // Outside corridor, integrator OFF, LOS guidance
-	    ref = ts.track_bearing - std::atan(ts.track_pos.y/m_args.lookahead);
+            // Outside corridor, m_integrator OFF, LOS guidance
+            ref = ts.track_bearing - std::atan(ts.track_pos.y / m_args.lookahead);
           }
-	  else
-	  {
-   	    // Inside corridor, integrator ON, ILOS guidance
-	    ref = ts.track_bearing - std::atan((ts.track_pos.y+m_args.int_gain*integrator)/m_args.lookahead);
+          else
+          {
+            // Inside corridor, m_integrator ON, ILOS guidance
+            ref = ts.track_bearing - std::atan((ts.track_pos.y + m_args.int_gain * m_integrator) / m_args.lookahead);
           }
 
           // Dispatch heading reference
@@ -220,7 +226,7 @@ namespace Control
           dispatch(m_heading);
         }
 
-	//! Execute a loiter control step
+        //! Execute a loiter control step
         //! From base class PathController & VectorField guidance law
         void
         loiter(const IMC::EstimatedState& state, const TrackingState& ts)
