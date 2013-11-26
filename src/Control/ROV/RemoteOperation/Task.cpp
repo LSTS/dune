dcb948f0e08a9dd6e08d758f00dc09e67d2b2911
Renato Caldas <rmsc@fe.up.pt>
2013-11-26 16:15:16 +0000

    Quick-hack: added dead-zone to the depth control RemoteAction.

--- src/Control/ROV/RemoteOperation/Task.cpp
+++ src/Control/ROV/RemoteOperation/Task.cpp
@@ -46,6 +46,7 @@ namespace Control
         bool dh_control;
         bool as_control;
         float depth_rate;
+	int depth_deadzone;
         float heading_rate;
       };
 
@@ -98,6 +99,10 @@ namespace Control
           .defaultValue("0.1")
           .units(Units::MeterPerSecond)
           .description("Rate of increase or decrease of depth with control enabled.");
+
+          param("Depth Action Dead Zone", m_args.depth_deadzone)
+          .defaultValue("20")
+          .description("Remote action deadzone for the depth rate.");
 
           param("Heading Rate", m_args.heading_rate)
           .defaultValue("5.0")
@@ -171,8 +176,12 @@ namespace Control
             m_forces(0, 0) = tuples.get("Forward", 0) / 127.0;   // X
             m_forces(1, 0) = tuples.get("Starboard", 0) / 127.0; // Y
 
-            m_depth += tuples.get("Up", 0) / 127.0 * m_args.depth_rate;
-            m_depth = std::max(0.0f, m_depth);
+	    int up = tuples.get("Up", 0);
+	    if (up > m_args.depth_deadzone || up < -m_args.depth_deadzone)
+	    {
+	      m_depth += up / 127.0 * m_args.depth_rate;
+	      m_depth = std::max(0.0f, m_depth);
+	    }
 
             if (!m_args.as_control)
             {
