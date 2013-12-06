44a30b4ecc38e234d1284eece737e46d9a8e0860
Jose Braga <eejbraga@gmail.com>
2013-12-06 16:02:14 +0000

    DUNE Monitors: added a very simple medium handler.

--- src/DUNE/Monitors.hpp
+++ src/DUNE/Monitors.hpp
@@ -36,8 +36,9 @@ namespace DUNE
 }
 
 #include <DUNE/Monitors/DelayedTrigger.hpp>
+#include <DUNE/Monitors/MediumHandler.hpp>
+#include <DUNE/Monitors/MotorCurrentMonitor.hpp>
 #include <DUNE/Monitors/ServoCurrentMonitor.hpp>
 #include <DUNE/Monitors/ServoPositionMonitor.hpp>
-#include <DUNE/Monitors/MotorCurrentMonitor.hpp>
 
 #endif
