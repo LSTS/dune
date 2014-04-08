fd205e63b5c95323d48011e91c82b64d20f78191
José Pinto <zepinto@gmail.com>
2014-04-08 11:08:48 +0100

    Sensors/AIS: Added task (skeleton).

---
+++ src/Sensors/AIS/Task.cpp
@@ -0,0 +1,85 @@
+//***************************************************************************
+// Copyright 2007-2014 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://www.lsts.pt/dune/licence.                                        *
+//***************************************************************************
+// Author: Ricardo Martins                                                  *
+//***************************************************************************
+
+// ISO C++ 98 headers.
+#include <cstring>
+#include <algorithm>
+#include <cstddef>
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+namespace Sensors
+{
+  //! Device driver for AIS receivers.
+  namespace AIS
+  {
+    using DUNE_NAMESPACES;
+
+    struct Arguments
+    {
+      //! Serial port device.
+      std::string uart_dev;
+      //! Serial port baud rate.
+      unsigned uart_baud;
+    };
+
+    struct Task: public Tasks::Periodic
+    {
+      IO::Handle* m_handle;
+      Arguments m_args;
+
+      Task(const std::string& name, Tasks::Context& ctx):
+        Tasks::Periodic(name, ctx),
+        m_handle(NULL)
+      {
+        // Define configuration parameters.
+        param("Serial Port - Device", m_args.uart_dev)
+        .defaultValue("")
+        .description("Serial port device used to communicate with the sensor");
+
+        param("Serial Port - Baud Rate", m_args.uart_baud)
+        .defaultValue("4800")
+        .description("Serial port baud rate");
+      }
+
+      void
+      onResourceInitialization(void)
+      {
+        war("resource init");
+      }
+
+      void
+      task(void)
+      {
+        war("run");
+      }
+    };
+  }
+}
+
+DUNE_TASK
