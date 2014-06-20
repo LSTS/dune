76efc8c800cc1978c072c055ed5bea1994f87758
Pedro Calado <pdcalado@gmail.com>
2014-06-20 14:17:50 +0100

    Monitors/OperationalLimits: Setting default maximum depth to be larger than zero.

--- src/Monitors/OperationalLimits/Task.cpp
+++ src/Monitors/OperationalLimits/Task.cpp
@@ -117,7 +117,7 @@ namespace Monitors
         DUNE::Tasks::Periodic(name, ctx),
         m_atest_time(0),
         m_use_cfg(true),
-        m_max_depth(-1.0)
+        m_max_depth(50.0)
       {
         param("Initial Setting - Maximum Depth", m_args.i_max_depth)
         .units(Units::Meter)
@@ -162,6 +162,8 @@ namespace Monitors
         .defaultValue("0.2")
         .description("Minimum depth required to check altitude operational limits");
 
+        m_ctx.config.get("General", "Absolute Maximum Depth", "50.0", m_max_depth);
+
         bind<IMC::EstimatedState>(this);
         bind<IMC::GetOperationalLimits>(this);
         bind<IMC::OperationalLimits>(this);
@@ -185,24 +187,19 @@ namespace Monitors
       void
       setMaxDepth(void)
       {
-        if (m_max_depth > 0.0)
-        {
-          if (m_ol.mask & IMC::OPL_MAX_DEPTH)
-            m_ol.max_depth = std::min(m_ol.max_depth, m_max_depth);
-          else
-            m_ol.max_depth = m_max_depth;
+        if (m_ol.mask & IMC::OPL_MAX_DEPTH)
+          m_ol.max_depth = std::min(m_ol.max_depth, m_max_depth);
+        else
+          m_ol.max_depth = m_max_depth;
 
-          m_ol.mask |= IMC::OPL_MAX_DEPTH;
+        m_ol.mask |= IMC::OPL_MAX_DEPTH;
 
-          inf("max depth set to %.2f", m_ol.max_depth);
-        }
+        inf("max depth set to %.2f", m_ol.max_depth);
       }
 
       void
       onUpdateParameters(void)
       {
-        m_ctx.config.get("General", "Absolute Maximum Depth", "-1.0", m_max_depth);
-
         if (m_use_cfg)
         {
           reset();
