2f4bc263ab62193dc7fc018ef11facbcd5ba42a6
Ricardo Martins <rasm@lsts.pt>
2014-07-24 12:39:24 +0100

    Vision/DFK51BG02H: Replaced custom logic with isActivating() and isDeactivating().

--- src/Vision/DFK51BG02H/Task.cpp
+++ src/Vision/DFK51BG02H/Task.cpp
@@ -179,8 +179,6 @@ namespace Vision
       double m_exposure;
       //! Automatic exposure control.
       AutoExposure m_ae;
-      //! True if task is activating.
-      bool m_activating;
 
       Task(const std::string& name, Tasks::Context& ctx):
         Tasks::Task(name, ctx),
@@ -189,8 +187,7 @@ namespace Vision
         m_kalive(0.5),
         m_log_dir(ctx.dir_log),
         m_debayer(BayerDecoder::TILE_GBRG),
-        m_white(c_width, c_height),
-        m_activating(false)
+        m_white(c_width, c_height)
       {
         // Retrieve configuration values.
         paramActive(Tasks::Parameter::SCOPE_MANEUVER,
@@ -408,7 +405,7 @@ namespace Vision
       void
       consume(const IMC::LoggingControl* msg)
       {
-        if (!m_activating && (msg->getDestination() != getSystemId()))
+        if (!isActivating() && (msg->getDestination() != getSystemId()))
           return;
 
         if (msg->op == IMC::LoggingControl::COP_CURRENT_NAME)
@@ -421,7 +418,6 @@ namespace Vision
       void
       onRequestActivation(void)
       {
-        m_activating = true;
         IMC::LoggingControl log_ctl;
         log_ctl.op = IMC::LoggingControl::COP_REQUEST_CURRENT_NAME;
         dispatch(log_ctl);
@@ -430,7 +426,6 @@ namespace Vision
       void
       onActivation(void)
       {
-        m_activating = false;
         m_log_dir.create();
         setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
       }
