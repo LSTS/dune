a7059f542c4f592d90007b90872de3aa685a98f0
Pedro Calado <pdcalado@gmail.com>
2014-08-05 17:00:10 +0100

    Maneuver/FollowSystem: Handling path_ref.

--- src/Maneuver/FollowSystem/Task.cpp
+++ src/Maneuver/FollowSystem/Task.cpp
@@ -281,6 +281,9 @@ namespace Maneuver
       void
       consume(const IMC::PathControlState* pcs)
       {
+        if (!checkPathReference(pcs))
+          return;
+
         if (pcs->flags & IMC::PathControlState::FL_NEAR)
           enableMovement(false);
       }
