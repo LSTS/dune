730b3969c1440da66f2ed8680edbf66234027b5e
Pedro Calado <pdcalado@gmail.com>
2014-08-05 17:00:23 +0100

    Maneuver/Goto: Handling path_ref.

--- src/Maneuver/Goto/Task.cpp
+++ src/Maneuver/Goto/Task.cpp
@@ -64,6 +64,9 @@ namespace Maneuver
       void
       consume(const IMC::PathControlState* pcs)
       {
+        if (!checkPathReference(pcs))
+          return;
+
         if (pcs->flags & IMC::PathControlState::FL_NEAR)
           signalCompletion();
         else
