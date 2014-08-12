08c0ed3824c19b918adf6355e917a4d3687609d4
Pedro Calado <pdcalado@gmail.com>
2014-08-12 16:05:07 +0100

    Maneuver/Multiplexer: Added header file to keep constants.

---
+++ src/Maneuver/Multiplexer/Constants.hpp
@@ -0,0 +1,44 @@
+//***************************************************************************
+// Copyright 2007-2014 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
+//                                                                          *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://www.lsts.pt/dune/licence.                                        *
+//***************************************************************************
+// Author: Pedro Calado                                                     *
+//***************************************************************************
+
+#ifndef MANEUVER_MULTIPLEXER_CONSTANTS_HPP_INCLUDED_
+#define MANEUVER_MULTIPLEXER_CONSTANTS_HPP_INCLUDED_
+
+namespace Maneuver
+{
+  namespace Multiplexer
+  {
+    //! Number of samples for vertical monitor moving average in elevator
+    static const unsigned c_vsamples = 10;
+    //! Value to be consider at surface
+    static const float c_depth_tol = 0.3f;
+    //! Minimum radius for the elevators
+    static const float c_min_elev_radius = 10.0f;
+  }
+}
+
+#endif
