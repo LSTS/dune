29c679b4bb80b053b53517319945234682ba75d0
Ricardo Martins <rasm@lsts.pt>
2014-08-12 11:00:25 +0100

    DUNE/Utils: Added endsWith() member function to String class.

--- src/DUNE/Utils/String.cpp
+++ src/DUNE/Utils/String.cpp
@@ -348,5 +348,14 @@ namespace DUNE
 
       return true;
     }
+
+    bool
+    String::endsWith(const std::string& str, const std::string& suffix)
+    {
+      if (str.size() < suffix.size())
+        return false;
+
+      return str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+    }
   }
 }
