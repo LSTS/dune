cdf75f4799ee01602d03cd4dcd159ddca41fba5a
Pedro Calado <pdcalado@gmail.com>
2014-09-03 18:57:19 +0100

    Monitors/FuelLevel: Reading Battery Capacity from General section.

--- src/Monitors/FuelLevel/Task.cpp
+++ src/Monitors/FuelLevel/Task.cpp
@@ -99,12 +99,6 @@ namespace Monitors
           .description("Entity label of the " + c_measure_names[i] + " measures.");
         }
 
-        param("Batteries Energy Capacity", m_args.filter_args.full_capacity)
-        .defaultValue("498.8")
-        .minimumValue("0.0")
-        .units(Units::WattHour)
-        .description("Energy capacity of the batteries advertised by manufacturer");
-
         param("Minimum Samples For Estimate", m_args.filter_args.min_samples)
         .defaultValue("20")
         .minimumValue("0")
@@ -186,6 +180,8 @@ namespace Monitors
         .defaultValue("")
         .units(Units::Watt)
         .description("List of estimated power consumed by the entities");
+
+        m_ctx.config.get("General", "Battery Capacity", "700.0", m_args.filter_args.full_capacity);
 
         // Register listeners.
         bind<IMC::Voltage>(this);
