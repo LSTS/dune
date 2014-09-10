5eb870add80996e37b6f945d1bdab7e649f57dfe
Tiago Rodrigues <tiago.carvalho.rodrigues@gmail.com>
2014-09-10 19:02:51 +0100

    Sensors/MetrecX: Recieving and dispatching sensor values. Missing repeated sensors check.

--- src/Sensors/MetrecX/Task.cpp
+++ src/Sensors/MetrecX/Task.cpp
@@ -27,7 +27,6 @@
 
 // DUNE headers.
 #include <DUNE/DUNE.hpp>
-#include "Functions.hpp"
 
 
 namespace Sensors
@@ -40,13 +39,21 @@ namespace Sensors
 
      struct Arguments
      {
-      //! Serial port device.
-      std::string uart_dev;
-      //! Serial port baud rate.
-      unsigned uart_baud;
-      //! Input timeout.
-      double input_timeout;
+       //! Serial port device.
+       std::string uart_dev;
+       //! Serial port baud rate.
+       unsigned uart_baud;
+       //! Input timeout.
+       double input_timeout;
      };
+
+    struct Analog_Labels
+    {
+      //! Number of channels
+      int n_channels;
+      //! Channels Label
+      std::string label;
+    };
 
      struct Sensor
      {
@@ -64,8 +71,10 @@ namespace Sensors
       SerialPort* m_uart;
       //! Task arguments
       Arguments m_args;
+      //! Analog Labels Structure
+      Analog_Labels a_label[4];
       //! Sensor structure
-      Sensor s_config[8];
+      Sensor s_config[20];
       //! Watchdog.
       Counter<double> m_wdog;
       //! Template detector for SV
@@ -92,6 +101,25 @@ namespace Sensors
       int S_D_flag;
       //! SV flag calculator
       int SV_flag;
+      //! dBar to m coefficient
+      float dbar2m_coef;
+      //! Temperature message.
+      IMC::Temperature m_temp;
+      //! Pressure message.
+      IMC::Pressure m_press;
+      //! Depth message
+      IMC::Depth m_depth;
+      //! Sound speed message.
+      IMC::SoundSpeed m_sspeed;
+      //! Conductivity message
+      IMC::Conductivity m_conduct;
+      //! Turbidity message
+      // IMC::Turbidity m_turb;
+      //! WaterDensity message
+      IMC::WaterDensity m_wdens;
+      //! Salinity message
+      IMC::Salinity m_sal;
+
 
       //! Constructor.
       //! @param[in] name task name.
@@ -114,10 +142,30 @@ namespace Sensors
         .units(Units::Second)
         .description("Amount of seconds to wait for data before reporting an error");
 
+        param("Number Channels", a_label[0].n_channels)
+        .defaultValue("2.0")
+        .description("Number of analog channels connected");
+
+        param("Analog C61 Label", a_label[0].label)
+        .defaultValue("N/A")
+        .description("Analog C61 Label indicating sensor type");
+
+        param("Analog C62 Label", a_label[1].label)
+        .defaultValue("N/A")
+        .description("Analog C62 Label indicating sensor type");
+
+        param("Analog C71 Label", a_label[2].label)
+        .defaultValue("N/A")
+        .description("Analog C71 Label indicating sensor type");
+
+        param("Analog C72 Label", a_label[3].label)
+        .defaultValue("N/A")
+        .description("Analog C72 Label indicating sensor type");
+
         strcpy(P_SV,"SV.X");
         strcpy(P_C,"C.X");
-        strcpy(S_U_P,"T.X");
-        strcpy(S_U_T,"P.X");
+        strcpy(S_U_P,"P.X");
+        strcpy(S_U_T,"T.X");
         strcpy(S_L_Tu, "TU.X");
         P_SV_flag = 0;
         P_C_flag = 0;
@@ -126,6 +174,7 @@ namespace Sensors
         S_L_Tu_flag = 0;
         S_D_flag = 0;
         SV_flag = 0;
+        dbar2m_coef = 1.019716;
       }
 
       //! Update internal state with new parameter values.
@@ -156,7 +205,7 @@ namespace Sensors
         m_uart->flush();
       }
 
-      //! Release resources.
+       //! Release resources.
       void
       onResourceRelease(void)
       {
@@ -164,9 +213,10 @@ namespace Sensors
       }
 
       void
-      getAnalogName()
+      getAnalogState(char bfr[],int &flag)
       {
-        printf("getAnalogName!!!\n");
+        if(strstr(bfr,"empty") == NULL)
+          flag++;
       }
 
 
@@ -207,22 +257,20 @@ namespace Sensors
           strcpy(s_config[i].name, S_L_Tu);
           s_config[i].channel = bfr[0] - '0';
           i++;
-        }
+          }
         if(strstr(bfr,"empty") != NULL)
           k++;
-
-        std::cout<<"Name - "<<s_config[i-1].name<<" | "<<"Channel - "<<s_config[i-1].channel<<" | "<<"i+k - "<<(i+k)<<std::endl;
       }
 
       void
-      SensorConfigDetect()
+      SensorConfigDetect(int &i,int &k)
       {
         char bfr[255];
-        int i;
-        int k;
+        int flag;
+        int j;
 
-        i = 0;
-        k = 0;
+        j = 0;
+        flag = 0;
 
         while(0 != std::strcmp(bfr,"Detection complete\r\n"))
         {
@@ -233,26 +281,41 @@ namespace Sensors
           m_uart->readString(bfr,sizeof(bfr));
 
           //! Avoid to send garbage to functions
-          if(0 != strcmp(bfr,"Detecting Sensors"))
+          if(0 != strcmp(bfr,"Detecting Sensors\r\n") && 0 != strcmp(bfr,"Detection complete\r\n"))
           {
+            //! Function to detect analog sensors configuration
+            if((i+k)>=5)
+              getAnalogState(bfr,flag);
+
             //! Function to detect digital sensors configuration
             if((i+k)<5)
               getDigName(bfr,i,k);
-            //! Function to detect analog sensors configuration
-            if(i>=5)
-              getAnalogName();
           }
         }
 
-            if( (P_SV_flag == 1 || P_C_flag == 1) && S_U_P_flag == 1 && S_U_T_flag == 1)
-              S_D_flag = 1;
-            else
-              S_D_flag = 0;
+        //! There is at least one analog channel active if flag>0
+        //! Possibility for future analog auto-configuration (talk Mode)
 
-            if(P_C_flag == 1  && S_U_P_flag == 1 && S_U_T_flag == 1)
-              SV_flag = 1;
-            else
-              SV_flag = 0;
+        //! Passing Sensor names to the structure
+        for(j = 0;j<a_label[0].n_channels;j++ )
+        {
+          strcpy(s_config[i+j].name,a_label[j].label.c_str());
+          s_config[i+j].channel = j + 6;
+        }
+
+        //! Indicating total number of analog and digital channels
+        //! To alocate Density, Salinity and SV calculation at the end
+        i = i + a_label[0].n_channels;
+
+       if( (P_SV_flag == 1 || P_C_flag == 1) && S_U_P_flag == 1 && S_U_T_flag == 1)
+         S_D_flag = 1;
+       else
+         S_D_flag = 0;
+
+       if(P_C_flag == 1  && S_U_P_flag == 1 && S_U_T_flag == 1)
+         SV_flag = 1;
+       else
+         SV_flag = 0;
       }
 
       bool
@@ -267,7 +330,7 @@ namespace Sensors
           m_uart->readString(bfr, sizeof(bfr));printf("Check reply - %s\n",bfr);
           if (std::strcmp(bfr, reply) == 0)
             return true;
-            }
+        }
 
         return false;
         }
@@ -275,6 +338,12 @@ namespace Sensors
       void
       onResourceInitialization(void)
       {
+        int i;
+        int k;
+
+        i = 0;
+        k = 0;
+
         m_uart->writeString("\r");
         Delay::wait(1.0);
         m_uart->flush();
@@ -285,21 +354,21 @@ namespace Sensors
         if(!sendCommand("DETECT\r",">DETECT\r\n"))
           throw RestartNeeded(DTR("failed to detect sensor configuration"), 5);
         else
-          SensorConfigDetect();
+          SensorConfigDetect(i,k);
 
         if(S_D_flag == 1)
         {
           if(!sendCommand("SET SCAN DENSITY\r",">SET SCAN DENSITY\r\n"))
              throw RestartNeeded(DTR("failed to set density calculation"), 5);
 
-          strcpy(s_config[5].name,"Density");
-          s_config[5].channel = 5;
+          strcpy(s_config[i].name,"Density");
+          s_config[i].channel = i;
 
           if(!sendCommand("SET SCAN SALINITY\r",">SET SCAN SALINITY\r\n"))
              throw RestartNeeded(DTR("failed to set salinity calculation"), 5);
 
-          strcpy(s_config[6].name,"Salinity");
-          s_config[6].channel = 6;
+          strcpy(s_config[i+1].name,"Salinity");
+          s_config[i+1].channel = i+1;
         }
         else if(S_D_flag == 0)
         {
@@ -315,8 +384,8 @@ namespace Sensors
           if(!sendCommand("SET SCAN SV\r",">SET SCAN SV\r\n"))
              throw RestartNeeded(DTR("failed to set sv calculation"), 5);
 
-          strcpy(s_config[7].name,"SV");
-          s_config[7].channel = 7;
+          strcpy(s_config[i+2].name,"SV");
+          s_config[i+2].channel = i+2;
         }
         else if(SV_flag == 0)
         {
@@ -339,9 +408,9 @@ namespace Sensors
       {
         char bfr[255];
         char *ptr;
-        float vals[8];
         int n_data;
         int n;
+        int i;
 
         while (!stopping())
         {
@@ -362,14 +431,47 @@ namespace Sensors
           ptr = bfr;
           n_data = 0;
           n = 0;
-          while( 1 == sscanf(ptr,"%f%n",&vals[n_data],&n) )
-          {//std::cout<<"While - "<<vals[n_data]<<" | "<<n_data<<std::endl;
+          //! Extract reveived values to structure
+          while( 1 == sscanf(ptr,"%f%n",&s_config[n_data].value,&n) )
+          {
             n_data++;
             ptr +=n;
           }
 
-          for(int i = 0; i<n_data; i++)
+          //! Dispatch of received values
+          for(i = 0; i<n_data; i++)
+          {
+            if(0 == strcmp(s_config[i].name,P_SV) || 0 == strcmp(s_config[i].name,"SV"))
+              m_sspeed.value = s_config[i].value;
+
+            if(0 == strcmp(s_config[i].name,P_C))
+              m_conduct.value = s_config[i].value;
+
+           if(0 == strcmp(s_config[i].name,S_U_T) || 0 == strcmp(s_config[i].name,"Temperature"))
+              m_temp.value = s_config[i].value;
+
+           if(0 == strcmp(s_config[i].name,S_U_P) || 0 == strcmp(s_config[i].name,"Pressure"))
+              m_press.value = s_config[i].value;
+
+          if(0 == strcmp(s_config[i].name,S_U_P) || 0 == strcmp(s_config[i].name,"Pressure"))
+              m_depth.value = s_config[i].value * dbar2m_coef;
+
+            if(0 == strcmp(s_config[i].name, "Salinity"))
+              m_sal.value = s_config[i].value;
+
+           if(0 == strcmp(s_config[i].name, "Density"))
+              m_wdens.value = s_config[i].value;
+
             std::cout<<"Name - "<<s_config[i].name<<" | "<<"Channel - "<<s_config[i].channel<<std::endl;
+          }
+
+          //! Dispatch
+          dispatch(m_sspeed);
+          dispatch(m_conduct);
+          dispatch(m_temp);
+          dispatch(m_press);
+          dispatch(m_sal);
+          dispatch(m_wdens);
 
           if (rv == 0)
             throw RestartNeeded(DTR("I/O error"), 5);
