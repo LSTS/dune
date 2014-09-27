36ab1999eb15bf908cdbb75f30dfcd9ecbeda3d0
Ricardo Martins <rasm@lsts.pt>
2014-09-27 18:02:33 +0100

    Programs: dune-create-params.py: Print configuration being used to run DUNE.

--- programs/scripts/dune-create-params.py
+++ programs/scripts/dune-create-params.py
@@ -96,8 +96,9 @@ for i18n in sint:
         os.environ['LC_ALL'] = lang
         os.environ['LANG'] = lang
         os.environ['LANGUAGE'] = lang
+        sys.stderr.write('\n* Processing %s:\n\n' % ini)
+        sys.stderr.flush()
         subprocess.check_call(['./dune', '-c', ini, '-p', 'Hardware', '-X', dst_dir])
-
 
 xmls = glob.glob(os.path.join(dst_dir, '*.xml'))
 for xml in xmls:
