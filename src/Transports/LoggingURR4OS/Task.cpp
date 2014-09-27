3005156c62d564d0fdbbc10934356f0a14edddf7
Pedro Calado <pdcalado@gmail.com>
2014-09-27 07:30:20 +0100

    Transports/LoggingURR4OS: Modified task to log file at runtime.

--- src/Transports/LoggingURR4OS/Task.cpp
+++ src/Transports/LoggingURR4OS/Task.cpp
@@ -41,44 +41,100 @@ namespace Transports
   {
     using DUNE_NAMESPACES;
 
+    //! Maximum temperature delay
+    const float c_msg_delay = 5.0f;
+    //! Comment char
+    const std::string c_comment = "% ";
+    //! Not valid value
+    const std::string c_not_valid = "-1";
+
+    struct MetaData
+    {
+      std::string vname;
+      std::string sname;
+      std::string type;
+      bool raw;
+    };
+
     struct Arguments
     {
       //! Program path
       std::string prog;
       //! Output file name
       std::string outfile;
+      //! Rhodamine label
+      std::string rhodamine_label;
+      //! MetaData for header of file
+      MetaData md;
+      //! Log raw data
+      bool log_raw;
+      //! Temperature entity label
+      std::string temp_label;
     };
 
     struct Task: public Tasks::Task
     {
-      // Task arguments.
+      //! Log file.
+      std::ofstream m_log_file;
+      //! Log filename
+      Path m_log_path;
+      //! True if header for this file has been written
+      bool m_wrote_header;
+      //! EstimatedState message
+      IMC::EstimatedState m_estate;
+      //! Temperature entity id
+      unsigned m_temp_id;
+      //! Rhodamine entity id
+      unsigned m_rhod_id;
+      //! Temperature message
+      IMC::Temperature m_temperature;
+      //! Voltage message for raw data
+      IMC::Voltage m_raw;
+      //! Task arguments.
       Arguments m_args;
 
       Task(const std::string& name, Tasks::Context& ctx):
-        Tasks::Task(name, ctx)
+        Tasks::Task(name, ctx),
+        m_wrote_header(false)
       {
-        param("Program Path", m_args.prog)
-        .defaultValue("/opt/lsts/dune/bin/urr4os_format");
+        param("Rhodamine Label", m_args.rhodamine_label)
+        .defaultValue("Rhodamine");
 
-        param("Output File Name", m_args.outfile)
-        .defaultValue("Bio.csv");
+        param("Sensor Name", m_args.md.sname)
+        .defaultValue("Cyclops7");
+
+        param("Sensor Type", m_args.md.type)
+        .defaultValue("Rhodamine");
+
+        param("Log raw data", m_args.log_raw)
+        .defaultValue("true");
+
+        param("Temperature Label", m_args.temp_label)
+        .defaultValue("CTD");
 
         bind<IMC::LoggingControl>(this);
+        bind<IMC::EstimatedState>(this);
+        bind<IMC::Temperature>(this);
+        bind<IMC::Voltage>(this);
+        bind<IMC::RhodamineDye>(this);
+      }
+
+      void
+      onUpdateParameters(void)
+      {
+        m_args.md.vname = getSystemName();
+      }
+
+      void
+      onEntityResolution(void)
+      {
+        m_rhod_id = resolveEntity(m_args.rhodamine_label);
+        m_temp_id = resolveEntity(m_args.temp_label);
       }
 
       void
       onResourceInitialization(void)
       {
-        // check if file exists
-        if (!fileExists(m_args.prog))
-        {
-          err("program not found");
-          // Initialize entity state.
-          setEntityState(IMC::EntityState::ESTA_ERROR, Status::CODE_IDLE);
-          requestDeactivation();
-          return;
-        }
-
         requestActivation();
 
         // Initialize entity state.
@@ -89,47 +145,171 @@ namespace Transports
       onResourceRelease(void)
       { }
 
-      bool
-      fileExists(const std::string& name)
+      void
+      writeHeader(std::ofstream& file, MetaData* m, double timestamp)
       {
-        if (FILE *file = fopen(name.c_str(), "r"))
+        file << c_comment
+             << m->vname << ", "
+             << m->sname << ", "
+             << m->type << ", "
+             << "1 Hz" << std::endl;
+
+        std::string date_safe = Time::Format::getDateSafe(timestamp);
+        std::string time_safe = Time::Format::getTimeSafe(timestamp);
+
+        file << c_comment
+             << date_safe[6] << date_safe[7] << "/"
+             << date_safe[4] << date_safe[5] << "/"
+             << date_safe[0] << date_safe[1] << date_safe[2] << date_safe[3]
+             << " "
+             << time_safe[0] << time_safe[1] << ":" << time_safe[2] << time_safe[3]
+             << std::endl;
+
+        file << c_comment << c_not_valid << std::endl;
+
+        file << c_comment
+             << "Time (seconds), Latitude (degrees), Longitude (degrees),"
+             << "Depth (meters), " << m->type << " (ppb),"
+             << m->type << " (raw), Temperature (Celsius)" << std::endl;
+      }
+
+      void
+      writeLine(std::ofstream& file, double timestamp, const IMC::EstimatedState* state,
+                const IMC::RhodamineDye* rdye, const IMC::Voltage* raw,
+                const IMC::Temperature* temp = NULL)
+      {
+        double lat;
+        double lon;
+        Coordinates::toWGS84(*state, lat, lon);
+
+        std::string t_str = "";
+        std::stringstream ss;
+        if (temp != NULL)
         {
-          fclose(file);
-          return true;
+          if (timestamp - temp->getTimeStamp() < c_msg_delay)
+          {
+            ss << std::setprecision(3) << "," << temp->value;
+            t_str = ss.str();
+          }
+        }
+
+        file << std::fixed << std::setprecision(2)
+             << timestamp << ","
+             << std::setprecision(5)
+             << Math::Angles::degrees(lat) << ","
+             << Math::Angles::degrees(lon) << ","
+             << std::setprecision(2)
+             << state->depth;
+
+        if (rdye != NULL)
+          file << "," << std::setprecision(3) << rdye->value;
+        else
+          file << "-1";
+
+        if (timestamp - raw->getTimeStamp() < c_msg_delay)
+        {
+          ss << "," << std::setprecision(3) << raw->value;
+          t_str = ss.str();
         }
         else
         {
-          return false;
+          ss << ",-1";
+        }
+
+        file << t_str;
+        file << std::endl;
+      }
+
+      void
+      openLog(const Path& path)
+      {
+        if (path == m_log_path)
+          return;
+
+        closeLog();
+
+        m_wrote_header = false;
+
+        m_log_path = path;
+        m_log_file.open(m_log_path.c_str(), std::ofstream::app);
+        debug("opening %s", m_log_path.c_str());
+      }
+
+      void
+      closeLog(void)
+      {
+        if (m_log_file.is_open())
+        {
+          m_log_file.close();
+          int64_t size = m_log_path.size();
+          if (size == 0)
+          {
+            debug("removing empty log '%s'", m_log_path.c_str());
+            m_log_path.remove();
+          }
+          m_log_path = Path();
         }
       }
 
       void
       consume(const IMC::LoggingControl* msg)
       {
+        if (msg->getSource() != getSystemId())
+          return;
+
+        switch (msg->op)
+        {
+          case IMC::LoggingControl::COP_STARTED:
+          case IMC::LoggingControl::COP_CURRENT_NAME:
+            openLog(m_ctx.dir_log / msg->name / "Bio.csv");
+            break;
+
+          case IMC::LoggingControl::COP_STOPPED:
+            closeLog();
+            break;
+        }
+      }
+
+      void
+      consume(const IMC::EstimatedState* msg)
+      {
+        if (msg->getSource() != getSystemId())
+          return;
+
         if (!isActive())
           return;
 
-        if (msg->op != IMC::LoggingControl::COP_STOPPED)
+        m_estate = *msg;
+      }
+
+      void
+      consume(const IMC::RhodamineDye* msg)
+      {
+        if (msg->getSource() != getSystemId())
           return;
 
-        std::string ofname = m_args.outfile;
+        if (!m_wrote_header)
+        {
+          writeHeader(m_log_file, &m_args.md, msg->getTimeStamp());
+          m_wrote_header = true;
+        }
 
-        std::stringstream cmd;
-        cmd << m_args.prog << " "
-            << "-s Cyclops7" << " "
-            << "-d Rhodamine" << " "
-            << "-o " << "\"" << m_ctx.dir_log / msg->name / m_args.outfile << "\" "
-            << "\"" << m_ctx.dir_log / msg->name << "/Data.lsf.gz\""
-            << " 2>/dev/null";
+        writeLine(m_log_file, msg->getTimeStamp(), &m_estate,
+                  msg, (m_args.log_raw) ? &m_raw : NULL, &m_temperature);
+      }
 
-        inf("running command %s", cmd.str().c_str());
+      void
+      consume(const IMC::Voltage* msg)
+      {
+        if (msg->getSourceEntity() == m_rhod_id)
+          m_raw = *msg;
+      }
 
-        int rc = std::system(cmd.str().c_str());
-
-        if (rc == 0)
-          inf("saved file: %s/%s", msg->name.c_str(), m_args.outfile.c_str());
-        else
-          err("failed to run the command");
+      void
+      consume(const IMC::Temperature* msg)
+      {
+        if (msg->getSourceEntity() == m_temp_id)
+          m_temperature = *msg;
       }
 
       void
