9afa8e8054bd1af375c28f6c86400ff7ea09077e
Ricardo Martins <rasm@lsts.pt>
2014-10-09 15:44:49 +0100

    Vision/Lumenera: Added support for LED GPIO.

--- src/Vision/Lumenera/Task.cpp
+++ src/Vision/Lumenera/Task.cpp
@@ -85,6 +85,8 @@ namespace Vision
       unsigned volume_size;
       //! Power GPIO.
       int pwr_gpio;
+      //! LED GPIO.
+      int led_gpio;
       //! Whether to configure the camera.
       bool camera_cfg;
       //! Whether to capture from the camera.
@@ -116,6 +118,8 @@ namespace Vision
       double m_timestamp;
       //! Power GPIO.
       Hardware::GPIO* m_pwr_gpio;
+      //! LEDs GPIO.
+      Hardware::GPIO* m_led_gpio;
       //! Config is dirty.
       bool m_cfg_dirty;
       //! Slave entities
@@ -133,6 +137,7 @@ namespace Vision
         m_volume_count(0),
         m_file_count(0),
         m_pwr_gpio(NULL),
+        m_led_gpio(NULL),
         m_cfg_dirty(false),
         m_slave_entities(NULL),
         m_log_dir_updated(false)
@@ -144,6 +149,10 @@ namespace Vision
         param("Power GPIO", m_args.pwr_gpio)
         .defaultValue("-1")
         .description("GPIO that controls power to the camera");
+
+        param("LED GPIO", m_args.led_gpio)
+        .defaultValue("-1")
+        .description("GPIO that controls power to the LEDs");
 
         param("Camera IPv4 Address", m_args.address)
         .defaultValue("10.0.10.82")
@@ -309,6 +318,13 @@ namespace Vision
           m_pwr_gpio->setValue(0);
         }
 
+        if (m_args.led_gpio > 0)
+        {
+          m_led_gpio = new Hardware::GPIO(m_args.led_gpio);
+          m_led_gpio->setDirection(Hardware::GPIO::GPIO_DIR_OUTPUT);
+          m_led_gpio->setValue(0);
+        }
+
         m_slave_entities = new EntityActivationMaster(this);
         updateSlaveEntities();
         setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_IDLE);
@@ -324,6 +340,12 @@ namespace Vision
         {
           delete m_pwr_gpio;
           m_pwr_gpio = NULL;
+        }
+
+        if (m_led_gpio != NULL)
+        {
+          delete m_led_gpio;
+          m_led_gpio = NULL;
         }
 
         if (m_slave_entities != NULL)
@@ -530,6 +552,9 @@ namespace Vision
           }
         }
 
+        if (m_led_gpio != NULL)
+          m_led_gpio->setValue(1);
+
         inf(DTR("started video stream"));
       }
 
@@ -542,6 +567,9 @@ namespace Vision
         debug("stopping video stream");
         delete m_http;
         m_http = NULL;
+
+        if (m_led_gpio != NULL)
+          m_led_gpio->setValue(0);
       }
 
       void
