d10d9395be8916215b63283f1790f6e395ee83fb
Ricardo Bencatel <ricardo.bencatel@fe.up.pt>
2014-10-13 18:47:24 -0400

    DUNE/Tasks: In class SourceFilter, correction of the filter input handling.

--- src/DUNE/Tasks/SourceFilter.cpp
+++ src/DUNE/Tasks/SourceFilter.cpp
@@ -103,88 +103,130 @@ namespace DUNE
       //! Process the systems and entities allowed to pass the message
       //==========================================
       uint32_t i_src;
+      uint32_t i_src_ini;
+      std::vector<std::string> parts;
+      std::vector<std::string> systems;
+      std::vector<std::string> entities;
+      unsigned int i_sys_n;
+      unsigned int i_ent_n;
+      uint32_t sys_tmp;
+
       m_filtered_sys.clear();
       m_filtered_ent.clear();
       for (unsigned int i = 0; i < src.size(); ++i)
       {
-        std::vector<std::string> parts;
+        parts.clear();
         Utils::String::split(src[i], ":", parts);
         if (parts.size() < 1)
           continue;
 
         // Split systems and entities.
-        std::vector<std::string> systems;
+        systems.clear();
+        entities.clear();
         Utils::String::split(parts[0], "+", systems);
-        std::vector<std::string> entities;
         Utils::String::split(parts[1], "+", entities);
+        i_sys_n = systems.size();
+        i_ent_n = entities.size();
 
-        m_filtered_ent.resize(systems.size()*entities.size());
-        m_filtered_sys.resize(systems.size()*entities.size());
-        unsigned int i_sys_n = systems.size();
-        unsigned int i_ent_n = entities.size();
-        // Resolve systems id.
+        i_src_ini = m_filtered_sys.size();
+        m_filtered_ent.resize(i_src_ini + i_sys_n*i_ent_n);
+        m_filtered_sys.resize(i_src_ini + i_sys_n*i_ent_n);
         for (unsigned j = 0; j < i_sys_n; j++)
         {
-          // Resolve entities id.
-          for (unsigned k = 0; k < i_ent_n; k++)
+          m_task.spew("%s filtering - System '%s' (%u/%u).", m_msg_name.c_str(),
+               systems[j].c_str(), j+1, i_sys_n);
+
+          // Resolve system id.
+          if (systems[j].empty())
           {
-            i_src = (j+1)*(k+1)-1;
-            // Resolve systems.
-            if (systems[j].empty())
+            sys_tmp = UINT_MAX;
+            m_task.debug("%s filtering - Filter source system undefined", m_msg_name.c_str());
+          }
+          else if (systems[j].compare("self") == 0)
+          {
+            sys_tmp = m_task.getSystemId();
+            m_task.debug("%s filtering - System '%s' with ID: %u", m_msg_name.c_str(),
+                         m_task.resolveSystemId(sys_tmp), sys_tmp);
+          }
+          else
+          {
+            try
             {
-              m_filtered_sys[i_src] = UINT_MAX;
-              m_task.debug("%s filtering - Filter source system undefined", m_msg_name.c_str());
-            }
-            else
-            {
-              if (systems[j].compare("self") == 0)
-              {
-                m_filtered_sys[i_src] = m_task.getSystemId();
-                m_task.debug("%s filtering - System '%s' with ID: %d", m_msg_name.c_str(),
-                             m_task.resolveSystemId(m_filtered_sys[i_src]), m_filtered_sys[i_src]);
-              }
+              sys_tmp = m_task.resolveSystemName(systems[j]);
+              if (sys_tmp != USHRT_MAX)
+                m_task.debug("%s filtering - System '%s' with ID: %u", m_msg_name.c_str(),
+                             m_task.resolveSystemId(sys_tmp), sys_tmp);
               else
               {
-                try
-                {
-                  m_filtered_sys[i_src] = m_task.resolveSystemName(systems[j]);
-                  m_task.debug("%s filtering - System '%s' with ID: %d", m_msg_name.c_str(),
-                               m_task.resolveSystemId(m_filtered_sys[i_src]), m_filtered_sys[i_src]);
-                }
-                catch (...)
-                {
-                  m_task.war("%s filtering - No system found with designation '%s'!",
-                             m_msg_name.c_str(), systems[j].c_str());
-                  i_sys_n--;
-                  j--;
-                }
+                m_task.war("%s filtering - No system found with designation '%s'!", m_msg_name.c_str(),
+                           systems[j].c_str());
+                for (unsigned j_tmp = j; j_tmp+1 < i_sys_n; j_tmp++)
+                  systems[j_tmp] = systems[j_tmp + 1];
+                i_sys_n--;
+                j--;
+                continue;
               }
             }
-            // Resolve entities.
-            if (entities[j].empty())
+            catch (...)
             {
-              m_filtered_ent[i_src] = UINT_MAX;
+              m_task.war("%s filtering - No system found with designation '%s'!",
+                         m_msg_name.c_str(), systems[j].c_str());
+              for (unsigned j_tmp = j; j_tmp+1 < i_ent_n; j_tmp++)
+                systems[j_tmp] = systems[j_tmp + 1];
+              i_sys_n--;
+              j--;
+              continue;
+            }
+          }
+
+          for (unsigned k = 0; k < i_ent_n; k++)
+          {
+            m_task.spew("%s filtering - Entity '%s' (%u/%u).", m_msg_name.c_str(),
+                        entities[k].c_str(), k+1, i_ent_n);
+
+            i_src = j*i_ent_n + k;
+
+            // Assign system id
+            m_filtered_sys[i_src_ini + i_src] = sys_tmp;
+
+            // Resolve entity id.
+            if (entities[k].empty())
+            {
+              m_filtered_ent[i_src_ini + i_src] = UINT_MAX;
               m_task.debug("%s filtering - Filter entity system undefined", m_msg_name.c_str());
             }
             else
             {
               try
               {
-                m_filtered_ent[i_src] = m_task.resolveEntity(entities[k]);
-                m_task.debug("%s filtering - Entity '%s' with ID: %d", m_msg_name.c_str(),
-                             m_task.resolveEntity(m_filtered_ent[i_src]).c_str(), m_filtered_ent[i_src]);
+                m_filtered_ent[i_src_ini + i_src] = m_task.resolveEntity(entities[k]);
+                m_task.debug("%s filtering - Entity '%s' with ID: %u", m_msg_name.c_str(),
+                             m_task.resolveEntity(m_filtered_ent[i_src_ini + i_src]).c_str(),
+                             m_filtered_ent[i_src_ini + i_src]);
               }
               catch (...)
               {
                 m_task.war("%s filtering - No entity found with designation '%s'!",
                            m_msg_name.c_str(), entities[k].c_str());
+                for (unsigned k_tmp = k; k_tmp+1 < i_ent_n; k_tmp++)
+                  entities[k_tmp] = entities[k_tmp + 1];
                 i_ent_n--;
                 k--;
               }
             }
+
+            m_task.spew("%s filtering state - System %u of %u : Entity %u of %u.",
+                        m_msg_name.c_str(), j+1, i_sys_n, k+1, i_ent_n);
           }
         }
+        m_filtered_ent.resize(i_src_ini + i_sys_n*i_ent_n);
+        m_filtered_sys.resize(i_src_ini + i_sys_n*i_ent_n);
       }
+      m_task.spew("%s filter sets:", m_msg_name.c_str());
+      for (unsigned i = 0; i < i_src_ini + i_sys_n*i_ent_n; i++)
+        m_task.spew("     System %8.s : Entity %8.s.",
+                    m_task.resolveSystemId(m_filtered_sys[i]),
+                    m_task.resolveEntity(m_filtered_ent[i]).c_str());
     }
   }
 }
