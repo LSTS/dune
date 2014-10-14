6a1938999a015626dafb1ff167af48d4e15767eb
Ricardo Bencatel <ricardo.bencatel@fe.up.pt>
2014-10-14 16:38:20 -0400

    DUNE/Tasks: in class SourceFilter, addition of filtering methods for control command messages.

--- src/DUNE/Tasks/SourceFilter.hpp
+++ src/DUNE/Tasks/SourceFilter.hpp
@@ -50,51 +50,82 @@ namespace DUNE
       SourceFilter(void);
       */
 
+      //! Construct a task command input filter object.
+      //! @param[in] task - filter parent task.
+      //! @param[in] source - desired command sources system and entity names.
+      SourceFilter(Tasks::Task& task, const std::vector<std::string>& source);
+
       //! Construct a task input filter object.
       //! @param[in] task - filter parent task.
       //! @param[in] source - desired sources system and entity names.
       //! @param[in] msg_name - filtered message name.
       SourceFilter(Tasks::Task& task, const std::vector<std::string>& source, const std::string msg_name);
 
-      /*
-      //! Construct a task input filter object.
-      //! @param[in] task - filter parent task.
-      //! @param[in] source - desired sources system and entity names.
-      //! @param[in] msg_name - filtered message name.
-      SourceFilter(std::string name, Tasks::Context& ctx, const std::vector<std::string>* source, const std::string msg_name);
-      */
-
-      /*
       //! Destructor.
-      ~SourceFilter(void)
-      {
-      }
-      */
+      ~SourceFilter(void);
 
       //! Verification of the message source system and entity.
       //! @param[in] msg - input message.
-      //! @param[out] match with the filter system and entity list.
+      //! @param[out] match - 'True' if the message source is allowed to pass it.
       bool
       match(const IMC::Message* msg);
 
     private:
+      //! Define the types Systems and Entities for the respective ids
+      typedef std::vector<uint32_t> Systems;
+      typedef std::vector<uint32_t> Entities;
+
+      //! Define the desired message source systems and entities.
+      //! @param[in] source - desired sources system and entity names.
+      //! @param[in] filtered_sys - list of systems allowed to pass a message.
+      //! @param[in] filtered_ent - list of entities allowed to pass a message.
+      void
+      defineSystemEntityFilter(const std::vector<std::string>& source,
+          Systems& filtered_sys, Entities& filtered_ent);
 
       //! Define the desired message source systems and entities.
       //! @param[in] source - desired sources system and entity names.
       void
-      defineFilter(const std::vector<std::string>& source);
+      defineCommandSystemEntityFilter(const std::vector<std::string>& source);
+
+      //! Verification of a general message source system and entity.
+      //! @param[in] msg - input message.
+      //! @param[in] filtered_sys - list of systems allowed to pass a message.
+      //! @param[in] filtered_ent - list of entities allowed to pass a message.
+      //! @param[out] match - 'True' if the message source is allowed to pass it.
+      bool
+      matchMessage(const IMC::Message* msg,
+          Systems& filtered_sys, Entities& filtered_ent);
+
+      //! Verification of a command message source system and entity.
+      //! @param[in] msg - input message.
+      //! @param[out] match - 'True' if the message source is allowed to pass it.
+      bool
+      matchCommand(const IMC::Message* msg);
+
+      //! Message rejection print-out.
+      //! @param[in] msg - input message.
+      void
+      printRejected(const IMC::Message* msg);
 
       //! List of systems and entities allowed to pass a message.
-      typedef std::vector<uint32_t> Systems;
-      typedef std::vector<uint32_t> Entities;
       Systems m_filtered_sys;
       Entities m_filtered_ent;
+      //! List of systems and entities allowed to define a command.
+      std::map<uint32_t, Systems> m_cmd_filtered_sys;
+      std::map<uint32_t, Entities> m_cmd_filtered_ent;
 
       // Parent task.
       Tasks::Task& m_task;
 
       // Filtered message name
       std::string m_msg_name;
+
+      // Flag for command messages filter
+      bool m_type_cmd;
+
+      // Command message names
+      std::vector<std::string> m_cmds;
     };
   }
 }
