53da30cc140638f2f24469a353dd70c7ac488468
Ricardo Bencatel <ricardo.bencatel@fe.up.pt>
2014-10-31 13:57:34 -0400

    DUNE/Tasks: In class SourceFilter, code clean-up.

--- src/DUNE/Tasks/SourceFilter.hpp
+++ src/DUNE/Tasks/SourceFilter.hpp
@@ -29,11 +29,13 @@
 #define DUNE_TASKS_SOURCE_FILTER_HPP_INCLUDED_
 
 // ISO C++ 98 headers.
+#include <stdint.h>
 #include <string>
 #include <vector>
 
 // DUNE headers.
-// none
+#include <DUNE/Tasks.hpp>
+#include <DUNE/IMC.hpp>
 
 namespace DUNE
 {
@@ -45,28 +47,23 @@ namespace DUNE
     class SourceFilter
     {
     public:
-      /*
-      //! Construct a task input filter object.
-      SourceFilter(void);
-      */
-
       //! Construct a task command input filter object.
-      //! @param[in] task - filter parent task.
-      //! @param[in] source - desired command sources system and entity names.
+      //! @param[in] task filter parent task.
+      //! @param[in] source desired command sources system and entity names.
       SourceFilter(Tasks::Task& task, const std::vector<std::string>& source);
 
       //! Construct a task input filter object.
-      //! @param[in] task - filter parent task.
-      //! @param[in] source - desired sources system and entity names.
-      //! @param[in] msg_name - filtered message name.
+      //! @param[in] task filter parent task.
+      //! @param[in] source desired sources system and entity names.
+      //! @param[in] msg_name filtered message name.
       SourceFilter(Tasks::Task& task, const std::vector<std::string>& source, const std::string msg_name);
 
       //! Destructor.
-      ~SourceFilter(void);
+      ~SourceFilter(void){ }
 
       //! Verification of the message source system and entity.
-      //! @param[in] msg - input message.
-      //! @param[out] match - 'True' if the message is allowed to pass.
+      //! @param[in] msg input message.
+      //! @return true if the message is allowed to pass.
       bool
       match(const IMC::Message* msg);
 
@@ -76,54 +73,50 @@ namespace DUNE
       typedef std::vector<uint32_t> Entities;
 
       //! Define the desired message source systems and entities.
-      //! @param[in] source - desired sources system and entity names.
-      //! @param[in] filtered_sys - list of systems allowed to pass a message.
-      //! @param[in] filtered_ent - list of entities allowed to pass a message.
+      //! @param[in] source desired sources system and entity names.
+      //! @param[in] filtered_sys list of systems allowed to pass a message.
+      //! @param[in] filtered_ent list of entities allowed to pass a message.
       void
       defineSystemEntityFilter(const std::vector<std::string>& source,
           Systems& filtered_sys, Entities& filtered_ent);
 
       //! Define the desired message source systems and entities.
-      //! @param[in] source - desired sources system and entity names.
+      //! @param[in] source desired sources system and entity names.
       void
       defineCommandSystemEntityFilter(const std::vector<std::string>& source);
 
       //! Verification of a general message source system and entity.
-      //! @param[in] msg - input message.
-      //! @param[in] filtered_sys - list of systems allowed to pass a message.
-      //! @param[in] filtered_ent - list of entities allowed to pass a message.
-      //! @param[out] match - 'True' if the message is allowed to pass.
+      //! @param[in] msg input message.
+      //! @param[in] filtered_sys list of systems allowed to pass a message.
+      //! @param[in] filtered_ent list of entities allowed to pass a message.
+      //! @return true if the message is allowed to pass.
       bool
       matchMessage(const IMC::Message* msg,
           Systems& filtered_sys, Entities& filtered_ent);
 
       //! Verification of a command message source system and entity.
-      //! @param[in] msg - input message.
-      //! @param[out] match - 'True' if the message is allowed to pass.
+      //! @param[in] msg input message.
+      //! @return true if the message is allowed to pass.
       bool
       matchCommand(const IMC::Message* msg);
 
       //! Message rejection print-out.
-      //! @param[in] msg - input message.
+      //! @param[in] msg input message.
       void
       printRejected(const IMC::Message* msg);
 
+      // Parent task.
+      Tasks::Task& m_task;
       //! List of systems and entities allowed to pass a message.
       Systems m_filtered_sys;
       Entities m_filtered_ent;
       //! List of systems and entities allowed to define a command.
       std::map<uint32_t, Systems> m_cmd_filtered_sys;
       std::map<uint32_t, Entities> m_cmd_filtered_ent;
-
-      // Parent task.
-      Tasks::Task& m_task;
-
       // Filtered message name
       std::string m_msg_name;
-
       // Flag for command messages filter
       bool m_type_cmd;
-
       // Command message names
       std::vector<std::string> m_cmds;
     };
