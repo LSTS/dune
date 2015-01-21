96d007e390892430a6d7b3e4e33eff8d0a60814c
Joao Fortuna <fortuna.araujo@gmail.com>
2015-01-21 18:00:28 +0100

    Programs: dune-sendmsg: Added Target.

--- programs/utils/dune-sendmsg.cpp
+++ programs/utils/dune-sendmsg.cpp
@@ -61,7 +61,7 @@ main(int argc, char** argv)
       fprintf(stdout, "  PushEntityParameters, QueryEntityInfo, QueryEntityParameters\n");
       fprintf(stdout, "  RegisterManeuver, RemoteActions, RemoteActionsRequest, ReplayControl, RestartSystem\n");
       fprintf(stdout, "  SaveEntityParameters, SetEntityParameters, SetLedBrightness, SetServoPosition\n");
-      fprintf(stdout, "  SetThrusterActuation, Sms, SoundSpeed, TeleoperationDone, Temperature\n");
+      fprintf(stdout, "  SetThrusterActuation, Sms, SoundSpeed, Target, TeleoperationDone, Temperature\n");
       fprintf(stdout, "  TextMessage, TrexCommand, VehicleCommand, VehicleMedium\n");
       return 1;
     }
@@ -642,6 +642,32 @@ main(int argc, char** argv)
     IMC::SoundSpeed* tmsg = new IMC::SoundSpeed;
     msg = tmsg;
     tmsg->value = atoi(argv[4]);
+  }
+
+  if (strcmp(argv[3], "Target") == 0)
+  {
+    IMC::Target* tmsg = new IMC::Target;
+    msg = tmsg;
+    tmsg->label = "dune-sendmsg";
+    tmsg->lat = Angles::radians(atof(argv[4]));
+    tmsg->lon = Angles::radians(atof(argv[5]));
+    tmsg->z = atof(argv[6]);
+
+    if (argc > 7)
+    {
+      if (!strcmp(argv[7], "DEP") || !strcmp(argv[7], "dep"))
+        tmsg->z_units = IMC::Z_DEPTH;
+      else if (!strcmp(argv[7], "ALT") || !strcmp(argv[7], "alt"))
+        tmsg->z_units = IMC::Z_ALTITUDE;
+      else if (!strcmp(argv[7], "HEI") || !strcmp(argv[7], "hei"))
+        tmsg->z_units = IMC::Z_HEIGHT;
+    }
+
+    if (argc > 9)
+    {
+      tmsg->cog = Angles::normalizeRadian(Angles::radians(atof(argv[8])));
+      tmsg->sog = atof(argv[9]);
+    }
   }
 
   if (strcmp(argv[3], "TeleoperationDone") == 0)
