d8e591184b8a1f30aa0d8d25476c67dc318a1e1f
Joao Fortuna <fortuna.araujo@gmail.com>
2015-05-07 19:55:47 +0200

    CMake: Added uEye camera Library support.

---
+++ cmake/Libraries/uEye.cmake
@@ -0,0 +1,56 @@
+############################################################################
+# Copyright 2013-2015 Norwegian University of Science and Technology (NTNU)#
+# Centre for Autonomous Marine Operations and Systems (AMOS)               #
+# Department of Engineering Cybernetics (ITK)                              #
+############################################################################
+# This file is part of DUNE: Unified Navigation Environment.               #
+#                                                                          #
+# Commercial Licence Usage                                                 #
+# Licencees holding valid commercial DUNE licences may use this file in    #
+# accordance with the commercial licence agreement provided with the       #
+# Software or, alternatively, in accordance with the terms contained in a  #
+# written agreement between you and Universidade do Porto. For licensing   #
+# terms, conditions, and further information contact lsts@fe.up.pt.        #
+#                                                                          #
+# European Union Public Licence - EUPL v.1.1 Usage                         #
+# Alternatively, this file may be used under the terms of the EUPL,        #
+# Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       #
+# included in the packaging of this file. You may not use this work        #
+# except in compliance with the Licence. Unless required by applicable     #
+# law or agreed to in writing, software distributed under the Licence is   #
+# distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     #
+# ANY KIND, either express or implied. See the Licence for the specific    #
+# language governing permissions and limitations at                        #
+# http://ec.europa.eu/idabc/eupl.html.                                     #
+############################################################################
+# Author: João Fortuna                                                     #
+############################################################################
+
+if(UEYE)
+  dune_test_lib(ueye_api is_GetImageMem)
+  dune_test_header(ueye.h)
+
+  if(DUNE_SYS_HAS_LIB_UEYE_API AND DUNE_SYS_HAS_UEYE_H)
+    set(DUNE_USING_UEYE 1 CACHE INTERNAL "ueye")
+  else(DUNE_SYS_HAS_LIB_UEYE_API AND DUNE_SYS_HAS_UEYE_H)
+    set(DUNE_USING_UEYE 0 CACHE INTERNAL "ueye")
+  endif(DUNE_SYS_HAS_LIB_UEYE_API AND DUNE_SYS_HAS_UEYE_H)
+endif(UEYE)
+
+#if(UEYE)
+#  CHECK_LIBRARY_EXISTS(ueye_api is_GetImageMem "" DUNE_SYS_HAS_LIB_UEYE_API)
+#
+#  if(DUNE_SYS_HAS_LIB_UEYE_API)
+#    set(DUNE_SYS_HAS_UEYE_H 1 CACHE INTERNAL "uEye library")
+#    set(DUNE_USING_UEYE 1 CACHE INTERNAL "uEye library")
+#
+#    dune_add_lib(ueye_api)
+#
+#    dune_test_header(ueye.h)
+#
+#  else(DUNE_SYS_HAS_LIB_UEYE_API)
+#    message(SEND_ERROR "uEye was not found on the system.")
+#    set(DUNE_SYS_HAS_UEYE_H 0 CACHE INTERNAL "uEye library")
+#    set(DUNE_USING_UEYE 0 CACHE INTERNAL "uEye library")
+#  endif(DUNE_SYS_HAS_LIB_UEYE_API)
+#endif(UEYE)
