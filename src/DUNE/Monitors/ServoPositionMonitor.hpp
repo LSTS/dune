da8c3cdf98ed326b68506d6a54c330446e39bb8c
Ricardo Martins <rasm@oceanscan-mst.com>
2016-01-11 18:01:28 +0000

    DUNE/Monitors/ServoPositionMonitor: reduced number of decimal places.

--- src/DUNE/Monitors/ServoPositionMonitor.hpp
+++ src/DUNE/Monitors/ServoPositionMonitor.hpp
@@ -93,8 +93,8 @@ namespace DUNE
             if (m_accum_pos < c_rate_factor * m_accum_com &&
                 std::fabs(position - command) > c_min_diff)
             {
-              *description = Utils::String::str("position change is %.5f, "
-                                                "command change is %.5f",
+              *description = Utils::String::str("position change is %.2f, "
+                                                "command change is %.2f",
                                                 m_accum_pos, m_accum_com);
               return true;
             }
