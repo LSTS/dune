d506a98b51a6e74bcb56352dfec4b085d74a6127
Jose Pinto <zepinto@gmail.com>
2016-03-02 17:44:44 +0000

    Math/Angles: Simplified formula and fixed indentation.

--- src/DUNE/Math/Angles.hpp
+++ src/DUNE/Math/Angles.hpp
@@ -53,8 +53,7 @@ namespace DUNE
       inline static fp64_t
       normalizeRadian(fp64_t a)
       {
-    	a = a - c_two_pi * floor((a + c_pi) / c_two_pi);
-		return a <= -c_pi ? c_pi : a;
+        return a - c_two_pi * floor((a + c_pi) / c_two_pi);
       }
 
       //! Convert a value given in degree to radian.
