LSTS/dune

commit 67688cf1b264263bbea5587cd1e9bd0b841d8d1e

Transports/UAN: Fixed message deletion.

src/Transports/UAN/Task.cpp
@@ -336,8 +336,10 @@ namespace Transports
           {
             aop.op = IMC::AcousticOperation::AOP_UNSUPPORTED;
             if (m_last_acop->op == IMC::AcousticOperation::AOP_MSG)
+            {
               delete m_msg_requests.front();
               m_msg_requests.erase(m_msg_requests.begin());
+            }
             break;
           }
           case IMC::UamTxStatus::UTS_DONE:
@@ -385,6 +387,7 @@ namespace Transports
                 aop.op = IMC::AcousticOperation::AOP_RANGE_TIMEOUT;
                 break;
               case IMC::AcousticOperation::AOP_MSG:
+                delete m_msg_requests.front();
                 m_msg_requests.erase(m_msg_requests.begin());
                 aop.op = IMC::AcousticOperation::AOP_MSG_FAILURE;
                 break;

commit 678d7d39796fc698dbb5b333d04aa2f61b99eb43

Transports/UAN: fixed a bug introduced in commit 67688cf that resulted in sending a second zeroed acoustic operation range received message that hid the first (and correct) one sent.

src/Transports/UAN/Task.cpp
@@ -336,7 +336,8 @@ namespace Transports
                 aop.op = IMC::AcousticOperation::AOP_ABORT_ACKED;
                 break;
               case IMC::AcousticOperation::AOP_RANGE:
-                aop.op = IMC::AcousticOperation::AOP_RANGE_RECVED;
+                // do nothing.
+                return;
                 break;
               case IMC::AcousticOperation::AOP_MSG:
                 delete m_msg_requests.front();