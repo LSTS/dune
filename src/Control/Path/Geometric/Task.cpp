7ec35f022590993bd3d66536371f2aafe5c66db3
Claudio Paliotta <claudio.paliotta@itk.ntnu.no>
2016-05-16 11:52:51 +0100

    Control/Path/Geometric: Task is now an empty PathController.

--- src/Control/Path/Geometric/Task.cpp
+++ src/Control/Path/Geometric/Task.cpp
@@ -36,61 +36,22 @@ namespace Control
     {
       using DUNE_NAMESPACES;
 
-      struct Task: public DUNE::Tasks::Task
+      struct Task: public DUNE::Control::PathController
       {
         //! Constructor.
         //! @param[in] name task name.
         //! @param[in] ctx context.
         Task(const std::string& name, Tasks::Context& ctx):
-          DUNE::Tasks::Task(name, ctx)
+          DUNE::Control::PathController(name, ctx)
         {
         }
-
-        //! Update internal state with new parameter values.
-        void
-        onUpdateParameters(void)
-        {
-        }
-
-        //! Reserve entity identifiers.
-        void
-        onEntityReservation(void)
-        {
-        }
-
-        //! Resolve entity names.
-        void
-        onEntityResolution(void)
-        {
-        }
-
-        //! Acquire resources.
-        void
-        onResourceAcquisition(void)
-        {
-        }
-
-        //! Initialize resources.
-        void
-        onResourceInitialization(void)
-        {
-        }
-
-        //! Release resources.
-        void
-        onResourceRelease(void)
-        {
-        }
-
-        //! Main loop.
-        void
-        onMain(void)
-        {
-          while (!stopping())
-          {
-            waitForMessages(1.0);
-          }
-        }
+	
+	void 
+        step(const IMC::EstimatedState& state, const TrackingState& ts)
+	{
+	  (void) state;
+          (void) ts;
+	}
       };
     }
   }
