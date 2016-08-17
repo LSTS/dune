dc7446a225844839af8abb3ef3434c0e1e075dae
José Braga <eejbraga@gmail.com>
2016-08-17 19:17:24 +0100

    Sensors/NortekDVL: fixed trigger argument.

--- src/Sensors/NortekDVL/Driver.hpp
+++ src/Sensors/NortekDVL/Driver.hpp
@@ -46,7 +46,7 @@ namespace Sensors
     //! No input trigger.
     static const std::string c_cmd_trg_no = "INTSR";
     //! Configured input trigger.
-    static const std::string c_cmd_trg_yes = "RS485FALL";
+    static const std::string c_cmd_trg_yes = "TTLRISE";
 
     //! Available power levels.
     enum PowerLevel
