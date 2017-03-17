5ce250f4373dc87006db9b2a8867e60c585b20c1
Tiago Marques <samarques.tiago@gmail.com>
2017-03-17 18:38:17 +0000

    www/js/Logbook: Fixed data reading.

--- www/js/Logbook.js
+++ www/js/Logbook.js
@@ -8,15 +8,14 @@ Logbook.prototype = new BasicSection;
 Logbook.prototype.update = function() {
     var logbookStr = "";
     for(i in g_dune_logbook.dune_logbook) {
-	var msg = g_dune_logbook[i];
+	var msg = g_dune_logbook.dune_logbook[i];
 	
 	if(msg.abbrev != "LogBookEntry")
 	    continue;
 
 	logbookStr = "<p>" +
-	    "[" + msg.timestamp; + "] - " +
-	    msg.type + " " +
-	    "[" + msg.context + "] >> " +
+	    "[" + msg.timestamp + "] - " + msg.type +
+	    " " + "[" + msg.context + "] >> " +
 	    msg.text +
 	    "<\p>";
     }
