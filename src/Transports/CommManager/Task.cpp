f33a4b6a88130686261628d4fdd99b7508ba4dfe
Jose Pinto <zepinto@gmail.com>
2017-12-12 19:25:48 +0000

    Transports/CommManager: Correct task initialization.

--- src/Transports/CommManager/Task.cpp
+++ src/Transports/CommManager/Task.cpp
@@ -314,6 +314,12 @@ namespace Transports
         Memory::clear(m_estate);
       }
 
+      void
+      onResourceInitialization(void)
+      {
+        setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_IDLE);
+      }
+
       IMC::StateReport* produceReport()
       {
         if (m_vstate == NULL || m_estate == NULL)
