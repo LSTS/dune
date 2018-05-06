213b32f4742467ed7d56743848c991bd0b41503f
José Pinto <zepinto@gmail.com>
2018-05-06 16:31:46 +0100

    Simulators/Iridium: Dispatch empty Iridium statuses.

--- src/Simulators/Iridium/Task.cpp
+++ src/Simulators/Iridium/Task.cpp
@@ -50,6 +50,8 @@ namespace Simulators
       std::string server_path;
     };
 
+    bool m_send_empty = false;
+
     //!
     struct Task: public Tasks::Periodic
     {
@@ -68,6 +70,8 @@ namespace Simulators
 
         param("Server Path", m_args.server_path)
           .defaultValue("/api/v1/irsim");
+
+        setFrequency(0.2);
 
         // Register consumers.
         bind<IMC::IridiumMsgTx>(this);
@@ -97,10 +101,13 @@ namespace Simulators
         status.setDestination(msg->getSource());
         status.setDestinationEntity(msg->getSourceEntity());
 
+        m_send_empty = false;
+
         std::stringstream ss;
-        ss << "POST " << m_args.server_path << " HTTP/1.0\r\n";
-        ss << "Content-Type: application/hub\r\n";
-        ss << "Content-Length: "<< msg->data.size()*2 << "\r\n\r\n";
+        ss << "POST " << m_args.server_path << " HTTP/1.1\n";
+        ss << "Host: " << m_args.server_address << "\n";
+        ss << "Content-Type: application/hub\n";
+        ss << "Content-Length: "<< msg->data.size()*2 << "\n\n";
 
         std::vector<char>::const_iterator it = msg->data.begin();
         for(; it != msg->data.end(); it++)
@@ -111,12 +118,21 @@ namespace Simulators
         inf("Sent message with %d bytes to %s:%d%s", (int) msg->data.size(),
             m_args.server_address.c_str(), m_args.server_port, m_args.server_path.c_str());
         try {
-          TCPSocket sock;
+          TCPSocket* sock = new TCPSocket();
+          char incoming[65535] = {0};
+
           Network::Address addr(m_args.server_address.c_str());
-          sock.connect(addr, m_args.server_port);
+          sock->connect(addr, m_args.server_port);
           std::string data = ss.str();
-          sock.write(data.c_str(), data.length());
-          sock.flush();
+          sock->write(data.c_str(), data.length());
+          sock->flushOutput();
+          int read = sock->read(incoming, 65535);
+          if (read > 0)
+            incoming[read] = 0;
+
+          debug("RESPONSE: %s", incoming);
+
+          Memory::clear(sock);
 
           status.status = IMC::IridiumTxStatus::TXSTATUS_OK;
           status.text = "Transmitted to Ripples (Simulation)";
@@ -128,12 +144,19 @@ namespace Simulators
           status.text = e.what();
           dispatch(status);
         }
+        m_send_empty = true;
       }
 
       //! @publish DUNE::IMC::IridiumMsgRx
       void
       task(void)
       {
+        if (m_send_empty)
+        {
+          IMC::IridiumTxStatus status;
+          status.status = IridiumTxStatus::TXSTATUS_EMPTY;
+          dispatch(status);
+        }
       }
     };
   }
