4b6e7d2b32ad24d753e2615b1b75776642220059
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2019-05-10 09:54:40 +0200

    DUNE/Tasks/Task: Make bind(id, consumer) protected (was private)

--- src/DUNE/Tasks/Task.hpp
+++ src/DUNE/Tasks/Task.hpp
@@ -608,6 +608,17 @@ namespace DUNE
                new Consumer<T, IMC::Message>(*task_obj, func));
       }
 
+      //! Register a consumer for a given message identifier.
+      //! @param[in] message_id message identifier.
+      //! @param[in] consumer consumer object.
+      void
+      bind(unsigned int message_id, AbstractConsumer* consumer)
+      {
+        spew("registering consumer for '%s'",
+             IMC::Factory::getAbbrevFromId(message_id).c_str());
+        m_recipient->bind(message_id, consumer);
+      }
+
       //! Request task to start/resume normal execution.
       void
       requestActivation(void);
@@ -808,17 +819,6 @@ namespace DUNE
 
       void
       run(void);
-
-      //! Register a consumer for a given message identifier.
-      //! @param[in] message_id message identifier.
-      //! @param[in] consumer consumer object.
-      void
-      bind(unsigned int message_id, AbstractConsumer* consumer)
-      {
-        spew("registering consumer for '%s'",
-             IMC::Factory::getAbbrevFromId(message_id).c_str());
-        m_recipient->bind(message_id, consumer);
-      }
 
       //! Consume QueryEntityState messages and reply accordingly.
       //! @param[in] msg QueryEntityState message.
