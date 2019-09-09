5cffc0cbbea07645c7467484c413be5a2021b71e
Jose Pinto <zepinto@gmail.com>
2019-09-10 00:48:38 +0100

    Autonomy/TextActions: Correctly reply to acoustic texts.

--- src/Autonomy/TextActions/Task.cpp
+++ src/Autonomy/TextActions/Task.cpp
@@ -335,7 +335,12 @@ namespace Autonomy
           	war("Bad acoustic origin: %s", origin.c_str());
           	return;
           }
+          IMC::TextMessage inner;
+          inner.text = text;
           req.comm_mean = TransmissionRequest::CMEAN_ACOUSTIC;
+          req.data_mode = TransmissionRequest::DMODE_INLINEMSG;
+          req.txt_data = "";
+          req.msg_data.set(&inner);
           req.destination = origin.substr(9);
           dispatch(req);
         }
