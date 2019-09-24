3e1372425a2b68559e655ef01464dfbb62ad4f99
mlancos <mlancos@lsts.pt>
2019-09-24 13:59:14 +0100

    Sensors/DMSv2: Created DMS control class "DriverDMSv2"

---
+++ src/Sensors/DMSv2/DriverDMSv2.hpp
@@ -0,0 +1,63 @@
+//***************************************************************************
+// Copyright 2007-2019 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Faculdade de Engenharia da             *
+// Universidade do Porto. For licensing terms, conditions, and further      *
+// information contact lsts@fe.up.pt.                                       *
+//                                                                          *
+// Modified European Union Public Licence - EUPL v.1.1 Usage                *
+// Alternatively, this file may be used under the terms of the Modified     *
+// EUPL, Version 1.1 only (the "Licence"), appearing in the file LICENCE.md *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: Miguel Lançós                                                    *
+//***************************************************************************
+
+
+#ifndef SENSORS_DMS_V2_DRIVER_HPP_INCLUDED_
+#define SENSORS_DMS_V2_DRIVER_HPP_INCLUDED_
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+// Local headers
+#include "CommsDMSv2.hpp"
+
+namespace Sensors
+{
+  //! Class to control the DMSv2 general functions.
+  //!
+  //! This class allows to send commands related to the DMS
+  //! general function and not related to a specific peripheral.
+  //! @author Miguel Lançós
+  namespace DMSv2
+  {
+    using DUNE_NAMESPACES;
+
+    class DriverDMSv2: private CommsDMSv2
+    {
+      public:
+      DriverDMSv2(DUNE::Tasks::Task* task, DUNE::Hardware::SerialPort* uart):
+      CommsDMSv2(task,uart)
+      {
+        m_task->trace("Initializing DriverDMSv2.");
+      }
+    };
+  }
+}
+
+#endif /*SENSORS_DMS_V2_DRIVER_HPP_INCLUDED_*/
