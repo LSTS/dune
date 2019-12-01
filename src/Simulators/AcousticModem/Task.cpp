df8c7a91069d746ce4220d95fb1bba38d1e014a0
Jose Pinto <zepinto@gmail.com>
2019-12-01 21:34:25 +0000

    Simulators/AcousticModem: Only processes requests destined to this entity or broadcast.

--- src/Simulators/AcousticModem/Task.cpp
+++ src/Simulators/AcousticModem/Task.cpp
@@ -201,6 +201,9 @@ namespace Simulators
         if (msg->getSource() != getSystemId())
           return;
 
+        if (msg->getDestinationEntity() != 255 && msg->getDestinationEntity() != getEntityId())
+          return;
+
         // Create and fill new ticket.
         Ticket ticket;
         ticket.imc_sid  = msg->getSource();
