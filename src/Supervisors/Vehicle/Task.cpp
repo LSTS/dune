f2d6e373d517d08dc6de9b763a93bc545abf31e8
mcpca <mca@netc.eu>
2020-06-19 16:48:45 +0100

    Supervisors/Vehicle: Add pause maneuver command description.

--- src/Supervisors/Vehicle/Task.cpp
+++ src/Supervisors/Vehicle/Task.cpp
@@ -49,8 +49,9 @@ namespace Supervisors
                                          DTR_RT("ERROR"), DTR_RT("MANEUVERING"),
                                          DTR_RT("EXTERNAL CONTROL"), DTR_RT("BOOT")};
     //! Vehicle command description strings
-    static const char* c_cmd_desc[] =
-    {"maneuver start", "maneuver stop", "calibration start", "calibration stop"};
+    static const char* c_cmd_desc[] = {"maneuver start", "maneuver stop",
+                                       "calibration start", "calibration stop",
+                                       "maneuver pause"};
     //! Printing errors period
     static const float c_error_period = 2.0;
     //! Maneuver request timeout
