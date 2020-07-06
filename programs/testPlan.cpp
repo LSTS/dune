b966257189aa4c2b73214202c78095116384932a
mcpca <mca@netc.eu>
2020-07-06 12:29:27 +0100

    programs: testPlan: Cleanup.
    Updated to use DUNE's OptionParser.

--- programs/testPlan.cpp
+++ programs/testPlan.cpp
@@ -29,185 +29,253 @@
 
 #include <DUNE/DUNE.hpp>
 
-using DUNE_NAMESPACES;
-
-using namespace DUNE::IMC;
-using namespace DUNE::Parsers;
-
-static
-void
-usage(void)
-{
-  std::cerr << "== Usage == "
-            << std::endl
-            << std::endl
-            << " testPlan [options] someplanfile.ini host port"
-            << std::endl
-            << std::endl
-            << "== Options =="
-            << std::endl
-            << std::endl
-            << " -a: all settings for a custom APDL setup (implies '-h -l -s')"
-            << std::endl
-            << " -n: *no* calibration step for plan startup"
-            << std::endl
-            << " -l: send a sample LBL config for APDL"
-            << std::endl
-            << " -L: just load plan, do not start it"
-            << std::endl
-            << " -p: do not parse plan (assumed it's been stored before)"
-            << std::endl
-            << " -s: send GpsFix (APDL)"
-            << std::endl
-            << std::endl;
-}
+using namespace DUNE;
+using namespace DUNE::Network;
+using namespace DUNE::Time;
 
 static void
-sendMsg(IMC::Message& msg, UDPSocket& sock, Address& dest, int port)
+sendMsg(Address const& dest, int port, IMC::Message& msg, UDPSocket& socket)
 {
-  DUNE::Utils::ByteBuffer bb;
+  Utils::ByteBuffer bb;
   msg.setTimeStamp();
   IMC::Packet::serialize(&msg, bb);
-  sock.write((const uint8_t*)bb.getBuffer(), msg.getSerializationSize(), dest, port);
+
+  socket.write((const std::uint8_t*) bb.getBuffer(), msg.getSerializationSize(),
+               dest, port);
   msg.toText(std::cout);
 }
 
-template <typename T>
-static void
-getAndSend(Parsers::Config& parser, std::string section, UDPSocket& sock, Address dest, int port)
+namespace
 {
-  T msg;
-  PlanConfigParser::parse(parser, section, msg);
-  sendMsg(msg, sock, dest, port);
+  struct Arguments
+  {
+    std::string config_path;
+    Address host;
+    std::uint16_t port;
+    bool gps_fix = false;
+    bool lbl_config = false;
+    bool just_load = false;
+    bool no_calibrate = false;
+    bool load_from_db = false;
+  };
+} // namespace
+
+static void
+printArguments(Arguments const& args, std::ostream& os)
+{
+#define PRINT_OPTION(field) \
+  #field ": " << args.field << '\n'
+
+#define PRINT_BOOL(field) \
+  #field ": " << (args.field ? "yes" : "no") << '\n'
+
+  os << "Command line arguments: \n"
+     << PRINT_OPTION(config_path)
+     << PRINT_OPTION(host)
+     << PRINT_OPTION(port)
+     << PRINT_BOOL(gps_fix)
+     << PRINT_BOOL(lbl_config)
+     << PRINT_BOOL(just_load)
+     << PRINT_BOOL(no_calibrate)
+     << PRINT_BOOL(load_from_db)
+     << std::endl;
+
+#undef PRINT_OPTION
+#undef PRINT_BOOL
+}
+
+static Arguments
+parseCommandLine(int argc, char** argv)
+{
+  Utils::OptionParser parser;
+  parser.program("testPlan")
+  .executable(argv[0])
+  .arch(DUNE_SYSTEM_NAME)
+  .copyright(DUNE_COPYRIGHT)
+  .date(getCompileDate())
+  .description("Send a PlanControl request to a system.")
+  .email(DUNE_CONTACT)
+  .version(getFullVersion())
+  .add("-c", "--config-file", "Path to plan configuration file", "CONFIG")
+  .add("-p", "--port", "Destination port", "PORT")
+  .add("-d", "--host", "Destination host", "HOST")
+  .add("-g", "--gps-fix", "Send GPSFix message before PlanControl")
+  .add("-l", "--lbl-config", "Send LblConfig message before PlanControl")
+  .add("-L", "--just-load", "Send PC_LOAD request (default is PC_START)")
+  .add("-n", "--no-calibrate", "Don't set FLG_CALIBRATE")
+  .add("-D", "--load-from-db", "Don't send inline PlanSpecification");
+
+  parser.parse(argc, argv);
+
+  if (parser.bad())
+  {
+    std::cerr << "Error parsing command line: " << parser.error() << std::endl;
+    parser.usage();
+    std::exit(1);
+  }
+
+  Arguments args;
+
+  args.config_path = parser.value("--config-file");
+
+  if (args.config_path.empty())
+  {
+    std::cerr << "You must specify a configuration file." << std::endl;
+    std::exit(1);
+  }
+
+  std::string const& host_str = parser.value("--host");
+
+  if (host_str.empty())
+  {
+    std::cerr << "You must specify a destination host." << std::endl;
+    std::exit(1);
+  }
+
+  args.host = Address(host_str.c_str());
+
+  std::string const& port_str = parser.value("--port");
+
+  if (port_str.empty())
+  {
+    std::cerr << "You must specify a destination port." << std::endl;
+    std::exit(1);
+  }
+
+  int port = std::atoi(port_str.c_str());
+
+  if (port <= 0 || port > UINT16_MAX)
+  {
+    std::cerr << "Invalid destination port \'" << port_str << "\'."
+              << std::endl;
+    std::exit(1);
+  }
+
+  args.port = static_cast<std::uint16_t>(port);
+
+  // Handle boolean options
+#define SET_BOOLEAN_OPT(opt, field)                                            \
+  do                                                                           \
+  {                                                                            \
+    if (!parser.value(opt).empty())                                            \
+      args.field = true;                                                       \
+  } while (0)
+
+  SET_BOOLEAN_OPT("--lbl-config", lbl_config);
+  SET_BOOLEAN_OPT("--gps-fix", gps_fix);
+  SET_BOOLEAN_OPT("--just-load", just_load);
+  SET_BOOLEAN_OPT("--no-calibrate", no_calibrate);
+  SET_BOOLEAN_OPT("--load-from-db", load_from_db);
+
+#undef SET_BOOLEAN_OPT
+
+  printArguments(args, std::cerr);
+
+  return args;
+}
+
+static IMC::GpsFix
+makeGpsFix(void)
+{
+  IMC::GpsFix origin;
+  origin.lat = Angles::radians(41.185781);
+  origin.lon = Angles::radians(-8.70606486);
+  origin.height = 0;
+  origin.validity = IMC::GpsFix::GFV_VALID_POS | IMC::GpsFix::GFV_VALID_HDOP;
+
+  return origin;
+}
+
+static IMC::LblConfig
+makeLblConfig(void)
+{
+  IMC::LblBeacon b0;
+  IMC::LblBeacon b1;
+
+  b0.beacon = "b0";
+  b0.lat = Angles::radians(41.18482);
+  b0.lon = Angles::radians(-8.70450);
+  b0.depth = 2;
+
+  b1.beacon = "b1";
+  b1.lat = Angles::radians(41.18607);
+  b1.lon = Angles::radians(-8.70588);
+  b1.depth = 2;
+
+  IMC::LblConfig lbl_config;
+  lbl_config.beacons.push_back(b0);
+  lbl_config.beacons.push_back(b1);
+
+  return lbl_config;
 }
 
 int
 main(int argc, char** argv)
 {
-  --argc; ++argv;
-  bool lbl = false, start_point = false, calibrate = true, just_load = false, do_parse = true;
+  Arguments const args = parseCommandLine(argc, argv);
 
-  for (; *argv && **argv == '-'; ++argv, --argc)
+  UDPSocket socket;
+
+  if (args.gps_fix)
   {
-    // @todo Use DUNE's OptionParser, too lazy to do it now.
-    char opt = argv[0][1];
-    switch (opt)
-    {
-      case 'a':
-        lbl = true;
-        start_point = true;
-        break;
-      case 'l':
-        lbl = true;
-        break;
-      case 'L':
-        just_load = true;
-        break;
-      case 'p':
-        do_parse = false;
-        break;
-      case 'n':
-        calibrate = false;
-        break;
-      case 's':
-        start_point = true;
-        break;
-      default:
-        std::cerr << "Invalid option: '-" << opt << "'\n";
-        usage();
-        return 1;
-    }
+    IMC::GpsFix msg = makeGpsFix();
+    sendMsg(args.host, args.port, msg, socket);
   }
 
-  if (argc < 3)
+  if (args.lbl_config)
   {
-    usage();
-    return 1;
+    IMC::LblConfig msg = makeLblConfig();
+    sendMsg(args.host, args.port, msg, socket);
   }
 
-  Parsers::Config config(argv[0]);
-
-  UDPSocket sock;
-  Address dest(argv[1]);
-  uint16_t port = atoi(argv[2]);
-
-  if (start_point)
+  if (args.gps_fix || args.lbl_config)
   {
-    GpsFix origin;
-    origin.lat = Angles::radians(41.185781);
-    origin.lon = Angles::radians(-8.70606486);
-    origin.height = 0;
-    origin.validity = IMC::GpsFix::GFV_VALID_POS | IMC::GpsFix::GFV_VALID_HDOP;
-    sendMsg(origin, sock, dest, port);
-  }
-
-  if (lbl)
-  {
-    LblBeacon b0;
-    LblBeacon b1;
-
-    b0.beacon = "b0";
-    b0.lat = Angles::radians(41.18482);
-    b0.lon = Angles::radians(-8.70450);
-    b0.depth = 2;
-
-    b1.beacon = "b1";
-    b1.lat = Angles::radians(41.18607);
-    b1.lon = Angles::radians(-8.70588);
-    b1.depth = 2;
-
-    LblConfig lbl_config;
-    lbl_config.beacons.push_back(b0);
-    lbl_config.beacons.push_back(b1);
-
-    sendMsg(lbl_config, sock, dest, port);
-  }
-
-  if (start_point || lbl)
-  {
-    std::cerr << "Giving some time for navigation to eventually stabilize (7s)" << std::endl;
+    std::cerr << "Giving some time for navigation to eventually stabilize (7s)"
+              << std::endl;
     Delay::wait(7);
   }
 
-  PlanControl cmd;
-  cmd.type = PlanControl::PC_REQUEST;
-  cmd.op = just_load ? PlanControl::PC_LOAD : PlanControl::PC_START;
-  if (calibrate)
-    cmd.flags = PlanControl::FLG_CALIBRATE;
-  else
-    cmd.flags = 0;
+  IMC::PlanControl cmd;
+  cmd.type = IMC::PlanControl::PC_REQUEST;
+  cmd.op
+  = args.just_load ? IMC::PlanControl::PC_LOAD : IMC::PlanControl::PC_START;
+
+  if (!args.no_calibrate)
+    cmd.flags |= IMC::PlanControl::FLG_CALIBRATE;
+
   cmd.request_id = 0;
 
-  if (do_parse)
+  Parsers::Config config(args.config_path.c_str());
+
+  if (!args.load_from_db)
   {
-    PlanSpecification mission_spec;
-    PlanConfigParser::parse(config, mission_spec);
+    IMC::PlanSpecification mission_spec;
+    Parsers::PlanConfigParser::parse(config, mission_spec);
     mission_spec.toText(std::cerr);
     cmd.arg.set(mission_spec);
     cmd.plan_id = mission_spec.plan_id;
 
-    OperationalLimits op;
-    std::vector<std::string> sections = config.sections();
+    IMC::OperationalLimits op;
 
-    for (std::vector<std::string>::iterator itr = sections.begin(); itr != sections.end(); ++itr)
+    for (auto const& section : config.sections())
     {
-      if ((*itr) == "Operational Limits")
-      {
-        config.get("Operational Limits", "Minimum Altitude", "0.0", op.min_altitude);
-        config.get("Operational Limits", "Maximum Depth", "5.0", op.max_depth);
-        op.mask = IMC::OPL_MAX_DEPTH | IMC::OPL_MIN_ALT;
+      if (section != "Operational Limits")
+        continue;
 
-        sendMsg(op, sock, dest, port);
-      }
+      config.get("Operational Limits", "Minimum Altitude", "0.0",
+                 op.min_altitude);
+      config.get("Operational Limits", "Maximum Depth", "5.0", op.max_depth);
+      op.mask = IMC::OPL_MAX_DEPTH | IMC::OPL_MIN_ALT;
+
+      sendMsg(args.host, args.port, op, socket);
     }
-
   }
   else
   {
     config.get("Plan Configuration", "Plan ID", "", cmd.plan_id);
   }
 
-  sendMsg(cmd, sock, dest, port);
+  sendMsg(args.host, args.port, cmd, socket);
 
   return 0;
 }
