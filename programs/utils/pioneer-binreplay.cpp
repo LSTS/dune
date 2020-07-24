4479812edb7c3499b64188311f609e2f0be3266d
Paulo Dias <paulo.sousa.dias@gmail.com>
2020-07-24 11:28:53 +0100

    programs/utils/pioneer-binreplay: First version of Pioneer bin replay.

--- programs/utils/pioneer-binreplay.cpp
+++ programs/utils/pioneer-binreplay.cpp
@@ -33,6 +33,42 @@
 
 //using DUNE_NAMESPACES;
 
+static double start_time_sec = DUNE::Time::Clock::getSinceEpoch();
+static double now_sec = start_time_sec;
+
+static double time_origin_ms = -1;
+
+static double
+timeAndPaceProgress(uint32_t msg_ts_ms, double speed)
+{
+  std::cerr << "Time ms: " << msg_ts_ms << std::endl;
+
+  if (time_origin_ms < 0)
+  {
+    time_origin_ms = msg_ts_ms;
+  }
+
+  if (speed <= 0)
+  {
+    now_sec = DUNE::Time::Clock::getSinceEpoch();
+    return now_sec;
+  }
+
+  double vtime_ms = msg_ts_ms - time_origin_ms;
+
+  double future_time_sec = 0;
+
+  if (speed > 0)
+  {
+    future_time_sec = start_time_sec + vtime_ms * 1E-3 / speed;
+    double delay_time = (future_time_sec - now_sec);
+    if (delay_time > 0)
+      DUNE::Time::Delay::wait(delay_time);
+  }
+  now_sec = DUNE::Time::Clock::getSinceEpoch();
+  return now_sec;
+}
+
 int
 main(int argc, char** argv)
 {
@@ -45,14 +81,16 @@ main(int argc, char** argv)
       .date(DUNE::getCompileDate())
       .arch(DUNE_SYSTEM_NAME)
       .description("Utility to replay Blueye Pioneer's binary telemetry data.")
-      .add("-d", "--ip",
-           "Destination IP", "DEST_IP")
+      .add("-d", "--dest",
+           "Destination IP (defaults to 127.0.0.1)", "DEST_IP")
       .add("-p", "--port",
-           "Destination UDP port", "DEST_PORT")
-      .add("-r", "--realtime",
-           "Replay with realtime spacing", "REALTIME")
+           "Destination UDP port (defaults to 2010)", "DEST_PORT")
+      .add("-s", "--speed",
+           "Replay with realtime spacing (default, =1.0). Use 0 for dump all", "SPEED")
       .add("-f", "--file",
-           "Telemetry file", "TELEMETRY_FILE");
+           "Telemetry file (can be zipped, bz2 or gzip)", "TELEMETRY_FILE")
+      .add("-c", "--count",
+           "Replay only c messages (default to all). Use 0 for dump all", "COUNT");
 
   // Parse command line arguments.
   if (!options.parse(argc, argv))
@@ -73,44 +111,60 @@ main(int argc, char** argv)
 
   // Get specified dest IP.
   DUNE::Network::Address dest;
-  try
+  if (options.value("--dest") != "")
+  {
+    try
     {
-      DUNE::castLexical(options.value("--ip"), dest);
+      DUNE::castLexical(options.value("--dest"), dest);
       // destination_address = new Address(options.value("--ip").c_str());
     }
-  catch (...)
+    catch (...)
     {
       std::cerr << "ERROR: invalid IP." << std::endl;
       return 1;
     }
+  }
+  else
+  {
+    DUNE::castLexical("127.0.0.1", dest);
+  }
 
   // Get specified dest UDP port.
   int port = 2010;
   if (DUNE::castLexical(options.value("--port"), port))
+  {
+    if (port < 1 && port > 65535)
     {
-      if (port < 1 && port > 65535)
-        {
-          std::cerr << "ERROR: invalid port." << std::endl;
-          return 1;
-        }
-    }
-
-  // Get specified dest UDP port.
-  bool realtime = false;
-  if (DUNE::castLexical(options.value("--realtime"), realtime))
-    {
-      std::cerr << "ERROR: invalid option for realtime." << std::endl;
+      std::cerr << "ERROR: invalid port." << std::endl;
       return 1;
     }
+  }
+
+  // Get specified dest UDP port.
+  double speed = 1;
+  if (options.value("--speed") != "" && !DUNE::castLexical(options.value("--speed"), speed))
+  {
+    std::cerr << "ERROR: invalid option for speed." << std::endl;
+    return 1;
+  }
+  if (speed < 0)
+  {
+    std::cerr << "ERROR: invalid option for speed (should be >= 0.0." << std::endl;
+    return 1;
+  }
+
+  uint64_t max_count = 0;
+  uint64_t cur_count = 0;
+  DUNE::castLexical(options.value("--count"), max_count);
 
   DUNE::FileSystem::Path file(bin_file_path);
   std::istream* ifs;
 
   if (!file.isFile())
-    {
-      std::cerr << file << " does not exist" << std::endl;
-      return 1;
-    }
+  {
+    std::cerr << file << " does not exist" << std::endl;
+    return 1;
+  }
 
   DUNE::Compression::Methods method = DUNE::Compression::Factory::detect(file.c_str());
   if (method == DUNE::Compression::Methods::METHOD_UNKNOWN)
@@ -122,10 +176,10 @@ main(int argc, char** argv)
 
   std::vector<char> data;
 
-  while (!ifs->eof())
+  while (!ifs->eof() && (max_count <= 0 || cur_count++ < max_count ))
   {
     // Get the message header.
-    data.resize(2);
+    data.resize(512);
     ifs->read(&data[0], 2);
 
     if (ifs->eof())
@@ -139,7 +193,7 @@ main(int argc, char** argv)
             Control::Pioneer::ProtocolMessages::DataVersion1Telemetry* msgV1Telm;
             msgV1Telm = new Control::Pioneer::ProtocolMessages::DataVersion1Telemetry();
             const int sizeMsg = sizeof(*msgV1Telm);
-            data.resize(sizeMsg);
+            //data.resize(sizeMsg);
             ifs->read(&data[2], sizeMsg - 2);
             if (ifs->gcount() < sizeMsg - 2)
               throw DUNE::IMC::BufferTooShort();
@@ -148,8 +202,14 @@ main(int argc, char** argv)
             if (rb > 0)
             {
               uint32_t  ms_time = msgV1Telm->time;
-              size_t  st = sock.write((uint8_t*)&data[0], sizeMsg, dest, port);
-              std::cout << "sent " << st << std::endl;
+              double new_time_sec = timeAndPaceProgress(ms_time, speed);
+              printf("Time(ms) %d   TimeRT(s) %f", msgV1Telm->time, msgV1Telm->rt_clock);
+              msgV1Telm->rt_clock = new_time_sec;
+              msgV1Telm->time = (uint32_t)((new_time_sec - start_time_sec) * 1E3);
+              printf(" => Time(ms) %d   TimeRT(s) %f\n", msgV1Telm->time, msgV1Telm->rt_clock);
+              int dataLength = Control::Pioneer::ProtocolPack::Pack::pack(NULL, msgV1Telm, buf);
+              size_t st = sock.write(buf, dataLength, dest, port);
+              std::cout << "sent " << st << " bytes of " << dataLength << "" << std::endl;
             }
             DUNE::Memory::clear(msgV1Telm);
           }
@@ -159,7 +219,7 @@ main(int argc, char** argv)
             Control::Pioneer::ProtocolMessages::DataVersion2Telemetry* msgV2Telm;
             msgV2Telm = new Control::Pioneer::ProtocolMessages::DataVersion2Telemetry();
             const int sizeMsg = sizeof(*msgV2Telm);
-            data.resize(sizeMsg);
+            //data.resize(sizeMsg);
             ifs->read(&data[2], sizeMsg - 2);
             if (ifs->gcount() < sizeMsg - 2)
               throw DUNE::IMC::BufferTooShort();
@@ -168,8 +228,14 @@ main(int argc, char** argv)
             if (rb > 0)
             {
               uint32_t  ms_time = msgV2Telm->time;
-              size_t st = sock.write(buf, sizeMsg, dest, port);
-              std::cout << "sent " << st << std::endl;
+              double new_time_sec = timeAndPaceProgress(ms_time, speed);
+              printf("Time(ms) %d   TimeRT(s) %f", msgV2Telm->time, msgV2Telm->rt_clock);
+              msgV2Telm->rt_clock = new_time_sec;
+              msgV2Telm->time = (uint32_t)((new_time_sec - start_time_sec) * 1E3);
+              printf(" => Time(ms) %d   TimeRT(s) %f\n", msgV2Telm->time, msgV2Telm->rt_clock);
+              int dataLength = Control::Pioneer::ProtocolPack::Pack::pack(NULL, msgV2Telm, buf);
+              size_t st = sock.write(buf, dataLength, dest, port);
+              std::cout << "sent " << st << " bytes of " << dataLength << "" << std::endl;
             }
             DUNE::Memory::clear(msgV2Telm);
           }
@@ -179,7 +245,7 @@ main(int argc, char** argv)
             Control::Pioneer::ProtocolMessages::DataVersion2Compasscalibration* msgV2CompassCal;
             msgV2CompassCal = new Control::Pioneer::ProtocolMessages::DataVersion2Compasscalibration();
             const int sizeMsg = sizeof(*msgV2CompassCal);
-            data.resize(sizeMsg);
+            //data.resize(sizeMsg);
             ifs->read(&data[2], sizeMsg - 2);
             if (ifs->gcount() < sizeMsg - 2)
               throw DUNE::IMC::BufferTooShort();
