e0a9f199430152543d88ca44b08ddb5b4920581c
mariacosta <mariacosta_89@hotmail.com>
2020-12-17 00:23:54 +0000

    Otter: Added NTNU contributions for Otter ASV integration in DUNE (fork: https://github.com/nikkone/dune/tree/ntnuOtterASV/).

--- src/DUNE/Hardware/SocketCAN.hpp
+++ src/DUNE/Hardware/SocketCAN.hpp
@@ -24,38 +24,99 @@
 // https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
 // http://ec.europa.eu/idabc/eupl.html.                                     *
 //***************************************************************************
-// Author: Ricardo Martins                                                  *
+// Author: Nikolai Lauvås (NTNU, Department of Engineering Cybernetics)     *
 //***************************************************************************
 
-#ifndef DUNE_HARDWARE_HPP_INCLUDED_
-#define DUNE_HARDWARE_HPP_INCLUDED_
+#ifndef DUNE_HARDWARE_SOCKETCAN_HPP_INCLUDED_
+#define DUNE_HARDWARE_SOCKETCAN_HPP_INCLUDED_
+/* TODO/missing functionality:
+* Research flush functions
+
+*/
+// DUNE headers.
+#include <DUNE/Config.hpp>
+#include <DUNE/IO/Handle.hpp>
+
+#if defined(DUNE_OS_LINUX)
+#include <linux/can.h>
+#include <net/if.h>
+#else
+      throw Error("Unimplemented feature", "DUNE::Hardware::SocketCAN");
+#endif
+
 
 namespace DUNE
 {
-  //! Low level hardware drivers.
   namespace Hardware
-  { }
+  {
+    // Export DLL Symbol.
+    class DUNE_DLL_SYM SocketCAN;
+
+    //! The SocketCAN class encapsulates CAN access.
+    class SocketCAN: public IO::Handle
+    {
+    public:
+      class Error: public std::runtime_error
+      {
+      public:
+        Error(std::string op, std::string msg):
+          std::runtime_error("Socket CAN error (" + op + "): " + msg)
+        { }
+      };
+#if defined(DUNE_OS_LINUX)
+      enum can_frame_t {
+        CAN_BASIC_SFF = 0,
+        CAN_BASIC_EFF = 1,
+        CAN_FD = 2
+      };
+      //! SocketCAN constructor.
+      SocketCAN(const std::string& can_dev, can_frame_t frame_type);
+      
+      //! Socket CAN destructor.
+      ~SocketCAN(void);
+      
+      void setTXID(uint32_t id);
+      uint32_t getRXID();
+      size_t readHexString(char* bfr, size_t length);
+      //size_t writeHexString(const char* cstr);
+    private:
+      //! CAN connection variables.
+      struct sockaddr_can m_addr;
+      struct ifreq m_ifr;
+      int m_can_socket;
+      can_frame_t can_frame_type;
+      uint32_t cantxid = 0;
+      uint32_t canrxid = 0;
+      
+      IO::NativeHandle
+      doGetNative(void) const
+      {
+        return m_can_socket; // Makes Poll::poll work
+      }
+
+      size_t
+      doWrite(const uint8_t* bfr, size_t size);
+
+      size_t
+      doRead(uint8_t* bfr, size_t size);
+
+      //! Flush input buffer, discarding all of it's contents.
+      void
+      doFlushInput(void);
+
+      //! Flush output buffer, aborting output.
+      void
+      doFlushOutput(void);
+
+      //! Flush both input and output buffers.
+      void
+      doFlush(void);
+#else
+      throw Error("Unimplemented feature", "DUNE::Hardware::SocketCAN");
+#endif
+    };
+  }
 }
 
-#include <DUNE/Hardware/SerialPort.hpp>
-#include <DUNE/Hardware/I2C.hpp>
-#include <DUNE/Hardware/IOPort.hpp>
-#include <DUNE/Hardware/GPIO.hpp>
-#include <DUNE/Hardware/Buttons.hpp>
-#include <DUNE/Hardware/ESCC.hpp>
-#include <DUNE/Hardware/IntelHEX.hpp>
-#include <DUNE/Hardware/BasicModem.hpp>
-#include <DUNE/Hardware/HayesModem.hpp>
-#include <DUNE/Hardware/BasicDeviceDriver.hpp>
-#include <DUNE/Hardware/Exceptions.hpp>
-#include <DUNE/Hardware/UCTK/Constants.hpp>
-#include <DUNE/Hardware/UCTK/Errors.hpp>
-#include <DUNE/Hardware/UCTK/Parser.hpp>
-#include <DUNE/Hardware/UCTK/Bootloader.hpp>
-#include <DUNE/Hardware/LUCL/Protocol.hpp>
-#include <DUNE/Hardware/LUCL/ProtocolParser.hpp>
-#include <DUNE/Hardware/LUCL/BootLoader.hpp>
-#include <DUNE/Hardware/PWM.hpp>
-#include <DUNE/Hardware/SocketCAN.hpp>
 
 #endif
