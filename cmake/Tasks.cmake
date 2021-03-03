5d4f76d025acf2c81b7c95d15a3a4a010c536572
Paulo Dias <paulo.sousa.dias@gmail.com>
2021-03-03 19:50:30 +0000

    cmake/Tasks.cmake: Changes to allow multiple private and user folders.

--- cmake/Tasks.cmake
+++ cmake/Tasks.cmake
@@ -171,8 +171,16 @@ if(TASK_FILE)
   endif()
 else(TASK_FILE)
   dune_add_tasks(${PROJECT_SOURCE_DIR}/src)
-  dune_add_tasks(${PROJECT_SOURCE_DIR}/private/src)
-  dune_add_tasks(${PROJECT_SOURCE_DIR}/user/src)
+  # Adding private folders.
+  file(GLOB privdir ${PROJECT_SOURCE_DIR}/private*/)
+  foreach(privdir ${privdir})
+    dune_add_tasks(${privdir}/src)
+  endforeach(privdir ${privdir})
+  # Adding user folders.
+  file(GLOB userdir ${PROJECT_SOURCE_DIR}/user*/)
+  foreach(userdir ${userdir})
+    dune_add_tasks(${userdir}/src)
+  endforeach(userdir ${userdir})
 endif(TASK_FILE)
 
 list(SORT DUNE_TASKS_ENABLED)
