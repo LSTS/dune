8a748310b8216b3b20706d3c723d2748f96f5ff8
zimmele2 <38285415+zimmele2@users.noreply.github.com>
2021-07-30 16:32:05 +0100

    DtlsClient Task executes handshake with dtls_server from mbedtls

--- vendor/libraries/mbedtls/Library.cmake
+++ vendor/libraries/mbedtls/Library.cmake
@@ -1,8 +1,9 @@
 # TODO: MAKE GUARD
 aux_source_directory(vendor/libraries/mbedtls/ DUNE_MBEDTLS_FILES)
 aux_source_directory(vendor/libraries/mbedtls/library/ DUNE_MBEDTLS_FILES)
-include_directories(vendor/libraries/mbedtls/include vendor/libraries/mbedtls/tests/include)
-
+include_directories(vendor/libraries/mbedtls/include
+                    vendor/libraries/mbedtls/tests/include
+                    vendor/libraries/mbedtls/library)
 
 set_source_files_properties(${DUNE_MBEDTLS_FILES}
   PROPERTIES COMPILE_FLAGS "${DUNE_CXX_FLAGS} ${DUNE_CXX_FLAGS_STRICT}")
