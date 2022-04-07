4c51f18203f5e49be9570ac3bbdbdc5ff26da01b
José Braga <jose.braga@airtame.com>
2022-04-07 15:24:23 +0100

    Vendor: zlib: silence fall through warnings.

--- vendor/libraries/zlib/Library.cmake
+++ vendor/libraries/zlib/Library.cmake
@@ -7,7 +7,12 @@ else(DUNE_CXX_GNU AND DUNE_OS_WINDOWS)
   set(ZLIB_CFLAGS "")
 endif(DUNE_CXX_GNU AND DUNE_OS_WINDOWS)
 
+check_cxx_compiler_flag(-Wimplicit-fallthrough=0 _has_wimplicit_fallthrough_0)
+if(_has_wimplicit_fallthrough_0)
+  set(_zlib_extra_cflags -Wimplicit-fallthrough=0)
+endif()
+
 set_source_files_properties(${DUNE_ZLIB_FILES}
-  PROPERTIES COMPILE_FLAGS "${DUNE_C_FLAGS} ${DUNE_C_FLAGS_STRICT} ${ZLIB_CFLAGS}")
+  PROPERTIES COMPILE_FLAGS "${DUNE_C_FLAGS} ${DUNE_C_FLAGS_STRICT} ${ZLIB_CFLAGS} ${_zlib_extra_cflags}")
 
 list(APPEND DUNE_VENDOR_FILES ${DUNE_ZLIB_FILES})
