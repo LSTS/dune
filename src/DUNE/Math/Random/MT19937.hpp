6636a2a62a4ca0ac8bde7776218121b71a12c260
Paulo Dias <paulo.sousa.dias@gmail.com>
2024-02-05 10:12:33 +0000

    Updated copy years.

--- src/DUNE/Math/Random/MT19937.hpp
+++ src/DUNE/Math/Random/MT19937.hpp
@@ -1,5 +1,5 @@
 //***************************************************************************
-// Copyright 2007-2023 Universidade do Porto - Faculdade de Engenharia      *
+// Copyright 2007-2024 Universidade do Porto - Faculdade de Engenharia      *
 // Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
 //***************************************************************************
 // This file is part of DUNE: Unified Navigation Environment.               *
