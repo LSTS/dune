953b7f0614d3e50ccf85077de5eb339008db5a5f
elcapitanu <bernardoacgabriel@gmail.com>
2024-02-08 12:12:39 +0000

    DUNE/Tasks/Task: added Boot Priority argument.
    Co-authored-by: Luis Venancio <luisvenancio109@gmail.com>

--- src/DUNE/Tasks/Task.cpp
+++ src/DUNE/Tasks/Task.cpp
@@ -74,6 +74,12 @@ namespace DUNE
       .defaultValue("")
       .description(DTR("Main entity label"));
 
+      param(DTR_RT("Boot Priority"), m_args.boot_priority)
+      .defaultValue("0")
+      .minimumValue("0")
+      .maximumValue("10")
+      .description(DTR("Priority to sort start and stop of tasks"));
+
       param(DTR_RT("Execution Priority"), m_args.priority)
       .defaultValue("10")
       .description(DTR("Execution priority"));
