9783556fbbb0125824f863e84c79c2f85b8ea5d8
Luis Venancio <venancio@lsts.pt>
2024-03-04 12:33:27 +0000

    Vision/PointGrey: Fix warnings.

--- src/Vision/PointGrey/Task.cpp
+++ src/Vision/PointGrey/Task.cpp
@@ -653,10 +653,10 @@ namespace Vision
           }
           catch (...)
           {
-            std::fclose(pipe);
+            pclose(pipe);
             throw;
           }
-          std::fclose(pipe);
+          pclose(pipe);
           std::sscanf(buffer, "%s", governor);
           if( std::strcmp(governor, "ondemand") == 0)
           {
