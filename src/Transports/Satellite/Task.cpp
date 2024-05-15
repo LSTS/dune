f4601cb6ea2246f72c3a333898ef2d23838b0db6
xBogas <119903183+xBogas@users.noreply.github.com>
2024-05-15 22:52:17 +0100

    Transports/Satellite: Add handles for additional Iridium messages.
    Add param for max age of received Iridium messages.

--- src/Transports/Satellite/Task.cpp
+++ src/Transports/Satellite/Task.cpp
@@ -43,10 +43,18 @@ namespace Transports
   {
     using DUNE_NAMESPACES;
 
+    struct Arguments
+    {
+      //! Maximum age of received messages.
+      double max_age_secs;
+    };
+
     struct Task: public DUNE::Tasks::Task
     {
       //! Map of received iridium fragments.
       std::unordered_map<uint8_t, IridiumFragment*> m_ir_frags;
+      //! Task arguments.
+      Arguments m_args;
 
       //! Constructor.
       //! @param[in] name task name.
@@ -54,6 +62,11 @@ namespace Transports
       Task(const std::string& name, Tasks::Context& ctx):
         DUNE::Tasks::Task(name, ctx)
       {
+        param("Maximum age", m_args.max_age_secs)
+          .units(Units::Second)
+          .defaultValue("1200")
+          .description("Age, in seconds, after which received IMC messages are discarded.");
+
         bind<IMC::SatelliteRequest>(this);
         bind<IMC::IridiumTxStatus>(this);
         bind<IMC::IridiumMsgRx>(this);
@@ -118,6 +131,97 @@ namespace Transports
       }
 
       void
+      handleIridiumCmd(IridiumCommand* cmd)
+      {
+        IMC::TextMessage tm;
+        tm.text   = cmd->command;
+        tm.origin = "iridium";
+        tm.setSource(cmd->source);
+
+        spew("dispatching text message: %s", tm.text.c_str());
+        dispatch(tm);
+      }
+
+      void
+      handleUpdates(std::vector<DevicePosition>& positions)
+      {
+        inf("Received device update with %d positions", (int)positions.size());
+
+        for (auto itr = positions.begin(); itr != positions.end(); itr++)
+        {
+          DevicePosition& pos = *itr;
+          int selector        = (pos.id & 0x0F);
+
+          IMC::RemoteSensorInfo rsi;
+          rsi.alt     = -1;
+          rsi.lat     = pos.lat;
+          rsi.lon     = pos.lon;
+          rsi.heading = 0;
+
+          war("System %X is at (%.5f, %.5f).", pos.id, Angles::degrees(pos.lat),
+              Angles::degrees(pos.lon));
+
+          std::string name = resolveSystemId(pos.id);
+
+          switch (selector)
+          {
+            case 0:
+              rsi.sensor_class = "Unmanned Vehicle";
+              break;
+            case 2:
+              rsi.sensor_class = "External Vehicle";
+              break;
+            case 4:
+              rsi.sensor_class = "Console";
+              break;
+            case 6:
+              rsi.sensor_class = "Sensor";
+              break;
+            case 8:
+              rsi.sensor_class = "Manned Vehicle";
+              break;
+            default:
+            {
+              rsi.sensor_class = "Unknown";
+              std::stringstream ss;
+              ss << rsi.sensor_class << "_" << pos.id;
+              rsi.id = ss.str();
+            }
+            break;
+          }
+
+          if (pos.pos_class != 0)
+          {
+            std::stringstream ss;
+            ss << "Argos Class " << (char)pos.pos_class;
+            rsi.sensor_class = ss.str();
+          }
+          else
+            rsi.id = name;
+
+          dispatch(rsi);
+        }
+      }
+
+      void
+      handleIMCMessage(ImcIridiumMessage* ir_msg)
+      {
+        IMC::Message* msg = ir_msg->msg;
+
+        double age = Clock::getSinceEpoch() - msg->getTimeStamp();
+        if (age >= m_args.max_age_secs)
+        {
+          war("discarded IMC message of type %s because it is too old (%f seconds of age).",
+              msg->getName(), age);
+          return;
+        }
+
+        inf("received IMC message of type %s via Iridium from %d.", msg->getName(), ir_msg->source);
+        msg->setSource(ir_msg->source);
+        dispatch(msg);
+      }
+
+      void
       handleFragment(IridiumFragment* frag)
       {
         debug("Received fragment %d/%d", frag->hdr.frag_id, frag->hdr.num_frags);
@@ -163,15 +267,19 @@ namespace Transports
         switch (ir_msg->msg_id)
         {
           case ID_IRIDIUMCMD:
+            handleIridiumCmd(static_cast<IridiumCommand*>(ir_msg));
             break;
 
           case (ID_DEVICEUPDATE):
+            handleUpdates(static_cast<DeviceUpdate*>(ir_msg)->positions);
             break;
 
           case (ID_EXTDEVUPDATE):
+            handleUpdates(static_cast<ExtendedDeviceUpdate*>(ir_msg)->positions);
             break;
 
           case (ID_IMCMESSAGE):
+            handleIMCMessage(static_cast<ImcIridiumMessage*>(ir_msg));
             break;
 
           case (ID_FRAGMENT):
