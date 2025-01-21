e0009d1ae74288b5e0014a4d5be7ae20675fd7a9
xBogas <119903183+xBogas@users.noreply.github.com>
2025-01-21 15:03:56 +0000

    vendor/libraries: Update cmake file.

--- vendor/libraries/RTIMULib/Library.cmake
+++ vendor/libraries/RTIMULib/Library.cmake
@@ -1,20 +1,9 @@
 
-# add_subdirectory(${PROJECT_SOURCE_DIR}/vendor/libraries/RTIMULib/RTIMULib)
-
 include_directories(${PROJECT_SOURCE_DIR}/vendor/libraries/RTIMULib/RTIMULib)
 include_directories(${PROJECT_SOURCE_DIR}/vendor/libraries/RTIMULib/RTIMULib/IMUDrivers)
 
-
 file(GLOB DUNE_RTIMU_CPP_FILES ${PROJECT_SOURCE_DIR}/vendor/libraries/RTIMULib/RTIMULib/*.cpp)
-
-# message(FATAL_ERROR "DUNE_RTIMU_CPP_FILES: ${DUNE_RTIMU_CPP_FILES}")
-
-file(GLOB DUNE_RTIMU_CPP_FILES_2 vendor/libraries/RTIMULib/RTIMULib/IMUDrivers/*.cpp)
+file(GLOB DUNE_RTIMU_CPP_DRIVERS vendor/libraries/RTIMULib/RTIMULib/IMUDrivers/*.cpp)
 
 
-# set_source_files_properties(${DUNE_TLSF_FILES}
-# PROPERTIES COMPILE_FLAGS "${DUNE_CXX_FLAGS} ${DUNE_CXX_FLAGS_STRICT}")
-
-list(APPEND DUNE_VENDOR_FILES ${DUNE_RTIMU_CPP_FILES} ${DUNE_RTIMU_CPP_FILES_2})
-
-message(WARNING "DUNE_VENDOR_FILES: ${DUNE_VENDOR_FILES}")
+list(APPEND DUNE_VENDOR_FILES ${DUNE_RTIMU_CPP_FILES} ${DUNE_RTIMU_CPP_DRIVERS})
