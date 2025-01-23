1e513f3e7b1ddc2500f348a25bb9b74d46d7cedd
Bernardo-Gabriel <bernardo.gabriel@lsts.pt>
2025-01-23 14:42:49 +0000

    Transports/UDP: ignore messages from external sources, to avoid redirecting messages that just arrived from outside.

--- src/Transports/UDP/Task.cpp
+++ src/Transports/UDP/Task.cpp
@@ -324,6 +324,9 @@ namespace Transports
       void
       consume(const IMC::Message* msg)
       {
+        if (msg->getSource() != getSystemId())
+          return;
+
         if (m_lcomms->isActive())
         {
           if (msg->getId() == DUNE_IMC_ESTIMATEDSTATE)
