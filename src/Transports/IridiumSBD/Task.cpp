c7a36f8a4e4d53a63bb66b959f05c5b35f46f3c1
Pedro Gonçalves <pedro@lsts.pt>
2025-02-13 14:44:51 +0000

    Transports/IridiumSBD: remove duplicated code.

--- src/Transports/IridiumSBD/Task.cpp
+++ src/Transports/IridiumSBD/Task.cpp
@@ -278,9 +278,6 @@ namespace Transports
           else
             m_uart = new SerialPort(m_args.uart_dev, m_args.uart_baud);
 
-          m_driver = new Driver(this, m_uart, m_args.use_9523, c_pwr_on_delay, m_args.rssi_check_period);
-          m_driver->initialize();
-          m_driver->setTxRateMax(m_args.max_tx_rate);
           IMC::VersionInfo vi;
           std::string version_model = "no libd-9523";
           m_driver = new Driver(this, m_uart, m_args.use_9523, c_pwr_on_delay, m_args.rssi_check_period);
