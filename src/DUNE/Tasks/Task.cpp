bbff092204a396f99264e6936f54fe7af9aa3dfc
Bernardo-Gabriel <bernardo.gabriel@lsts.pt>
2025-03-21 18:34:28 +0000

    DUNE/Tasks/Task: changed AMS directory.

--- src/DUNE/Tasks/Task.cpp
+++ src/DUNE/Tasks/Task.cpp
@@ -714,12 +714,11 @@ namespace DUNE
         if (pitr->first == "Enabled")
           continue;
 
-        // Ignore Supervisors.Delegator and Monitors.AcousticModemsSelector sections
+        // Ignore Supervisors.Delegator and UserInterfaces.AcousticModemsSelector sections
         std::string section = getName();
         std::string::size_type p = section.find('/');
-        if(!std::strcmp(section.substr(0,p).c_str(),"Supervisors.Delegator"))
-          continue;
-        if(!std::strcmp(section.substr(0,p).c_str(),"Monitors.AcousticModemsSelector"))
+        if(!std::strcmp(section.substr(0,p).c_str(),"Supervisors.Delegator") ||
+           !std::strcmp(section.substr(0,p).c_str(),"UserInterfaces.AcousticModemsSelector"))
           continue;
 
         if (m_params.find(pitr->first) == m_params.end())
