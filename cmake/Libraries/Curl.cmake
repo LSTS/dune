e25211f1a011448691d145996de56b4ed2dbce3b
xBogas <119903183+xBogas@users.noreply.github.com>
2025-04-03 14:21:59 +0100

    cmake/Libraries/Curl: Add dune_test_header for libcurl

--- cmake/Libraries/Curl.cmake
+++ cmake/Libraries/Curl.cmake
@@ -2,7 +2,7 @@
 find_package(CURL QUIET)
 
 if (CURL_FOUND)
-  message(STATUS "libcurl found.")
   include_directories(${CURL_INCLUDE_DIR})
+  dune_test_header(curl/curl.h)
   dune_add_lib(curl)
 endif()
