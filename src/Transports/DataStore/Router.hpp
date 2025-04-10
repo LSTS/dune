512ead6c6d13d600e0017c39509c35beaff7a4c9
Bernardo-Gabriel <bernardo.gabriel@lsts.pt>
2025-04-10 08:25:02 +0100

    Transports/DataStore: m_reqid is uint16_t, it already loops around 0xffff.

--- src/Transports/DataStore/Router.hpp
+++ src/Transports/DataStore/Router.hpp
@@ -221,14 +221,9 @@ namespace Transports
       }
 
       uint16_t
-      createInternalId(){
-        if(m_reqid==0xFFFF){
-          m_reqid=0;
-        }
-        else{
-          m_reqid++;
-        }
-        return m_reqid;
+      createInternalId(void)
+      {
+        return ++m_reqid;
       }
 
       void iridiumUpload(DataStore* store)
