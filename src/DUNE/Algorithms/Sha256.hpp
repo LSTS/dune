04b1f91311f728b973bfdf8a0685a7e519c55791
Luís Venâncio <lvenancio@inegi.up.pt>
2025-04-15 11:11:27 +0100

    DUNE/Algorithms/Sha256: Added class to compute sha256 hash.

---
+++ src/DUNE/Algorithms/Sha256.hpp
@@ -0,0 +1,205 @@
+//***************************************************************************
+// Copyright 2007-2025 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Faculdade de Engenharia da             *
+// Universidade do Porto. For licensing terms, conditions, and further      *
+// information contact lsts@fe.up.pt.                                       *
+//                                                                          *
+// Modified European Union Public Licence - EUPL v.1.1 Usage                *
+// Alternatively, this file may be used under the terms of the Modified     *
+// EUPL, Version 1.1 only (the "Licence"), appearing in the file LICENCE.md *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: Luis Venâncio                                                    *
+//***************************************************************************
+
+#ifndef DUNE_ALGORITHMS_SHA256_HPP_INCLUDED_
+#define DUNE_ALGORITHMS_SHA256_HPP_INCLUDED_
+
+// ISO C++ 98 headers.
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+// DUNE headers.
+#include <DUNE/Config.hpp>
+
+namespace DUNE
+{
+  namespace Algorithms
+  {
+    // Export DLL Symbol.
+    class DUNE_DLL_SYM Sha256;
+
+    //! Base64 encoding and decoding as defined by RFC 2045.
+    class Sha256
+    {
+    public:
+      Sha256()
+      {
+        reset();
+      }
+
+      void
+      update(const uint8_t* data, size_t length)
+      {
+        while (length--)
+        {
+          dataBuffer[dataLength++] = *(data++);
+          if (dataLength == 64)
+          {
+            transform();
+            bitLength += 512;
+            dataLength = 0;
+          }
+        }
+      }
+
+      void
+      update(const std::string& data)
+      {
+        update(reinterpret_cast<const uint8_t*>(data.c_str()), data.size());
+      }
+
+      std::string
+      digest()
+      {
+        int64_t totalBits = bitLength + dataLength * 8;
+        dataBuffer[dataLength++] = 0x80;
+
+        if (dataLength > 56)
+        {
+          while (dataLength < 64)
+            dataBuffer[dataLength++] = 0x00;
+          transform();
+          dataLength = 0;
+        }
+
+        while (dataLength < 56)
+          dataBuffer[dataLength++] = 0x00;
+
+        for (int i = 7; i >= 0; --i)
+          dataBuffer[dataLength++] = totalBits >> (i * 8);
+
+        transform();
+
+        std::stringstream ss;
+        for (int i = 0; i < 8; ++i)
+          ss << std::hex << std::setw(8) << std::setfill('0') << state[i];
+
+        return ss.str();
+      }
+
+    private:
+      int8_t dataBuffer[64];
+      int32_t state[8];
+      int32_t dataLength;
+      int64_t bitLength;
+
+      void
+      reset()
+      {
+        dataLength = 0;
+        bitLength = 0;
+        state[0] = 0x6a09e667;
+        state[1] = 0xbb67ae85;
+        state[2] = 0x3c6ef372;
+        state[3] = 0xa54ff53a;
+        state[4] = 0x510e527f;
+        state[5] = 0x9b05688c;
+        state[6] = 0x1f83d9ab;
+        state[7] = 0x5be0cd19;
+      }
+
+      void
+      transform()
+      {
+        static const int32_t k[64] = {
+          0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4,
+          0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe,
+          0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f,
+          0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+          0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc,
+          0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+          0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116,
+          0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+          0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7,
+          0xc67178f2
+        };
+
+        int32_t m[64];
+        for (int i = 0; i < 16; ++i)
+        {
+          m[i] = (dataBuffer[i * 4] << 24) | (dataBuffer[i * 4 + 1] << 16)
+                 | (dataBuffer[i * 4 + 2] << 8) | (dataBuffer[i * 4 + 3]);
+        }
+
+        for (int i = 16; i < 64; ++i)
+        {
+          int32_t s0 = rotr(m[i - 15], 7) ^ rotr(m[i - 15], 18) ^ (m[i - 15] >> 3);
+          int32_t s1 = rotr(m[i - 2], 17) ^ rotr(m[i - 2], 19) ^ (m[i - 2] >> 10);
+          m[i] = m[i - 16] + s0 + m[i - 7] + s1;
+        }
+
+        int32_t a = state[0];
+        int32_t b = state[1];
+        int32_t c = state[2];
+        int32_t d = state[3];
+        int32_t e = state[4];
+        int32_t f = state[5];
+        int32_t g = state[6];
+        int32_t h = state[7];
+
+        for (int i = 0; i < 64; ++i)
+        {
+          int32_t S1 = rotr(e, 6) ^ rotr(e, 11) ^ rotr(e, 25);
+          int32_t ch = (e & f) ^ (~e & g);
+          int32_t temp1 = h + S1 + ch + k[i] + m[i];
+          int32_t S0 = rotr(a, 2) ^ rotr(a, 13) ^ rotr(a, 22);
+          int32_t maj = (a & b) ^ (a & c) ^ (b & c);
+          int32_t temp2 = S0 + maj;
+
+          h = g;
+          g = f;
+          f = e;
+          e = d + temp1;
+          d = c;
+          c = b;
+          b = a;
+          a = temp1 + temp2;
+        }
+
+        state[0] += a;
+        state[1] += b;
+        state[2] += c;
+        state[3] += d;
+        state[4] += e;
+        state[5] += f;
+        state[6] += g;
+        state[7] += h;
+      }
+
+      inline int32_t
+      rotr(int32_t x, int32_t n)
+      {
+        return (x >> n) | (x << (32 - n));
+      }
+    };
+  }
+}
+
+#endif
