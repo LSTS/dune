6ccc58501773a5ce1a9c155437aa6b5707bed6e5
Luís Venâncio <lvenancio@inegi.up.pt>
2025-04-17 10:01:11 +0100

    Monitors/Emergency: Moved emergency message generation to DUNE/Utils.

--- src/Monitors/Emergency/Task.cpp
+++ src/Monitors/Emergency/Task.cpp
@@ -61,28 +61,18 @@ namespace Monitors
 
     struct Task: public DUNE::Tasks::Periodic
     {
-      //! Emergency message.
-      std::string m_emsg;
-      //! Fuel level.
-      float m_fuel;
-      //! Confidence in fuel level.
-      float m_fuel_conf;
-      //! Batteries voltage
-      float m_bat_voltage;
       //! True if executing plan.
       bool m_in_mission;
-      //! Executing plan's progress.
-      float m_progress;
       //! Iridium request identifier.
       unsigned m_req;
-      //! Vehicle State
-      uint8_t m_vstate;
       //! Lost communications timer.
       Counter<double> m_lost_coms_timer;
       //! Medium handler.
       DUNE::Monitors::MediumHandler m_hand;
       //! Reporter API.
       Supervisors::Reporter::Client* m_reporter;
+      // Emergency message generator.
+      Utils::EmergencyMessage* m_emsg;
       //! Task arguments.
       Arguments m_args;
 
@@ -137,7 +127,6 @@ namespace Monitors
         bind<IMC::PlanControlState>(this);
         bind<IMC::ReportControl>(this);
         bind<IMC::VehicleMedium>(this);
-        bind<IMC::TextMessage>(this);
         bind<IMC::VehicleState>(this);
         bind<IMC::Voltage>(this);
       }
@@ -153,6 +142,7 @@ namespace Monitors
       onResourceRelease(void)
       {
         Memory::clear(m_reporter);
+        Memory::clear(m_emsg);
       }
 
       void
@@ -160,6 +150,12 @@ namespace Monitors
       {
         m_reporter = new Supervisors::Reporter::Client(this, Supervisors::Reporter::IS_GSM,
                                                        2.0, false);
+
+        // Initialize emergency message generator.
+        std::string system_name = getSystemName();
+        if(!m_args.external_info_text.empty())
+          system_name += String::str(" - ") + m_args.external_info_text;
+        m_emsg = new Utils::EmergencyMessage(system_name);
       }
 
       void
@@ -172,11 +168,6 @@ namespace Monitors
           setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_IDLE);
 
         m_lost_coms_timer.setTop(m_args.heartbeat_tout);
-        m_fuel = -1.0;
-        m_fuel_conf = -1.0;
-        m_bat_voltage = -1.0;
-        m_progress = -1.0;
-        m_vstate = '?';
       }
 
       void
@@ -196,28 +187,7 @@ namespace Monitors
       {
         if (msg->validity & IMC::GpsFix::GFV_VALID_POS)
         {
-          int lat_deg;
-          double lat_min;
-          Angles::convertDecimalToDM(Angles::degrees(msg->lat), lat_deg, lat_min);
-
-          int lon_deg;
-          double lon_min;
-          Angles::convertDecimalToDM(Angles::degrees(msg->lon), lon_deg, lon_min);
-
-          Time::BrokenDown bdt;
-          std::string system_name;
-          if(m_args.external_info_text.empty())
-            system_name = getSystemName();
-          else
-            system_name = getSystemName() + String::str(" - ") +m_args.external_info_text;
-
-          m_emsg = String::str("(%s) %02u:%02u:%02u / %d %f, %d %f / f:%d v:%d c:%d / s: %c",
-                               system_name.c_str(),
-                               bdt.hour, bdt.minutes, bdt.seconds,
-                               lat_deg, lat_min, lon_deg, lon_min,
-                               (int)m_fuel, (int) m_bat_voltage, (int)m_fuel_conf, vehicleStateChar(m_vstate));
-
-          m_emsg += m_in_mission ? String::str(" / p:%d", (int)m_progress) : "";
+          m_emsg->update(msg);
         }
       }
 
@@ -226,34 +196,6 @@ namespace Monitors
       {
         if (m_reporter != NULL)
           m_reporter->consume(msg);
-      }
-
-      void
-      consume(const IMC::TextMessage* msg)
-      {
-        spew("processing text message from %s: \"%s\"", msg->origin.c_str(), sanitize(msg->text).c_str());
-        std::istringstream iss(msg->text);
-        std::string cmd, args = "";
-        getline(iss, cmd, ' ');
-        if (iss.str().size() > cmd.size())
-          args = std::string(iss.str(), cmd.size() + 1, iss.str().size() - cmd.size() + 1);
-
-        spew("command is %s, args are %s", cmd.c_str(), args.c_str());
-
-        String::toLowerCase(cmd);
-        String::toLowerCase(args);
-
-        if (cmd == "gsm")
-        {
-          if (args == "true")
-            requestActivation();
-
-          if (args == "false")
-            requestDeactivation();
-        }
-
-        if (cmd == "pos")
-          sendSMS("T", m_args.sms_lost_coms_ttl, msg->origin);
       }
 
       void
@@ -279,8 +221,7 @@ namespace Monitors
       void
       consume(const IMC::FuelLevel* msg)
       {
-        m_fuel = msg->value;
-        m_fuel_conf = msg->confidence;
+        m_emsg->update(msg);
       }
 
       void
@@ -290,7 +231,7 @@ namespace Monitors
         {
           if(msg->getSourceEntity() != resolveEntity("Batteries"))
             return;
-          m_bat_voltage = msg->value * 10;
+          m_emsg->update(msg);
         }
         catch (std::runtime_error& e) {
           spew("Batteries entity is not present.");
@@ -301,7 +242,7 @@ namespace Monitors
       consume(const IMC::PlanControlState* msg)
       {
         m_in_mission = msg->state == IMC::PlanControlState::PCS_EXECUTING;
-        m_progress = msg->plan_progress;
+        m_emsg->update(msg);
       }
 
       void
@@ -316,29 +257,7 @@ namespace Monitors
       void
       consume(const IMC::VehicleState* msg)
       {
-        m_vstate = msg->op_mode;
-      }
-
-      char
-      vehicleStateChar(const uint8_t vstate)
-      {
-        switch((IMC::VehicleState::OperationModeEnum) vstate)
-        {
-          case IMC::VehicleState::VS_BOOT:
-            return 'B';
-          case IMC::VehicleState::VS_CALIBRATION:
-            return 'C';
-          case IMC::VehicleState::VS_ERROR:
-            return 'E';
-          case IMC::VehicleState::VS_EXTERNAL:
-            return 'X';
-          case IMC::VehicleState::VS_MANEUVER:
-            return 'M';
-          case IMC::VehicleState::VS_SERVICE:
-            return 'S';
-          default:
-            return '?';
-        }
+        m_emsg->update(msg);
       }
 
       //! Send SMS request.
@@ -350,31 +269,7 @@ namespace Monitors
       {
         IMC::TransmissionRequest msg;
         msg.data_mode= IMC::TransmissionRequest::DMODE_TEXT;
-
-        if (!m_emsg.empty())
-        {
-          msg.txt_data = String::str("(%s) %s", prefix, m_emsg.c_str());
-        }
-        else
-        {
-          std::string s;
-          Time::BrokenDown bdt;
-          std::string system_name;
-          if(m_args.external_info_text.empty())
-            system_name = getSystemName();
-          else
-            system_name = getSystemName() + String::str(" - ") +m_args.external_info_text;
-
-          s = String::str("(%s) %02u:%02u:%02u / Unknown Location / f:%d v:%d c:%d",
-                               system_name.c_str(),
-                               bdt.hour, bdt.minutes, bdt.seconds,
-                            (int)m_fuel, (int)m_bat_voltage, (int)m_fuel_conf);
-
-          s += String::str(" / s: %c", vehicleStateChar(m_vstate));
-          s += m_in_mission ? String::str(" / p:%d", (int)m_progress) : "";
-
-          msg.txt_data = String::str("(%s) %s", prefix, s.c_str());
-        }
+        msg.txt_data = m_emsg->get(prefix);
 
         msg.setDestination(getSystemId());
         msg.setDestinationEntity(getEntityId());
