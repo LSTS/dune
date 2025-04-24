0ad776c24150725ade3cfe20f3d523f3e5e1346e
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2025-04-24 18:24:27 +0100

    Sensors/GPS: correctly update BasicDeviceDriver parameters and use BasicDeviceDriver base arguments for power channel and post power on delay.

--- src/Sensors/GPS/Task.cpp
+++ src/Sensors/GPS/Task.cpp
@@ -81,10 +81,6 @@ namespace Sensors
       std::string init_cmds[c_max_init_cmds];
       //! Initialization replies.
       std::string init_rpls[c_max_init_cmds];
-      //! Power channels.
-      std::vector<std::string> pwr_channels;
-      //! Power on delay
-      double post_pwr_on_delay;
       //! Enable novatel sbas mode.
       bool novatelSbas;
     };
@@ -136,14 +132,6 @@ namespace Sensors
         .minimumValue("0.0")
         .description("Input timeout");
 
-        param("Power Channel - Names", m_args.pwr_channels)
-        .defaultValue("")
-        .description("Device's power channels");
-
-        param("Post Power On Delay", m_args.post_pwr_on_delay)
-        .defaultValue("0.0")
-        .description("Delay on power on before attempts to connect");
-
         param("Sentence Order", m_args.stn_order)
         .defaultValue("")
         .description("Sentence order");
@@ -176,17 +164,7 @@ namespace Sensors
       void
       onUpdateParameters(void)
       {
-        if (paramChanged(m_args.pwr_channels))
-        {
-          clearPowerChannelNames();
-          for (std::string pc : m_args.pwr_channels)
-            addPowerChannelName(pc);
-        }
-
-        if (paramChanged(m_args.post_pwr_on_delay))
-        {
-          setPostPowerOnDelay(m_args.post_pwr_on_delay);
-        }
+        BasicDeviceDriver::onUpdateParameters();
       }
 
       //! Try to connect to the device.
