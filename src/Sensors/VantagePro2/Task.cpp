dd2144911dc328c5436f35336b7fb1e65c24782b
Luís Venâncio <lvenancio@inegi.up.pt>
2025-05-21 09:16:50 +0100

    Sensors/VantagePro2: Added AbsoluteWind message. Added wind statistics (average and maximum). Fixed code style.

--- src/Sensors/VantagePro2/Task.cpp
+++ src/Sensors/VantagePro2/Task.cpp
@@ -29,9 +29,9 @@
 //***************************************************************************
 
 // ISO C++ 98 headers.
-#include <string>
 #include <cstring>
 #include <stdio.h>
+#include <string>
 
 // DUNE headers.
 #include <DUNE/DUNE.hpp>
@@ -43,7 +43,7 @@ namespace Sensors
 {
   //! This task reads data from the Davis Instruments Vantage Pro2 Weather Station.
   //!
-  //! Periodicaly is sent a LOOP command to the serial data logger. It returns the 
+  //! Periodicaly is sent a LOOP command to the serial data logger. It returns the
   //! temperature, humidity, wind and rain values measured by the weather station.
   //! @author Joao Costa
   namespace VantagePro2
@@ -74,12 +74,14 @@ namespace Sensors
       std::string uart_dev;
       //! Serial port baud rate.
       unsigned uart_baud;
+      //! Absolute wind statistics interval.
+      double stats_interval;
     };
 
-    struct Task : public DUNE::Tasks::Task
+    struct Task: public DUNE::Tasks::Task
     {
       //! Serial port handle.
-      SerialPort *m_uart;
+      SerialPort* m_uart;
       //! Command to be sent via usart.
       char m_cmd[c_max_buffer];
       //! Serial input buffer.
@@ -87,7 +89,7 @@ namespace Sensors
       //! I/O Multiplexer.
       Poll m_poll;
       //! Parser
-      Parser *m_parse;
+      Parser* m_parse;
       //! Serial buffer in
       uint8_t m_bfr[c_max_buffer];
       //! Current state machine's state
@@ -104,41 +106,73 @@ namespace Sensors
       bool m_init_loop;
       //! Task arguments.
       Arguments m_args;
+      //! Current estimated state.
+      IMC::EstimatedState m_estate;
+      //! Absolute wind statistics.
+      //! Absolute wind statistics timer.
+      Time::Counter<double> m_abs_wind_stats_timer;
+      //! Absolute wind speed cumulative.
+      double m_abs_wind_spd_cum;
+      //! Absolute wind direction cumulative.
+      Math::CircularMean<double> m_abs_wind_dir_avg;
+      //! Absolute wind maximum speed.
+      double m_abs_wind_max_spd;
+      //! Absolute wind maximum speed direction.
+      double m_abs_wind_max_spd_dir;
+      //! Absolute Wind average entity id.
+      unsigned int m_abs_wind_avg_eid;
+      //! Absolute Wind max entity id.
+      unsigned int m_abs_wind_max_eid;
+
 
       //! Constructor.
       //! @param [in] name task name.
       //! @param [in] ctx context.
-      Task(const std::string &name, Tasks::Context &ctx) : DUNE::Tasks::Task(name, ctx),
-                                                           m_LOOP_sent(false),
-                                                           m_init_station(true),
-                                                           m_init_loop(false)
+      Task(const std::string& name, Tasks::Context& ctx):
+        DUNE::Tasks::Task(name, ctx),
+        m_uart(nullptr),
+        m_parse(nullptr),
+        m_LOOP_sent(false),
+        m_init_station(true),
+        m_init_loop(false),
+        m_abs_wind_spd_cum(0.0),
+        m_abs_wind_max_spd(0.0),
+        m_abs_wind_max_spd_dir(0.0)
       {
         param("Serial Port - Device", m_args.uart_dev)
-            .defaultValue("/dev/ttyUSB0")
-            .description("Serial port used to communicate with the meteo station");
+          .defaultValue("/dev/ttyUSB0")
+          .description("Serial port used to communicate with the meteo station");
 
         param("Serial Port - Baud Rate", m_args.uart_baud)
-            .defaultValue("19200")
-            .description("Serial port baudrate");
+          .defaultValue("19200")
+          .description("Serial port baudrate");
+
+        param("Absolute Wind Statistics Interval", m_args.stats_interval)
+          .units(Units::Second)
+          .defaultValue("900.0")
+          .description("Interval to compute absolute wind statistics. Default is 15 min.");
+
+        bind<IMC::EstimatedState>(this);
       }
 
       //! Update internal state with new parameter values.
       void
       onUpdateParameters(void)
-      {
-      }
+      { }
 
       //! Reserve entity identifiers.
       void
       onEntityReservation(void)
       {
+        std::string label = getEntityLabel();
+        m_abs_wind_avg_eid = reserveEntity(label + " - Average");
+        m_abs_wind_max_eid = reserveEntity(label + " - Maximum");
       }
 
       //! Resolve entity names.
       void
       onEntityResolution(void)
-      {
-      }
+      { }
 
       //! Acquire resources.
       void
@@ -152,7 +186,7 @@ namespace Sensors
 
           m_poll.add(*m_uart);
         }
-        catch (std::runtime_error &e)
+        catch (std::runtime_error& e)
         {
           throw RestartNeeded(e.what(), 10);
         }
@@ -170,7 +204,9 @@ namespace Sensors
         m_timer.setTop(2);
 
         // Wake up console
-        //m_uart->writeString("\n\r");
+        // m_uart->writeString("\n\r");
+
+        m_abs_wind_stats_timer.setTop(m_args.stats_interval);
       }
 
       //! Release resources.
@@ -182,6 +218,12 @@ namespace Sensors
           m_poll.remove(*m_uart);
           Memory::clear(m_uart);
         }
+      }
+
+      void
+      consume(const IMC::EstimatedState* msg)
+      {
+        m_estate = *msg;
       }
 
       //! Read data sent by Doris Sampler.
@@ -196,7 +238,7 @@ namespace Sensors
           {
             m_num_bytes = m_uart->read(m_bfr, sizeof(m_bfr));
           }
-          catch (std::exception &e)
+          catch (std::exception& e)
           {
             err(DTR("read error: %s"), e.what());
             return false;
@@ -227,6 +269,107 @@ namespace Sensors
         return false;
       }
 
+      //! Send new data to bus
+      void
+      dispatchData(void)
+      {
+        IMC::Temperature temp;
+        temp.value = ((float)m_parse->m_LOOPData.OutsideTemp / 10 - 32) * 5 / 9;
+        debug("temperature = %.2f", temp.value);
+
+        IMC::WindSpeed wind_speed;
+        wind_speed.speed = m_parse->m_LOOPData.WindSpeed;
+        wind_speed.direction = Angles::radians(m_parse->m_LOOPData.WindDir);
+        debug("wind speed = %.2f", wind_speed.speed);
+        debug("wind direction = %.2f", wind_speed.direction);
+
+        IMC::AbsoluteWind abs_wind;
+        toAbsoluteWind(wind_speed, abs_wind);
+        debug("absolute wind direction = %.2f", abs_wind.direction);
+        debug("absolute wind speed = %.2f", abs_wind.speed);
+
+        IMC::RelativeHumidity humidity;
+        humidity.value = m_parse->m_LOOPData.OutsideHum;
+        debug("outside humidity = %.2f", humidity.value);
+
+        Delay::wait(0.2);
+
+        dispatch(temp);
+        dispatch(wind_speed);
+        dispatch(abs_wind);
+        dispatch(humidity);
+
+        // Absolute wind statistics
+        m_abs_wind_spd_cum += abs_wind.speed;
+        m_abs_wind_dir_avg.update(abs_wind.direction);
+        if (abs_wind.speed > m_abs_wind_max_spd)
+        {
+          m_abs_wind_max_spd = abs_wind.speed;
+          m_abs_wind_max_spd_dir = abs_wind.direction;
+        }
+
+        if (m_abs_wind_stats_timer.overflow())
+        {
+          // Average and Maximum messages
+          IMC::AbsoluteWind aw_avg;
+          aw_avg.setSourceEntity(m_abs_wind_avg_eid);
+          aw_avg.speed = m_abs_wind_spd_cum / m_abs_wind_dir_avg.sampleSize();
+          aw_avg.direction = m_abs_wind_dir_avg.mean();
+          IMC::AbsoluteWind aw_max;
+          aw_max.setSourceEntity(m_abs_wind_max_eid);
+          aw_max.speed = m_abs_wind_max_spd;
+          aw_max.direction = m_abs_wind_max_spd_dir;
+
+          dispatch(aw_avg, DF_KEEP_SRC_EID);
+          dispatch(aw_max, DF_KEEP_SRC_EID);
+
+          // Vertical Profile messages
+          IMC::VerticalProfile vprofile;
+          IMC::ProfileSample ps;
+          
+          double lat, lon;
+          toWGS84(m_estate, lat, lon);
+          vprofile.lat = Angles::degrees(lat);
+          vprofile.lon = Angles::degrees(lon);
+          vprofile.numsamples = m_abs_wind_dir_avg.sampleSize();
+          // vprofile.parameter = IMC::VerticalProfile::PROF_ABSOLUTE_WIND;
+
+          ps.depth = aw_avg.direction;
+          ps.avg = aw_avg.speed;
+          vprofile.samples.push_back(ps);
+          dispatch(vprofile);
+
+          // Reset statistics
+          m_abs_wind_spd_cum = 0.0;
+          m_abs_wind_dir_avg.clear();
+          m_abs_wind_stats_timer.setTop(m_args.stats_interval);
+        }
+
+      }
+
+      void
+      toAbsoluteWind(const IMC::WindSpeed& ws, IMC::AbsoluteWind& aw)
+      {
+        struct
+        {
+          double x;
+          double y;
+        } relative_components = { 0.0, 0.0 }, absolute_components = { 0.0, 0.0 };
+        double dummy = 0.0;
+
+        // Remove vehicle velocity and convert to inertial frame
+        Coordinates::displace(relative_components, ws.direction, ws.speed);
+        relative_components.x -= m_estate.u;
+        relative_components.y -= m_estate.v;
+        Coordinates::BodyFixedFrame::toInertialFrame(m_estate.phi, m_estate.theta, m_estate.psi,
+                                                     relative_components.x, relative_components.y, 0.0,
+                                                     &absolute_components.x, &absolute_components.y, &dummy);
+
+        
+        aw.direction = std::atan2(absolute_components.y, absolute_components.x);
+        aw.speed = Math::norm(absolute_components.x, absolute_components.y);
+      }
+
       //! Creates a Log Book Entry with the VP2's firmware version
       //! @return Returns 1 if got version successfully
       /*void
@@ -250,17 +393,17 @@ namespace Sensors
             if (m_init_station)
             {
               m_LOOP_sent = false;
-              strcpy(m_cmd, "NVER\n"); // make Davis cmd string
+              strcpy(m_cmd, "NVER\n");  // make Davis cmd string
             }
             else
             {
               m_LOOP_sent = true;
-              strcpy(m_cmd, "LOOP 1\n"); // make Davis cmd string
+              strcpy(m_cmd, "LOOP 1\n");  // make Davis cmd string
             }
 
             m_uart->writeString(m_cmd);
             m_timer.reset();
-            //war("NVER");
+            // war("NVER");
           }
 
           if (m_poll.poll(0.01))
@@ -271,25 +414,7 @@ namespace Sensors
               {
                 if (m_LOOP_sent)
                 {
-                  IMC::Temperature m_temp;
-                  m_temp.value = ((float)m_parse->m_LOOPData.OutsideTemp / 10 - 32) * 5/9;
-                  debug("temperature = %.2f", m_temp.value);
-                  
-                  IMC::WindSpeed m_windSpd;
-                  m_windSpd.speed = m_parse->m_LOOPData.WindSpeed;
-                  m_windSpd.direction = Angles::radians(m_parse->m_LOOPData.WindDir);
-                  debug("wind speed = %.2f", m_windSpd.speed);
-                  debug("wind direction = %.2f", m_windSpd.direction);
-                  
-                  IMC::RelativeHumidity m_humidity;
-                  m_humidity.value = m_parse->m_LOOPData.OutsideHum;
-                  debug("outside humidity = %.2f", m_humidity.value);
-
-                  Delay::wait(0.2);
-
-                  dispatch(m_temp);
-                  dispatch(m_windSpd);
-                  dispatch(m_humidity);
+                  dispatchData();
                 }
                 else
                 {
