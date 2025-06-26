97ea53f5ead09bb58132e812c9c48d83aede952f
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2025-06-26 19:34:46 +0100

    Revert "DUNE/Timer/Counter: Counter is now compatible with any variable type."
    This reverts commit 475947775058eafe41a75155cb7891e0a80b5a02.

--- src/DUNE/Time/Counter.hpp
+++ src/DUNE/Time/Counter.hpp
@@ -74,7 +74,7 @@ namespace DUNE
       inline T
       reset(void)
       {
-        m_last = Clock::get();
+        m_last = static_cast<T>(Clock::get());
         m_overflow = false;
         return m_last;
       }
@@ -113,7 +113,7 @@ namespace DUNE
       //! Top value.
       T m_top;
       //! Time of last reset.
-      double m_last;
+      T m_last;
       //! True if the counter overflowed.
       bool m_overflow;
     };
