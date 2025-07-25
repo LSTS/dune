94ff8170626053c4f973c347fa91d5b8b2f3d038
Luís Venâncio <lvenancio@inegi.up.pt>
2025-07-25 11:37:30 +0100

    DUNE/Utils: add smart pointer handling

--- src/DUNE/Utils/SmartPointers.hpp
+++ src/DUNE/Utils/SmartPointers.hpp
@@ -1,5 +1,5 @@
 //***************************************************************************
-// Copyright 2007-2025 Universidade do Porto - Faculdade de Engenharia      *
+// Copyright 2007-2015 Universidade do Porto - Faculdade de Engenharia      *
 // Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
 //***************************************************************************
 // This file is part of DUNE: Unified Navigation Environment.               *
@@ -8,49 +8,37 @@
 // Licencees holding valid commercial DUNE licences may use this file in    *
 // accordance with the commercial licence agreement provided with the       *
 // Software or, alternatively, in accordance with the terms contained in a  *
-// written agreement between you and Faculdade de Engenharia da             *
-// Universidade do Porto. For licensing terms, conditions, and further      *
-// information contact lsts@fe.up.pt.                                       *
+// written agreement between you and Universidade do Porto. For licensing   *
+// terms, conditions, and further information contact lsts@fe.up.pt.        *
 //                                                                          *
-// Modified European Union Public Licence - EUPL v.1.1 Usage                *
-// Alternatively, this file may be used under the terms of the Modified     *
-// EUPL, Version 1.1 only (the "Licence"), appearing in the file LICENCE.md *
+// European Union Public Licence - EUPL v.1.1 Usage                         *
+// Alternatively, this file may be used under the terms of the EUPL,        *
+// Version 1.1 only (the "Licence"), appearing in the file LICENCE.md       *
 // included in the packaging of this file. You may not use this work        *
 // except in compliance with the Licence. Unless required by applicable     *
 // law or agreed to in writing, software distributed under the Licence is   *
 // distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
 // ANY KIND, either express or implied. See the Licence for the specific    *
 // language governing permissions and limitations at                        *
-// https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
 // http://ec.europa.eu/idabc/eupl.html.                                     *
 //***************************************************************************
-// Author: Ricardo Martins                                                  *
+// Copyright 2023 EvoLogics GmbH                                            *
+//***************************************************************************
+// Author: Ievgenii Glushko                                                 *
 //***************************************************************************
 
-#ifndef DUNE_UTILS_HPP_INCLUDED_
-#define DUNE_UTILS_HPP_INCLUDED_
+#ifndef DUNE_UTILS_SMART_POINTERS_HPP_INCLUDED_
+#define DUNE_UTILS_SMART_POINTERS_HPP_INCLUDED_
 
-namespace DUNE
-{
-  //! General purpose types and routines
-  namespace Utils
-  { }
-}
+#include <memory>
 
-#include <DUNE/Utils/ByteBuffer.hpp>
-#include <DUNE/Utils/ByteCopy.hpp>
-#include <DUNE/Utils/CircularBuffer.hpp>
-#include <DUNE/Utils/EmergencyMessage.hpp>
-#include <DUNE/Utils/Exceptions.hpp>
-#include <DUNE/Utils/OptionParser.hpp>
-#include <DUNE/Utils/RawFifo.hpp>
-#include <DUNE/Utils/StateMachine.hpp>
-#include <DUNE/Utils/String.hpp>
-#include <DUNE/Utils/TupleList.hpp>
-#include <DUNE/Utils/Utils.hpp>
-#include <DUNE/Utils/XML.hpp>
-#include <DUNE/Utils/Codecs.hpp>
-#include <DUNE/Utils/Actuators.hpp>
-#include <DUNE/Utils/LineParser.hpp>
-#include <DUNE/Utils/SmartPointers.hpp>
+#if __cplusplus >= 201103L || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
+#   define MOVE_PTR(PTR) std::move(PTR)
+#else
+#   define MOVE_PTR(PTR) (PTR)
+#   define nullptr 0
 #endif
+
+#endif
+
+
