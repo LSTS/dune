15e42b61886de315860b564407991a330b35a4a7
Lubern1 <lufezia@hotmail.it>
2025-11-18 12:07:24 +0000

    Transports/DCCL: Added the following payoad: FLUOROMETERS, ADCP, DISSOLVED OXYGEN, CTD; HEADING CONTROLLER and relative paramteres

--- src/Transports/DCCL/Task.cmake
+++ src/Transports/DCCL/Task.cmake
@@ -4,7 +4,7 @@ if(DUNE_SYS_HAS_DCCL AND DUNE_SYS_HAS_PROTOBUF)
   file(GLOB DATAMODEL_PROTOS "${CMAKE_CURRENT_LIST_DIR}/Proto/*.proto")
   foreach(proto ${DATAMODEL_PROTOS})
     file(TO_NATIVE_PATH ${proto} proto_native)
-    execute_process(COMMAND "/usr/bin/protoc" --proto_path=${PROTOMODEL_PATH} --cpp_out=${PROTOBINDING_PATH} ${proto_native} RESULT_VARIABLE rv)
+    execute_process(COMMAND "/usr/bin/protoc" --proto_path=${PROTOMODEL_PATH} --proto_path=/usr/local/include --cpp_out=${PROTOBINDING_PATH} ${proto_native} RESULT_VARIABLE rv)
     #oss: /usr/bin/protoc if this is the exact location of this file. It should be this one. 
     # ${PROTOBUF_PROTOC_EXECUTABLE} previously was used this cache variable but found problem, not setted
     if(${rv})
