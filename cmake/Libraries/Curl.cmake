5600607e094983b489b45efc9671ab31c49e8b8f
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2025-11-28 18:04:41 +0000

    cmake/Libraries/Curl: remove Curl for DUNELEGACY.

--- cmake/Libraries/Curl.cmake
+++ cmake/Libraries/Curl.cmake
@@ -1,10 +1,12 @@
-# Find the libcurl package
-if(NOT DEFINED CROSS)
-  find_package(CURL QUIET)
+if(DUNELEGACY)
+  return()
+endif()
 
-  if(CURL_FOUND)
-    include_directories(${CURL_INCLUDE_DIR})
-    dune_test_header(curl/curl.h)
-    dune_add_lib(curl)
-  endif(CURL_FOUND)
-endif(NOT DEFINED CROSS)
+# Find the libcurl package
+find_package(CURL QUIET)
+
+if(CURL_FOUND)
+  include_directories(${CURL_INCLUDE_DIR})
+  dune_test_header(curl/curl.h)
+  dune_add_lib(curl)
+endif(CURL_FOUND)
