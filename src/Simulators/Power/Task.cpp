c86f60bdcc3987517b85bbbdf4bb3e4b38a36567
Pedro Gonçalves <pedro@lsts.pt>
2026-01-13 11:08:03 +0000

    Simulators/Power: add scheduling capabilities.

--- src/Simulators/Power/Task.cpp
+++ src/Simulators/Power/Task.cpp
@@ -46,6 +46,18 @@ namespace Simulators
     // Maximum number of power channels.
     static const int c_max_power_channel = 12;
 
+    //! Power Channel data structure.
+    struct PowerChannel
+    {
+      IMC::PowerChannelState pcs_msg;
+      unsigned id = 0;
+      double sched_time_in_seconds;
+      bool is_sched_off;
+      bool is_sched_on;
+      bool is_scheduled_active;
+      Counter<double> sched_timer;
+    };
+
     //! %Task arguments.
     struct Arguments
     {
@@ -68,7 +80,7 @@ namespace Simulators
       //! Flag to control no power channel to simulate.
       bool m_no_power_channel;
       //! IMC Power Channel messages.
-      IMC::PowerChannelState m_power_channels[c_max_power_channel];
+      PowerChannel m_power_channels[c_max_power_channel];
       //! IMC Voltage messages.
       IMC::Voltage m_voltage_msgs[c_max_power_channel];
       //! IMC Current messages.
@@ -141,10 +153,10 @@ namespace Simulators
           for (int i = 0; i < (int)m_args.power_channel_names.size(); ++i)
           {
             unsigned int eid = getEid(m_args.power_channel_names[i]);
-            m_power_channels[i].setSourceEntity(eid);
-            m_power_channels[i].name = m_args.power_channel_names[i];
-            m_power_channels[i].state = IMC::PowerChannelState::PCS_ON;
-            inf("Power channel entity '%s' reserved with id %u.", m_power_channels[i].name.c_str(),
+            m_power_channels[i].pcs_msg.setSourceEntity(eid);
+            m_power_channels[i].pcs_msg.name = m_args.power_channel_names[i];
+            m_power_channels[i].pcs_msg.state = IMC::PowerChannelState::PCS_ON;
+            inf("Power channel entity '%s' reserved with id %u.", m_power_channels[i].pcs_msg.name.c_str(),
                 eid);
             // Voltage message
             m_voltage_msgs[i].setSourceEntity(eid);
@@ -187,7 +199,7 @@ namespace Simulators
         (void)msg;
         for (int i = 0; i < (int)m_args.power_channel_names.size(); ++i)
         {
-          dispatch(m_power_channels[i]);
+          dispatch(m_power_channels[i].pcs_msg);
         }
       }
 
@@ -196,35 +208,67 @@ namespace Simulators
       {
         for (int i = 0; i < (int)m_args.power_channel_names.size(); ++i)
         {
-          if (msg->name == m_power_channels[i].name)
+          if (msg->name == m_power_channels[i].pcs_msg.name)
           {
             switch (msg->op)
             {
               case IMC::PowerChannelControl::PCC_OP_TURN_OFF:
-                m_power_channels[i].state = IMC::PowerChannelState::PCS_OFF;
-                inf("Power channel '%s' turned OFF.", m_power_channels[i].name.c_str());
+                m_power_channels[i].pcs_msg.state = IMC::PowerChannelState::PCS_OFF;
+                inf("Power channel '%s' turned OFF.", m_power_channels[i].pcs_msg.name.c_str());
                 break;
+
               case IMC::PowerChannelControl::PCC_OP_TURN_ON:
-                m_power_channels[i].state = IMC::PowerChannelState::PCS_ON;
-                inf("Power channel '%s' turned ON.", m_power_channels[i].name.c_str());
+                m_power_channels[i].pcs_msg.state = IMC::PowerChannelState::PCS_ON;
+                inf("Power channel '%s' turned ON.", m_power_channels[i].pcs_msg.name.c_str());
                 break;
+
               case IMC::PowerChannelControl::PCC_OP_TOGGLE:
-                if (m_power_channels[i].state == IMC::PowerChannelState::PCS_ON)
+                if (m_power_channels[i].pcs_msg.state == IMC::PowerChannelState::PCS_ON)
                 {
-                  m_power_channels[i].state = IMC::PowerChannelState::PCS_OFF;
-                  inf("Power channel '%s' toggled to OFF.", m_power_channels[i].name.c_str());
+                  m_power_channels[i].pcs_msg.state = IMC::PowerChannelState::PCS_OFF;
+                  inf("Power channel '%s' toggled to OFF.", m_power_channels[i].pcs_msg.name.c_str());
                 }
                 else
                 {
-                  m_power_channels[i].state = IMC::PowerChannelState::PCS_ON;
-                  inf("Power channel '%s' toggled to ON.", m_power_channels[i].name.c_str());
+                  m_power_channels[i].pcs_msg.state = IMC::PowerChannelState::PCS_ON;
+                  inf("Power channel '%s' toggled to ON.", m_power_channels[i].pcs_msg.name.c_str());
                 }
                 break;
+
+              case IMC::PowerChannelControl::PCC_OP_SCHED_OFF:
+                m_power_channels[i].is_scheduled_active = true;
+                m_power_channels[i].is_sched_off = true;
+                m_power_channels[i].is_sched_on = false;
+                m_power_channels[i].sched_time_in_seconds = msg->sched_time;
+                m_power_channels[i].sched_timer.setTop(m_power_channels[i].sched_time_in_seconds);
+                m_power_channels[i].sched_timer.reset();
+                war("Power channel '%s' scheduled turn OFF in %.0f seconds.",
+                    m_power_channels[i].pcs_msg.name.c_str(), m_power_channels[i].sched_time_in_seconds);
+                break;
+
+              case IMC::PowerChannelControl::PCC_OP_SCHED_ON:
+                m_power_channels[i].is_scheduled_active = true;
+                m_power_channels[i].is_sched_on = true;
+                m_power_channels[i].is_sched_off = false;
+                m_power_channels[i].sched_time_in_seconds = msg->sched_time;
+                m_power_channels[i].sched_timer.setTop(m_power_channels[i].sched_time_in_seconds);
+                m_power_channels[i].sched_timer.reset();
+                war("Power channel '%s' scheduled turn ON in %.0f seconds.",
+                    m_power_channels[i].pcs_msg.name.c_str(), m_power_channels[i].sched_time_in_seconds);
+                break;
+
+              case IMC::PowerChannelControl::PCC_OP_SCHED_RESET:
+                m_power_channels[i].is_scheduled_active = false;
+                m_power_channels[i].is_sched_off = false;
+                m_power_channels[i].is_sched_on = false;
+                war("Power channel '%s' scheduled operations RESET.", m_power_channels[i].pcs_msg.name.c_str());
+                break;
+
               default:
                 war("Power channel control operation %u not implemented.", msg->op);
                 break;
             }
-            dispatch(m_power_channels[i]);
+            dispatch(m_power_channels[i].pcs_msg);
             return;
           }
         }
@@ -257,7 +301,7 @@ namespace Simulators
               {
                 // Dispatch voltage message
                 //check if channel is ON
-                if (m_power_channels[i].state == IMC::PowerChannelState::PCS_OFF)
+                if (m_power_channels[i].pcs_msg.state == IMC::PowerChannelState::PCS_OFF)
                 {
                   m_voltage_msgs[i].value = 0.0;
                   m_current_msgs[i].value = 0.0;
@@ -276,6 +320,31 @@ namespace Simulators
                 m_fuel_level.confidence = 0.9f;
                 m_fuel_level.opmodes = "SIMULATED";
                 dispatch(m_fuel_level);
+
+                // check if scheduled operations are active
+                if(m_power_channels[i].is_scheduled_active)
+                {
+                  if(m_power_channels[i].sched_timer.overflow())
+                  {
+                    if(m_power_channels[i].is_sched_off)
+                    {
+                      m_power_channels[i].pcs_msg.state = IMC::PowerChannelState::PCS_OFF;
+                      war("Power channel '%s' scheduled turn OFF executed.", m_power_channels[i].pcs_msg.name.c_str());
+                    }
+                    else if(m_power_channels[i].is_sched_on)
+                    {
+                      m_power_channels[i].pcs_msg.state = IMC::PowerChannelState::PCS_ON;
+                      war("Power channel '%s' scheduled turn ON executed.", m_power_channels[i].pcs_msg.name.c_str());
+                    }
+                    // reset scheduling flags
+                    m_power_channels[i].is_scheduled_active = false;
+                    m_power_channels[i].is_sched_off = false;
+                    m_power_channels[i].is_sched_on = false;
+                    // dispatch updated state
+                    dispatch(m_power_channels[i].pcs_msg);
+                  }
+                }
+
                 setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_ACTIVE);
               }
             }
