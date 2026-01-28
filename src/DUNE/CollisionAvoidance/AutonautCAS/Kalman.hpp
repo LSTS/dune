f1ebb988c3079d25d1749d6e5c00ab027013a1ea
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2026-01-28 19:48:40 +0000

    DUNE/CollisionAvoidance/AutonautCAS: migrated autonaut collision avoidance system into a collision avoidance entity.

--- src/DUNE/CollisionAvoidance/AutonautCAS/Kalman.hpp
+++ src/DUNE/CollisionAvoidance/AutonautCAS/Kalman.hpp
@@ -12,35 +12,26 @@
  *    <https://www.ntnu.edu/autosea>
  */
 
-
 /**
- *    \file   kalman.h
+ *    \file   Kalman.hpp
  *    \brief  Declares the Kalman class.
  *    \author Giorgio D. K. M. Kufoalor
  */
 
-#ifndef DUNE_NAVIGATION_KALMAN_HPP_INCLUDED_
-#define DUNE_NAVIGATION_KALMAN_HPP_INCLUDED_
+#ifndef DUNE_COLLISION_AVOIDANCE_AUTONAUTCAS_KALMAN_HPP_INCLUDED_
+#define DUNE_COLLISION_AVOIDANCE_AUTONAUTCAS_KALMAN_HPP_INCLUDED_
 
-// DUNE headers.
-#include <DUNE/DUNE.hpp>
-//#include <DUNE/Tasks/Task.hpp>
 #include <Eigen/Dense>
 
-//#pragma once
-
-// Import namespaces.
-using DUNE_NAMESPACES;
-
-namespace Control
+namespace DUNE
 {
-  namespace Path
+  namespace CollisionAvoidance
   {
-    namespace CAS
+    namespace AutonautCAS
     {
       // Export DLL Symbol.
       class DUNE_DLL_SYM Kalman;
-      
+
       class Kalman
       {
         public:
@@ -74,17 +65,18 @@ namespace Control
         /**
         * Create a blank estimator.
         */
-        Kalman();
+        Kalman(void);
         
         /** 
         * Create a destructor
         */
-        ~Kalman();
+        ~Kalman(void);
 
         /**
         * Initialize the filter with initial states as zero.
         */
-        void init(void)
+        void
+        init(void)
         {
           x_hat.setZero();
           P = P0;
@@ -96,7 +88,9 @@ namespace Control
         /**
         * Initialize the filter with a guess for initial states.
         */
-        void init(double t0_, const Eigen::VectorXd& x0) {
+        void
+        init(double t0_, const Eigen::VectorXd& x0)
+        {
           x_hat = x0;
           P = P0;
           this->t0 = t0_;
@@ -108,51 +102,50 @@ namespace Control
         * Update the estimated state based on measured values. The
         * time step is assumed to remain constant.
         */
-        void update(const Eigen::VectorXd& y, double durationLost)
+        void
+        update(const Eigen::VectorXd& y, double durationLost)
         {
           if(!initialized)
             throw std::runtime_error("Filter is not initialized!");
 
           Eigen::VectorXd y_diff;
           y_diff.resize(n);
-
           x_hat_new = A * x_hat;
           P = A*P*A.transpose() + Q;
-        
-          if (durationLost == 0.0){    
+
+          if (durationLost == 0.0)
+          {    
             K = P*C.transpose()*(C*P*C.transpose() + R).inverse();
-            
             y_diff = y - C*x_hat_new;
-            
+
             // Correct difference for a state that is an angle
             // Only if C is a square matrix. Can be generalized for all C implementations!
-            
-            if (m==n){
-                for (int i=0; i<n; i++){
-              if ( angle_tag(i) == 1 ) 
+            if (m==n)
+            {
+              for (int i=0; i<n; i++)
+              {
+                if ( angle_tag(i) == 1 ) 
                   y_diff(i) = angle_diff(x_hat_new(i), y(i)); // y - x_hat_new	  
-                } 
+              } 
             }
-            
-            
+
             //x_hat_new += K * (y - C*x_hat_new);
             x_hat_new += K * y_diff;
-            
+
             P = (I - K*C)*P;
           }
           // else, we run the filter as an open-loop predictor, i.e. K = 0.0  
-          
-          x_hat = x_hat_new;
 
+          x_hat = x_hat_new;
           t += dt; // not used in filter computation, but for fault detection (measurement loss!)
-          std::cout << "tracking for approx. " << t << "secs" << std::endl;
         }
 
         /**
         * Update the estimated state based on measured values,
         * using the given time step and dynamics matrix.
         */
-        void update(const Eigen::VectorXd& y, double durationLost, double dt_, const Eigen::MatrixXd A_)
+        void
+        update(const Eigen::VectorXd& y, double durationLost, double dt_, const Eigen::MatrixXd A_)
         {
           this->A = A_;
           this->dt = dt_;
@@ -162,11 +155,30 @@ namespace Control
         /**
         * Return the current state and time.
         */
-        Eigen::VectorXd state() { return x_hat; };
-        Eigen::MatrixXd errorCov() { return P; };
-        double time() { return t; };
-        int filter_id(){return id;};
-        
+        Eigen::VectorXd
+        state(void)
+        {
+          return x_hat;
+        };
+
+        Eigen::MatrixXd
+        errorCov(void)
+        {
+          return P;
+        };
+
+        double
+        time(void)
+        {
+          return t;
+        };
+
+        int
+        filter_id(void)
+        {
+          return id;
+        };
+
         /**
         * Compute angle difference
         */
@@ -174,40 +186,30 @@ namespace Control
         {
           double dif = fmod(b - a + M_PI,2*M_PI);
           if (dif < 0)
-              dif += 2*M_PI;
+            dif += 2*M_PI;
+
           return dif - M_PI;
         }
-        
 
       private:
-
         // Specifies which states are angles
         Eigen::VectorXd angle_tag;
-        
         // Matrices for computation
         Eigen::MatrixXd A, C, Q, R, P, K, P0;
-
         // System dimensions
         int m, n;
-
         // Initial and current time
         double t0, t;
-
         // Discrete time step
         double dt;
-
         // Is the filter initialized?
         bool initialized;
-
         // n-size identity
         Eigen::MatrixXd I;
-
         // Estimated states
         Eigen::VectorXd x_hat, x_hat_new;
-
         // Filter id
         int id;
-
       };
     }
   }
--- src/DUNE/CollisionAvoidance/AutonautCAS/autonaut.hpp
+++ src/DUNE/CollisionAvoidance/AutonautCAS/autonaut.hpp
@@ -27,25 +27,17 @@
 // Author: Alberto Dallolio                                                 *
 //***************************************************************************
 
-#ifndef DUNE_NAVIGATION_AUTONAUT_HPP_INCLUDED_
-#define DUNE_NAVIGATION_AUTONAUT_HPP_INCLUDED_
-
-// DUNE headers.
-#include <DUNE/DUNE.hpp>
-//#include <DUNE/Tasks/Task.hpp>
+#ifndef DUNE_COLLISION_AVOIDANCE_AUTONAUTCAS_AUTONAUT_HPP_INCLUDED_
+#define DUNE_COLLISION_AVOIDANCE_AUTONAUTCAS_AUTONAUT_HPP_INCLUDED_
 
 #include <Eigen/Dense>
+#include <DUNE/Math/Angles.hpp>
 
-inline const double DEG2RAD = M_PI/180.0f;
-
-// Import namespaces.
-using DUNE_NAMESPACES;
-
-namespace Control
+namespace DUNE
 {
-  namespace Path
+  namespace CollisionAvoidance
   {
-    namespace CAS
+    namespace AutonautCAS
     {
       // Export DLL Symbol.
       class DUNE_DLL_SYM autonaut;
@@ -63,11 +55,9 @@ namespace Control
           m_u.resize(m_n_samp);
           m_v.resize(m_n_samp);
           m_r.resize(m_n_samp);
-
           // Simulation parameters
           m_T = T;
           m_DT = dt;
-
           // Model parameters
           m_A = 2.5; // [m]
           m_B = 2.5; // [m]
@@ -75,26 +65,26 @@ namespace Control
           m_D = 0.4; // [m]
           m_l = (m_A + m_B);
           m_w = (m_C + m_D); // this is the beam.
-
           calculate_position_offsets();
         }
 
         /// Destructor
-        ~autonaut()
+        ~autonaut(void)
         { }
 
-        void linearPrediction(const std::vector<double> &state, double u_d, double psi_d, const Eigen::Matrix<double, -1, 2> &waypoints_, double Chi_ca, int course_change_point, int guidance_strategy, double R, double de, double Ki)
+        void
+        linearPrediction(const std::vector<double> &state, double u_d, double psi_d,
+                         const Eigen::Matrix<double, -1, 2> &waypoints_, double Chi_ca, int course_change_point,
+                         int guidance_strategy, double R, double de, double Ki)
         {
           Eigen::Vector2d d_s_wp1, los_s_wp1, d_wp0_wp1, los_wp0_wp1;
           int n_Hd = 0; // 50, 200; //getHd()/getDT(); // 30/0.5
-
           // LOS guidance dynamics variables and parameters
-          double psi_r;                                   // LOS path course correction
-          double e = 0, e_integral = 0;                   // cross-track error and its integral
+          double psi_r; // LOS path course correction
+          double e = 0, e_integral = 0; // cross-track error and its integral
           double max_integral_corr = M_PI * 20.0 / 180.0; // max integral correction pi*20/180
           Eigen::MatrixXd waypoints(waypoints_.rows(), waypoints_.cols());
           waypoints = waypoints_;
-
           int init_samp = course_change_point * 25 / m_DT; // assumes course is constant for at least (25s)
           if (guidance_strategy >= 2)
             init_samp = init_samp * 4;
@@ -102,11 +92,10 @@ namespace Control
           // initial prediction state
           if (init_samp == 0)
           {
-            m_psi(0) = -normalize_angle(state[2]); // bug fix: use normalized psi // MR interface output sign change
-            psi_d = normalize_angle(psi_d);        // bug fix: use normalized psi_d
+            m_psi(0) = -Math::Angles::normalizeRadian(state[2]); // bug fix: use normalized psi // MR interface output sign change
+            psi_d = Math::Angles::normalizeRadian(psi_d); // bug fix: use normalized psi_d
             m_x(0) = state[0] + m_os_x * std::cos(m_psi(0)) - m_os_y * std::sin(m_psi(0));
             m_y(0) = state[1] + m_os_x * std::sin(m_psi(0)) + m_os_y * std::cos(m_psi(0));
-
             m_u(0) = state[3];
             m_v(0) = state[4];
             m_r(0) = state[5];
@@ -118,13 +107,10 @@ namespace Control
             d_s_wp1(0) = waypoints(1, 0) - m_x(init_samp);
             d_s_wp1(1) = waypoints(1, 1) - m_y(init_samp);
             los_s_wp1 = d_s_wp1 / d_s_wp1.norm();
-
             d_wp0_wp1(0) = waypoints(1, 0) - waypoints(0, 0);
             d_wp0_wp1(1) = waypoints(1, 1) - waypoints(0, 1);
             los_wp0_wp1 = d_wp0_wp1 / d_wp0_wp1.norm();
-
-            bool leg_passed = los_wp0_wp1.dot(-los_s_wp1) > cos(90 * DEG2RAD);
-
+            bool leg_passed = los_wp0_wp1.dot(-los_s_wp1) > std::cos(Math::Angles::radians(90.0));
             // update waypoints for the initial sample
             if (leg_passed)
             {
@@ -137,7 +123,6 @@ namespace Control
           }
 
           double r11, r12, r21, r22;
-
           // compute new course
           double psi_path = std::atan2(waypoints(1, 1) - waypoints(0, 1),
                                        waypoints(1, 0) - waypoints(0, 0));
@@ -151,21 +136,16 @@ namespace Control
           {
             // distance of track from wp0 to wp1
             double track_dist = sqrt(pow(waypoints(1, 0) - waypoints(0, 0), 2) + pow(waypoints(1, 1) - waypoints(0, 1), 2));
-
             if (track_dist > R + m_A + m_B)
             { // else last WP reached, assume constant course!
-
               // Guidance prediction OPT1: use next wp direction as desired course after t= t_Hd
               if (1 == guidance_strategy && i > n_Hd)
               { // 0.9 means the asv is on the move to the WP2
                 // use pure-pursuit guidance behavior after t= t_Hd?
                 d_s_wp1(0) = waypoints(1, 0) - m_x(i);
                 d_s_wp1(1) = waypoints(1, 1) - m_y(i);
-
                 los_s_wp1 = d_s_wp1 / d_s_wp1.norm();
-
                 psi_d = std::atan2(d_s_wp1(1), d_s_wp1(0));
-
                 // apply offset
                 psi_d = psi_d + Chi_ca; // approx behavior as input psi_d
               }
@@ -176,11 +156,8 @@ namespace Control
                 // compute cross-track error Eq (10.59 or 10.10) in Fossen 2011
                 e = -(m_x(i) - waypoints(1, 0)) * std::sin(psi_path) + (m_y(i) - waypoints(1, 1)) * std::cos(psi_path);
                 e_integral += e * m_DT;
-
                 if (e_integral * Ki > max_integral_corr)
-                {
                   e_integral -= e * m_DT;
-                }
 
                 psi_r = std::atan2(-(e + Ki * e_integral), de);
                 psi_d = psi_path + psi_r;
@@ -192,7 +169,6 @@ namespace Control
               // compute values for handling waypoint switching events
               // asv-wp radius
               double asv_wp_radius_sqrd = pow(m_x(i) - waypoints(1, 0), 2) + pow(m_y(i) - waypoints(1, 1), 2);
-
               // along-track error using Eq (10.58 or 10.10) in Fossen 2011
               // NB! the same as remaining track distance
               double along_track_dist = std::fabs((waypoints(1, 0) - m_x(i)) * std::cos(psi_path) + (waypoints(1, 1) - m_y(i)) * std::sin(psi_path));
@@ -208,20 +184,17 @@ namespace Control
               {
                 // use next waypoints as current waypoints
                 for (int k = 0; k < waypoints_.rows() - 1; k++)
-                {
                   waypoints.row(k) = waypoints_.row(k + 1);
-                }
 
                 // reset los integral
                 e_integral = 0;
-
                 // compute new course
                 psi_path = std::atan2(waypoints(1, 1) - waypoints(0, 1),
                                       waypoints(1, 0) - waypoints(0, 0));
               }
             }
 
-            psi_d = normalize_angle(psi_d);
+            psi_d = Math::Angles::normalizeRadian(psi_d);
 
             r11 = std::cos(psi_d);
             r12 = -std::sin(psi_d);
@@ -238,89 +211,29 @@ namespace Control
 
         void linearPredictionInger(const std::vector<double> &state, double u_d, double psi_d)
         {
-          m_psi(0) = normalize_angle(psi_d);
-          m_x(0) = state[0] + m_os_x * cos(state[2]) - m_os_y * sin(state[2]);
-          m_y(0) = state[1] + m_os_x * sin(state[2]) + m_os_y * cos(state[2]);
+          m_psi(0) = Math::Angles::normalizeRadian(psi_d);
+          m_x(0) = state[0] + m_os_x * std::cos(state[2]) - m_os_y * std::sin(state[2]);
+          m_y(0) = state[1] + m_os_x * std::sin(state[2]) + m_os_y * std::cos(state[2]);
           m_u(0) = state[3];
           m_v(0) = state[4];
           m_r(0) = state[5];
-
           double r11, r12, r21, r22;
-
-          r11 = cos(psi_d);
-          r12 = -sin(psi_d);
-          r21 = sin(psi_d);
-          r22 = cos(psi_d);
-
+          r11 = std::cos(psi_d);
+          r12 = -std::sin(psi_d);
+          r21 = std::sin(psi_d);
+          r22 = std::cos(psi_d);
           for (int i = 0; i < m_n_samp - 1; i++)
           {
-
             m_x(i + 1) = m_x(i) + m_DT * (r11 * m_u(i) + r12 * m_v(i));
             m_y(i + 1) = m_y(i) + m_DT * (r21 * m_u(i) + r22 * m_v(i));
             m_psi(i + 1) = psi_d;
             m_u(i + 1) = u_d;
             m_v(i + 1) = 0;
-
-            /*if(i<=m_n_samp-2 && i>= m_n_samp-20)
-            {
-              std::cout << "X INSIDE= " << m_x(i) << std::endl;
-              std::cout << "Y INSIDE= " << m_y(i) << std::endl;
-              std::cout << "PSI INSIDE= " << m_psi(i)*180.0f/M_PI << std::endl;
-            }*/
           }
         }
 
-        Eigen::VectorXd getX()
-        {
-          return m_x;
-        }
-
-        Eigen::VectorXd getY()
-        {
-          return m_y;
-        }
-
-        Eigen::VectorXd getPsi()
-        {
-          return m_psi;
-        }
-
-        Eigen::VectorXd getU()
-        {
-          return m_u;
-        }
-
-        Eigen::VectorXd getV()
-        {
-          return m_v;
-        }
-
-        Eigen::VectorXd getR()
-        {
-          return m_r;
-        }
-
-        double getA()
-        {
-          return m_A;
-        }
-
-        double getB()
-        {
-          return m_B;
-        }
-
-        double getC()
-        {
-          return m_C;
-        }
-
-        double getD()
-        {
-          return m_D;
-        }
-
-        double getL()
+        double
+        getL(void)
         {
           return m_l;
         }
@@ -330,63 +243,12 @@ namespace Control
           return m_w;
         }
 
-        double getT()
-        {
-          return m_T;
-        }
-
-        double getDT()
-        {
-          return m_DT;
-        }
-
-        double getNsamp()
-        {
-          return m_n_samp;
-        }
-
-        void setT(double T)
-        {
-          m_T = T;
-        }
-
-        void setDT(double DT)
-        {
-          m_DT = DT;
-        }
-
-        void setNsamp(int n_samp)
-        {
-          m_n_samp = n_samp;
-        }
-
-        void setA(double A)
-        {
-          m_A = A;
-        }
-
-        void setB(double B)
-        {
-          m_B = B;
-        }
-
-        void setC(double C)
-        {
-          m_C = C;
-        }
-
-        void setD(double D)
-        {
-          m_D = D;
-        }
-
         Eigen::VectorXd m_x;
         Eigen::VectorXd m_y;
         Eigen::VectorXd m_psi;
         Eigen::VectorXd m_u;
         Eigen::VectorXd m_v;
         Eigen::VectorXd m_r;
-
         double m_A, m_B, m_C, m_D, m_l, m_w;
         double m_os_x, m_os_y;
 
@@ -398,35 +260,14 @@ namespace Control
           m_os_y = m_D - m_C;
         }
 
-        // Assures that angle is between [-PI, PI)
-        double normalize_angle(double angle)
-        {
-          if (std::isinf(angle))
-            return angle;
-
-          while (angle <= -M_PI)
-          {
-            angle += 2 * M_PI;
-          }
-
-          while (angle > M_PI)
-          {
-            angle -= 2 * M_PI;
-          }
-
-          return angle;
-        }
-
         // Simulation parameters
         double m_DT;
         double m_T;
         // const int n_samp_;
         int m_n_samp; // possibility to set from sb_mpc
-
-        const double DEG2RAD = M_PI/180.0f;
       };
     }
   }
 }
 
-#endif /* AUTONAUT_HPP_ */
+#endif
