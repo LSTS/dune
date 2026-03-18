ad11b174a02d7e15b323f30165de150c9e5e09d2
Lubern1 <lufezia@hotmail.it>
2026-03-18 17:17:13 +0000

    Transports/DCCL: added switch case for enum

--- src/Transports/DCCL/EnumCodec.hpp
+++ src/Transports/DCCL/EnumCodec.hpp
@@ -1,47 +1,67 @@
 #ifndef ENUMCODEC_HPP
 #define ENUMCODEC_HPP
 
-// AUTO-GENERATED ENUM CODEC (ENCODER + DECODER)
-
 #include "IMC_DCCL.pb.h"
 #include <DUNE/DUNE.hpp>
 
-// =====================================
-//              DECODERS
-// =====================================
-DUNE::IMC::ZUnits decodeZUnits(const uint8_t& dccl_src);
-DUNE::IMC::SpeedUnits decodeSpeedUnits(const uint8_t& dccl_src);
-std::string decodeManeuverType(const uint8_t& dccl_src);
-std::string decodeParamValue(const uint8_t& dccl_src);
-std::string decodeTransitionConditionEnum(const uint8_t& dccl_src);
-std::string decodeParameterName(const uint8_t& dccl_src);
-std::string decodeEntityName(const uint8_t& dccl_src);
-DUNE::IMC::Loiter::LoiterTypeEnum decodeLoiterLoiterTypeEnum(const uint8_t& dccl_src);
+IMC_DCCL::Loiter::DirectionEnum encodeLoiterDirectionEnum(const uint8_t& imc_src);
+
 DUNE::IMC::Loiter::DirectionEnum decodeLoiterDirectionEnum(const uint8_t& dccl_src);
-DUNE::IMC::VerticalProfile::ParameterEnum decodeVerticalProfileParameterEnum(const uint8_t& dccl_src);
-DUNE::IMC::PlanDB::TypeEnum decodePlanDBTypeEnum(const uint8_t& dccl_src);
-DUNE::IMC::PlanDB::OperationEnum decodePlanDBOperationEnum(const uint8_t& dccl_src);
-DUNE::IMC::PlanControl::TypeEnum decodePlanControlTypeEnum(const uint8_t& dccl_src);
+
+IMC_DCCL::Loiter::LoiterTypeEnum encodeLoiterLoiterTypeEnum(const uint8_t& imc_src);
+
+DUNE::IMC::Loiter::LoiterTypeEnum decodeLoiterLoiterTypeEnum(const uint8_t& dccl_src);
+
+IMC_DCCL::PlanControl::OperationEnum encodePlanControlOperationEnum(const uint8_t& imc_src);
+
 DUNE::IMC::PlanControl::OperationEnum decodePlanControlOperationEnum(const uint8_t& dccl_src);
+
+IMC_DCCL::PlanControl::TypeEnum encodePlanControlTypeEnum(const uint8_t& imc_src);
+
+DUNE::IMC::PlanControl::TypeEnum decodePlanControlTypeEnum(const uint8_t& dccl_src);
+
+IMC_DCCL::PlanDB::OperationEnum encodePlanDBOperationEnum(const uint8_t& imc_src);
+
+DUNE::IMC::PlanDB::OperationEnum decodePlanDBOperationEnum(const uint8_t& dccl_src);
+
+IMC_DCCL::PlanDB::TypeEnum encodePlanDBTypeEnum(const uint8_t& imc_src);
+
+DUNE::IMC::PlanDB::TypeEnum decodePlanDBTypeEnum(const uint8_t& dccl_src);
+
+IMC_DCCL::PlanStatistics::TypeEnum encodePlanStatisticsTypeEnum(const uint8_t& imc_src);
+
 DUNE::IMC::PlanStatistics::TypeEnum decodePlanStatisticsTypeEnum(const uint8_t& dccl_src);
 
-// =====================================
-//              ENCODERS
-// =====================================
-IMC_DCCL::ZUnits encodeZUnits(const uint8_t& imc_src);
 IMC_DCCL::SpeedUnits encodeSpeedUnits(const uint8_t& imc_src);
-IMC_DCCL::ManeuverType encodeManeuverType(const std::string& imc_src);
-IMC_DCCL::ParamValue encodeParamValue(const std::string& imc_src);
-IMC_DCCL::TransitionConditionEnum encodeTransitionConditionEnum(const std::string& imc_src);
-IMC_DCCL::ParameterName encodeParameterName(const std::string& imc_src);
-IMC_DCCL::EntityName encodeEntityName(const std::string& imc_src);
-IMC_DCCL::Loiter::LoiterTypeEnum encodeLoiterLoiterTypeEnum(const uint8_t& imc_src);
-IMC_DCCL::Loiter::DirectionEnum encodeLoiterDirectionEnum(const uint8_t& imc_src);
+
+DUNE::IMC::SpeedUnits decodeSpeedUnits(const uint8_t& dccl_src);
+
 IMC_DCCL::VerticalProfile::ParameterEnum encodeVerticalProfileParameterEnum(const uint8_t& imc_src);
-IMC_DCCL::PlanDB::TypeEnum encodePlanDBTypeEnum(const uint8_t& imc_src);
-IMC_DCCL::PlanDB::OperationEnum encodePlanDBOperationEnum(const uint8_t& imc_src);
-IMC_DCCL::PlanControl::TypeEnum encodePlanControlTypeEnum(const uint8_t& imc_src);
-IMC_DCCL::PlanControl::OperationEnum encodePlanControlOperationEnum(const uint8_t& imc_src);
-IMC_DCCL::PlanStatistics::TypeEnum encodePlanStatisticsTypeEnum(const uint8_t& imc_src);
+
+DUNE::IMC::VerticalProfile::ParameterEnum decodeVerticalProfileParameterEnum(const uint8_t& dccl_src);
+
+IMC_DCCL::ZUnits encodeZUnits(const uint8_t& imc_src);
+
+DUNE::IMC::ZUnits decodeZUnits(const uint8_t& dccl_src);
+
+IMC_DCCL::EntityName encodeEntityName(const std::string& imc_src);
+
+std::string decodeEntityName(const uint8_t& dccl_src);
+
+IMC_DCCL::ManeuverType encodeManeuverType(const std::string& imc_src);
+
+std::string decodeManeuverType(const uint8_t& dccl_src);
+
+IMC_DCCL::ParamValue encodeParamValue(const std::string& imc_src);
+
+std::string decodeParamValue(const uint8_t& dccl_src);
+
+IMC_DCCL::ParameterName encodeParameterName(const std::string& imc_src);
+
+std::string decodeParameterName(const uint8_t& dccl_src);
+
+IMC_DCCL::TransitionConditionEnum encodeTransitionConditionEnum(const std::string& imc_src);
+
+std::string decodeTransitionConditionEnum(const uint8_t& dccl_src);
 
 #endif // ENUMCODEC_HPP
--- src/Transports/DCCL/MsgCodec.hpp
+++ src/Transports/DCCL/MsgCodec.hpp
@@ -6,25 +6,9 @@
 #include "EnumCodec.hpp"
 #include "MsgOnlyProtoCodec.hpp"
 
-void encodeVerticalProfile(const DUNE::IMC::VerticalProfile& imc, IMC_DCCL::VerticalProfile& dccl);
-
-void decodeVerticalProfile(const IMC_DCCL::VerticalProfile& dccl, DUNE::IMC::VerticalProfile& imc);
-
-void encodePlanSpecification(const DUNE::IMC::PlanSpecification& imc, IMC_DCCL::PlanSpecification& dccl);
-
-void decodePlanSpecification(const IMC_DCCL::PlanSpecification& dccl, DUNE::IMC::PlanSpecification& imc);
-
 void encodePathPoint(const DUNE::IMC::PathPoint& imc, IMC_DCCL::PathPoint& dccl);
 
 void decodePathPoint(const IMC_DCCL::PathPoint& dccl, DUNE::IMC::PathPoint& imc);
-
-void encodeLoiter(const DUNE::IMC::Loiter& imc, IMC_DCCL::Loiter& dccl);
-
-void decodeLoiter(const IMC_DCCL::Loiter& dccl, DUNE::IMC::Loiter& imc);
-
-void encodePlanManeuver(const DUNE::IMC::PlanManeuver& imc, IMC_DCCL::PlanManeuver& dccl);
-
-void decodePlanManeuver(const IMC_DCCL::PlanManeuver& dccl, DUNE::IMC::PlanManeuver& imc);
 
 void encodePlanDBState(const DUNE::IMC::PlanDBState& imc, IMC_DCCL::PlanDBState& dccl);
 
@@ -34,41 +18,9 @@ void encodeStationKeeping(const DUNE::IMC::StationKeeping& imc, IMC_DCCL::Statio
 
 void decodeStationKeeping(const IMC_DCCL::StationKeeping& dccl, DUNE::IMC::StationKeeping& imc);
 
-void encodeFollowPath(const DUNE::IMC::FollowPath& imc, IMC_DCCL::FollowPath& dccl);
+void encodeVerticalProfile(const DUNE::IMC::VerticalProfile& imc, IMC_DCCL::VerticalProfile& dccl);
 
-void decodeFollowPath(const IMC_DCCL::FollowPath& dccl, DUNE::IMC::FollowPath& imc);
-
-void encodePlanDB(const DUNE::IMC::PlanDB& imc, IMC_DCCL::PlanDB& dccl);
-
-void decodePlanDB(const IMC_DCCL::PlanDB& dccl, DUNE::IMC::PlanDB& imc);
-
-void encodeSetEntityParameters(const DUNE::IMC::SetEntityParameters& imc, IMC_DCCL::SetEntityParameters& dccl);
-
-void decodeSetEntityParameters(const IMC_DCCL::SetEntityParameters& dccl, DUNE::IMC::SetEntityParameters& imc);
-
-void encodePlanControl(const DUNE::IMC::PlanControl& imc, IMC_DCCL::PlanControl& dccl);
-
-void decodePlanControl(const IMC_DCCL::PlanControl& dccl, DUNE::IMC::PlanControl& imc);
-
-void encodePlanDBInformation(const DUNE::IMC::PlanDBInformation& imc, IMC_DCCL::PlanDBInformation& dccl);
-
-void decodePlanDBInformation(const IMC_DCCL::PlanDBInformation& dccl, DUNE::IMC::PlanDBInformation& imc);
-
-void encodeEstimatedState(const DUNE::IMC::EstimatedState& imc, IMC_DCCL::EstimatedState& dccl);
-
-void decodeEstimatedState(const IMC_DCCL::EstimatedState& dccl, DUNE::IMC::EstimatedState& imc);
-
-void encodeProfileSample(const DUNE::IMC::ProfileSample& imc, IMC_DCCL::ProfileSample& dccl);
-
-void decodeProfileSample(const IMC_DCCL::ProfileSample& dccl, DUNE::IMC::ProfileSample& imc);
-
-void encodeEntityParameter(const DUNE::IMC::EntityParameter& imc, IMC_DCCL::EntityParameter& dccl);
-
-void decodeEntityParameter(const IMC_DCCL::EntityParameter& dccl, DUNE::IMC::EntityParameter& imc);
-
-void encodeGoto(const DUNE::IMC::Goto& imc, IMC_DCCL::Goto& dccl);
-
-void decodeGoto(const IMC_DCCL::Goto& dccl, DUNE::IMC::Goto& imc);
+void decodeVerticalProfile(const IMC_DCCL::VerticalProfile& dccl, DUNE::IMC::VerticalProfile& imc);
 
 void encodePlanTransition(const DUNE::IMC::PlanTransition& imc, IMC_DCCL::PlanTransition& dccl);
 
@@ -77,5 +29,53 @@ void decodePlanTransition(const IMC_DCCL::PlanTransition& dccl, DUNE::IMC::PlanT
 void encodePlanStatistics(const DUNE::IMC::PlanStatistics& imc, IMC_DCCL::PlanStatistics& dccl);
 
 void decodePlanStatistics(const IMC_DCCL::PlanStatistics& dccl, DUNE::IMC::PlanStatistics& imc);
+
+void encodePlanDB(const DUNE::IMC::PlanDB& imc, IMC_DCCL::PlanDB& dccl);
+
+void decodePlanDB(const IMC_DCCL::PlanDB& dccl, DUNE::IMC::PlanDB& imc);
+
+void encodePlanDBInformation(const DUNE::IMC::PlanDBInformation& imc, IMC_DCCL::PlanDBInformation& dccl);
+
+void decodePlanDBInformation(const IMC_DCCL::PlanDBInformation& dccl, DUNE::IMC::PlanDBInformation& imc);
+
+void encodeSetEntityParameters(const DUNE::IMC::SetEntityParameters& imc, IMC_DCCL::SetEntityParameters& dccl);
+
+void decodeSetEntityParameters(const IMC_DCCL::SetEntityParameters& dccl, DUNE::IMC::SetEntityParameters& imc);
+
+void encodeEstimatedState(const DUNE::IMC::EstimatedState& imc, IMC_DCCL::EstimatedState& dccl);
+
+void decodeEstimatedState(const IMC_DCCL::EstimatedState& dccl, DUNE::IMC::EstimatedState& imc);
+
+void encodeEntityParameter(const DUNE::IMC::EntityParameter& imc, IMC_DCCL::EntityParameter& dccl);
+
+void decodeEntityParameter(const IMC_DCCL::EntityParameter& dccl, DUNE::IMC::EntityParameter& imc);
+
+void encodeFollowPath(const DUNE::IMC::FollowPath& imc, IMC_DCCL::FollowPath& dccl);
+
+void decodeFollowPath(const IMC_DCCL::FollowPath& dccl, DUNE::IMC::FollowPath& imc);
+
+void encodeProfileSample(const DUNE::IMC::ProfileSample& imc, IMC_DCCL::ProfileSample& dccl);
+
+void decodeProfileSample(const IMC_DCCL::ProfileSample& dccl, DUNE::IMC::ProfileSample& imc);
+
+void encodeGoto(const DUNE::IMC::Goto& imc, IMC_DCCL::Goto& dccl);
+
+void decodeGoto(const IMC_DCCL::Goto& dccl, DUNE::IMC::Goto& imc);
+
+void encodePlanManeuver(const DUNE::IMC::PlanManeuver& imc, IMC_DCCL::PlanManeuver& dccl);
+
+void decodePlanManeuver(const IMC_DCCL::PlanManeuver& dccl, DUNE::IMC::PlanManeuver& imc);
+
+void encodePlanControl(const DUNE::IMC::PlanControl& imc, IMC_DCCL::PlanControl& dccl);
+
+void decodePlanControl(const IMC_DCCL::PlanControl& dccl, DUNE::IMC::PlanControl& imc);
+
+void encodePlanSpecification(const DUNE::IMC::PlanSpecification& imc, IMC_DCCL::PlanSpecification& dccl);
+
+void decodePlanSpecification(const IMC_DCCL::PlanSpecification& dccl, DUNE::IMC::PlanSpecification& imc);
+
+void encodeLoiter(const DUNE::IMC::Loiter& imc, IMC_DCCL::Loiter& dccl);
+
+void decodeLoiter(const IMC_DCCL::Loiter& dccl, DUNE::IMC::Loiter& imc);
 
 #endif // MSGCODEC_HPP
