a4a907493e0ee960f65a339e70cd0f764cfc8cfc
Bernardo Gabriel <bernardo.gabriel@lsts.pt>
2026-03-25 10:03:59 +0000

    www: store system info to use when Announce message is not sent to frontend.

--- www/js/Main.js
+++ www/js/Main.js
@@ -40,6 +40,7 @@ Main.prototype = new BasicSection;
 var system_mode = 6;
 var GPS_srcEntity_id = -1;
 var rpm_value = 0;
+var system_info = 'Unknown';
 
 function getHeader(label) {
   return function(data) {
@@ -602,11 +603,11 @@ function getSystemInfo(data) {
         var msg = data.dune_messages[i];
         var systemType = getSystemType(data, msg.sys_type);
         document.title = msg.sys_name + ' (' + getSystemTypeString(msg.sys_type) + ')';
-        return msg.sys_name + ' (' + systemType + ')';
+        system_info = msg.sys_name + ' (' + systemType + ')';
       }
     }
   }
-  return 'Unknown';
+  return system_info;
 }
 
 function getSystemTypeString(value) {
