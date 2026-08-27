251662b10c4d2bf61b1c644026ca837e12615f6f
Luís Venâncio <lvenancio@inegi.up.pt>
2026-08-27 13:58:29 +0100

    DUNE/Hardware/GPIO/GPIOCharDevice: Added GPIO interface implementation based on linux/gpio.h.

---
+++ src/DUNE/Hardware/GPIO/GPIOCharDevice.hpp
@@ -0,0 +1,87 @@
+//***************************************************************************
+// Copyright 2007-2026 Universidade do Porto - Faculdade de Engenharia      *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)                  *
+//***************************************************************************
+// This file is part of DUNE: Unified Navigation Environment.               *
+//                                                                          *
+// Commercial Licence Usage                                                 *
+// Licencees holding valid commercial DUNE licences may use this file in    *
+// accordance with the commercial licence agreement provided with the       *
+// Software or, alternatively, in accordance with the terms contained in a  *
+// written agreement between you and Faculdade de Engenharia da             *
+// Universidade do Porto. For licensing terms, conditions, and further      *
+// information contact lsts@fe.up.pt.                                       *
+//                                                                          *
+// Modified European Union Public Licence - EUPL v.1.1 Usage                *
+// Alternatively, this file may be used under the terms of the Modified     *
+// EUPL, Version 1.1 only (the "Licence"), appearing in the file LICENCE.md *
+// included in the packaging of this file. You may not use this work        *
+// except in compliance with the Licence. Unless required by applicable     *
+// law or agreed to in writing, software distributed under the Licence is   *
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF     *
+// ANY KIND, either express or implied. See the Licence for the specific    *
+// language governing permissions and limitations at                        *
+// https://github.com/LSTS/dune/blob/master/LICENCE.md and                  *
+// http://ec.europa.eu/idabc/eupl.html.                                     *
+//***************************************************************************
+// Author: Luis Venâncio                                                    *
+//***************************************************************************
+
+#ifndef DUNE_HARDWARE_GPIO_GPIO_CHAR_DEVICE_HPP_INCLUDED_
+#define DUNE_HARDWARE_GPIO_GPIO_CHAR_DEVICE_HPP_INCLUDED_
+
+// ISO C++ 98 headers.
+#include <string>
+
+// DUNE headers.
+#include <DUNE/Hardware/GPIO/GPIOInterface.hpp>
+
+namespace DUNE
+{
+  namespace Hardware
+  {
+    namespace GPIOInternals
+    {
+      class GPIOCharDevice: public GPIOInterface
+      {
+      public:
+        GPIOCharDevice(const std::string& device, unsigned int offset);
+
+        ~GPIOCharDevice(void);
+
+        void
+        setDirection(GPIO::Direction direction);
+
+        void
+        setValue(bool value);
+
+        bool
+        getValue(void);
+
+      private:
+        enum Version
+        {
+          VERSION_PENDING,
+          VERSION_1,
+          VERSION_2
+        };
+
+        unsigned int m_offset;
+        Version m_version;
+        int m_chip_fd;
+        int m_line_fd;
+
+        static bool
+        isUnavailable(int code);
+
+        void
+        requestLine(GPIO::Direction direction);
+
+        void
+        configureLine(GPIO::Direction direction);
+      };
+    }
+  }
+}
+
+#endif
